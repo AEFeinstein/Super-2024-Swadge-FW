9ca700276834e8f2a9e746b04b67736c3e10ec67
AEFeinstein <gelakinetic@gmail.com>
2023-01-22 22:56:07 -0500

    Fix URLs

--- components/hdw-tft/hdw-tft.c
+++ components/hdw-tft/hdw-tft.c
@@ -3,7 +3,7 @@
  * \section tft_design Design Philosophy
  *
  * TFT code is based on <a
- * href="https://github.com/espressif/esp-idf/tree/master/examples/peripherals/lcd/tjpgd">Espressif's LCD tjpgd
+ * href="https://github.com/espressif/esp-idf/tree/release/v5.0/examples/peripherals/lcd/tjpgd">Espressif's LCD tjpgd
  * example</a>.
  *
  * Each pixel in the framebuffer is of type ::paletteColor_t.
