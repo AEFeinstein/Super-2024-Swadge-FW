cf6f03ef088671ffa451a784b2f74155b6fa6bdb
AEFeinstein <gelakinetic@gmail.com>
2023-03-07 01:01:54 -0500

    Rework pong

--- main/modes/pong/pong.c
+++ main/modes/pong/pong.c
@@ -1,27 +1,15 @@
 #include "esp_random.h"
 #include "pong.h"
 
-// #define BOARD_SIZE 240 /* In pixels */
-#define IRQ_HZ 32
+#define DECIMAL_BITS 4
 
-// #define S_M                    IRQ_HZ          /* Position Multiplier */
-#define V_M IRQ_HZ /* Velocity Multiplier */
-// #define PADDLE_SIZE            (4 * V_M * S_M) /* in sim size */
-// #define TIME_STEP              (S_M / IRQ_HZ)  /* (1/32) seconds * 32 multiplier */
-// #define EXTRA_ROTATION_ON_EDGE 15              /* degrees */
-#define SPEED_LIMIT 1200
+#define BALL_RADIUS   (8 << DECIMAL_BITS)
+#define PADDLE_WIDTH  (8 << DECIMAL_BITS)
+#define PADDLE_HEIGHT (40 << DECIMAL_BITS)
+#define FIELD_HEIGHT  (TFT_HEIGHT << DECIMAL_BITS)
+#define FIELD_WIDTH   (TFT_WIDTH << DECIMAL_BITS)
 
-#define X 0
-#define Y 1
-
-#define BALL_RADIUS   16
-#define PADDLE_WIDTH  8
-#define PADDLE_HEIGHT 40
-
-#define FIELD_HEIGHT TFT_HEIGHT
-#define FIELD_WIDTH  TFT_WIDTH
-
-#define DEAD_ZONE 15
+#define SPEED_LIMIT (15 << DECIMAL_BITS)
 
 static void pongMainLoop(int64_t elapsedUs);
 static void pongEnterMode(void);
@@ -29,15 +17,10 @@ static void pongExitMode(void);
 static void pongEspNowRecvCb(const esp_now_recv_info_t* esp_now_info, const uint8_t* data, uint8_t len, int8_t rssi);
 static void pongEspNowSendCb(const uint8_t* mac_addr, esp_now_send_status_t status);
 
-void ResetGame(uint8_t isInit, uint8_t whoWon);
+void ResetGame(bool isInit, uint8_t whoWon);
 void RotateBall(int16_t degree);
 void IncreaseSpeed(int16_t speedM);
 void DrawField(void);
-void ProcessInput(__attribute__((unused)) int32_t p1ax, int32_t p1ay, __attribute__((unused)) int8_t p1bl,
-                  __attribute__((unused)) int8_t p1br, __attribute__((unused)) int8_t p1bu,
-                  __attribute__((unused)) int8_t p1bd, __attribute__((unused)) int32_t p2ax, int32_t p2ay,
-                  __attribute__((unused)) int8_t p2bl, __attribute__((unused)) int8_t p2br,
-                  __attribute__((unused)) int8_t p2bu, __attribute__((unused)) int8_t p2bd);
 
 const char pongName[] = "Pong";
 
@@ -59,20 +42,27 @@ swadgeMode_t pongMode = {
 
 typedef struct
 {
+    int32_t x;
+    int32_t y;
+} vec_t;
+
+typedef struct
+{
     p2pInfo p2p;
-    int32_t ballLoc[2];
-    int32_t ballVel[2];
+    vec_t ballLoc;
+    vec_t ballVel;
     int32_t paddleLocL;
     int32_t paddleLocR;
-    uint16_t restartTimer;
+    uint32_t restartTimerUs;
+    uint16_t btnState;
 } pong_t;
 
 pong_t* pong = NULL;
 
 /**************************
- *                         *
- *         Pong!!          *
- *                         *
+ * *
+ * Pong!! *
+ * *
  ***************************/
 
 /**
@@ -154,47 +144,67 @@ static bool circleRect(int32_t cx, int32_t cy, int32_t radius, int32_t rx, int32
  */
 static void pongMainLoop(int64_t elapsedUs)
 {
-    // Physics!
+    // Process button events
+    buttonEvt_t evt = {0};
+    while (checkButtonQueue(&evt))
+    {
+        pong->btnState = evt.state;
+    }
+
+    // TODO count-in timer
+
+    // Move the player paddle
+    if (pong->btnState & PB_UP)
+    {
+        pong->paddleLocL = MAX(pong->paddleLocL - (5 << DECIMAL_BITS), 0);
+    }
+    else if (pong->btnState & PB_DOWN)
+    {
+        pong->paddleLocL = MIN(pong->paddleLocL + (5 << DECIMAL_BITS), FIELD_HEIGHT - PADDLE_HEIGHT);
+    }
+
+    // TODO Move the computer paddle
+    pong->paddleLocR = CLAMP(pong->ballLoc.y - PADDLE_HEIGHT / 2, 0, FIELD_HEIGHT - PADDLE_HEIGHT);
+
     // Update the ball's position
-    pong->ballLoc[X] += (pong->ballVel[X] * elapsedUs) / 100000;
-    pong->ballLoc[Y] += (pong->ballVel[Y] * elapsedUs) / 100000;
+    pong->ballLoc.x += (pong->ballVel.x * elapsedUs) / 100000;
+    pong->ballLoc.y += (pong->ballVel.y * elapsedUs) / 100000;
 
     // Checks for top and bottom wall collisons
-    if (((pong->ballLoc[Y] - BALL_RADIUS) < 0 && pong->ballVel[Y] < 0)
-        || ((pong->ballLoc[Y] + BALL_RADIUS) > FIELD_HEIGHT && pong->ballVel[Y] > 0))
+    if (((pong->ballLoc.y - BALL_RADIUS) < 0 && pong->ballVel.y < 0)
+        || ((pong->ballLoc.y + BALL_RADIUS) > FIELD_HEIGHT && pong->ballVel.y > 0))
     {
         // Reverse direction
-        pong->ballVel[Y] = -pong->ballVel[Y];
+        pong->ballVel.y = -pong->ballVel.y;
     }
 
-    if (((pong->ballLoc[X] - BALL_RADIUS) < 0 && pong->ballVel[X] < 0)
-        || ((pong->ballLoc[X] + BALL_RADIUS) > FIELD_WIDTH && pong->ballVel[X] > 0))
+    // check for game over
+    if (pong->ballLoc.x < 0 || pong->ballLoc.x > FIELD_WIDTH)
     {
-        // Reverse direction
-        pong->ballVel[X] = -pong->ballVel[X];
+        ResetGame(false, pong->ballLoc.x < 0 ? 0 : 1);
+        DrawField();
+        return;
     }
 
-    if ((pong->ballVel[X] < 0)
-        && circleRect(pong->ballLoc[X], pong->ballLoc[Y], BALL_RADIUS, 0, pong->paddleLocL, PADDLE_WIDTH,
-                      PADDLE_HEIGHT))
+    // Check for left paddle collision
+    if ((pong->ballVel.x < 0)
+        && circleRect(pong->ballLoc.x, pong->ballLoc.y, BALL_RADIUS, 0, pong->paddleLocL, PADDLE_WIDTH, PADDLE_HEIGHT))
     {
         // Reverse direction
-        pong->ballVel[X] = -pong->ballVel[X];
-        // TODO increase speed
+        pong->ballVel.x = -pong->ballVel.x;
+        IncreaseSpeed(1 << DECIMAL_BITS);
         // TODO angle collision
     }
-    else if ((pong->ballVel[X] > 0)
-             && circleRect(pong->ballLoc[X], pong->ballLoc[Y], BALL_RADIUS, FIELD_WIDTH - PADDLE_WIDTH,
-                           pong->paddleLocR, PADDLE_WIDTH, PADDLE_HEIGHT))
+    // Check for right paddle collision
+    else if ((pong->ballVel.x > 0)
+             && circleRect(pong->ballLoc.x, pong->ballLoc.y, BALL_RADIUS, FIELD_WIDTH - PADDLE_WIDTH, pong->paddleLocR,
+                           PADDLE_WIDTH, PADDLE_HEIGHT))
     {
         // Reverse direction
-        pong->ballVel[X] = -pong->ballVel[X];
-        // TODO increase speed
+        pong->ballVel.x = -pong->ballVel.x;
+        IncreaseSpeed(1 << DECIMAL_BITS);
         // TODO angle collision
     }
-
-    // TODO check for win condition
-    // TODO count-in timer
 
     DrawField();
 
@@ -207,61 +217,61 @@ static void pongMainLoop(int64_t elapsedUs)
     // {
     //     // Physics!
     //     // Update the ball's position
-    //     pong->ballLoc[X] += (pong->ballVel[X] * elapsedUs);
-    //     pong->ballLoc[Y] += (pong->ballVel[Y] * elapsedUs);
+    //     pong->ballLoc.x += (pong->ballVel.x * elapsedUs);
+    //     pong->ballLoc.y += (pong->ballVel.y * elapsedUs);
 
     //     // Check for collisions, win conditions. Nudge the ball if necessary
     //     // Top wall collision
-    //     if (pong->ballLoc[Y] < 0 && pong->ballVel[Y] < 0)
+    //     if (pong->ballLoc.y < 0 && pong->ballVel.y < 0)
     //     {
-    //         pong->ballVel[Y] = -pong->ballVel[Y];
+    //         pong->ballVel.y = -pong->ballVel.y;
     //     }
     //     // Bottom wall collision
-    //     else if (pong->ballLoc[Y] >= FIELD_HEIGHT && pong->ballVel[Y] > 0)
+    //     else if (pong->ballLoc.y >= FIELD_HEIGHT && pong->ballVel.y > 0)
     //     {
-    //         pong->ballVel[Y] = -pong->ballVel[Y];
+    //         pong->ballVel.y = -pong->ballVel.y;
     //     }
     //     // left paddle collision
-    //     else if (pong->ballLoc[X] < 1 * S_M * V_M && pong->ballVel[X] < 0
-    //              && (pong->paddleLocL <= pong->ballLoc[Y] && pong->ballLoc[Y] < pong->paddleLocL + PADDLE_SIZE))
+    //     else if (pong->ballLoc.x < 1 * S_M * V_M && pong->ballVel.x < 0
+    //              && (pong->paddleLocL <= pong->ballLoc.y && pong->ballLoc.y < pong->paddleLocL + PADDLE_SIZE))
     //     {
-    //         pong->ballVel[X] = -pong->ballVel[X];
-    //         pong->ballLoc[X] = 1 * S_M * V_M; // right on the edge of the paddle
+    //         pong->ballVel.x = -pong->ballVel.x;
+    //         pong->ballLoc.x = 1 * S_M * V_M; // right on the edge of the paddle
 
     //         // Increase speed on collision
     //         IncreaseSpeed(37); // remember, divided by V_M
 
     //         // Apply extra rotation depending on part of the paddle hit
     //         // range of diff is -PADDLE_SIZE/2 to PADDLE_SIZE/2 (+/- 8192)
-    //         int16_t diff = pong->ballLoc[Y] - (pong->paddleLocL + PADDLE_SIZE / 2);
+    //         int16_t diff = pong->ballLoc.y - (pong->paddleLocL + PADDLE_SIZE / 2);
     //         // rotate 45deg at edge of paddle, 0deg in middle, linear in between
     //         rotation = (EXTRA_ROTATION_ON_EDGE * diff) / (PADDLE_SIZE / 2);
     //         RotateBall(rotation);
     //     }
     //     // right paddle collision
-    //     else if (pong->ballLoc[X] >= (BOARD_SIZE - 1) * S_M * V_M && pong->ballVel[X] > 0
-    //              && (pong->paddleLocR <= pong->ballLoc[Y] && pong->ballLoc[Y] < pong->paddleLocR + PADDLE_SIZE))
+    //     else if (pong->ballLoc.x >= (BOARD_SIZE - 1) * S_M * V_M && pong->ballVel.x > 0
+    //              && (pong->paddleLocR <= pong->ballLoc.y && pong->ballLoc.y < pong->paddleLocR + PADDLE_SIZE))
     //     {
-    //         pong->ballVel[X] = -pong->ballVel[X];
-    //         pong->ballLoc[X] = (BOARD_SIZE - 1) * S_M * V_M - 1; // right on the edge of the paddle
+    //         pong->ballVel.x = -pong->ballVel.x;
+    //         pong->ballLoc.x = (BOARD_SIZE - 1) * S_M * V_M - 1; // right on the edge of the paddle
 
     //         // Increase speed on collision
     //         IncreaseSpeed(37); // remember, divided by V_M
 
     //         // Apply extra rotation depending on part of the paddle hit
     //         // range of diff is -PADDLE_SIZE/2 to PADDLE_SIZE/2 (+/- 8192)
-    //         int16_t diff = pong->ballLoc[Y] - (pong->paddleLocR + PADDLE_SIZE / 2);
+    //         int16_t diff = pong->ballLoc.y - (pong->paddleLocR + PADDLE_SIZE / 2);
     //         // rotate 45deg at edge of paddle, 0deg in middle, linear in between
     //         rotation = (EXTRA_ROTATION_ON_EDGE * diff) / (PADDLE_SIZE / 2);
     //         RotateBall(-rotation);
     //     }
     //     // left wall win
-    //     else if (pong->ballLoc[X] < 0 && pong->ballVel[X] < 0)
+    //     else if (pong->ballLoc.x < 0 && pong->ballVel.x < 0)
     //     {
     //         ResetGame(0, 1);
     //     }
     //     // right wall win
-    //     else if (pong->ballLoc[X] >= BOARD_SIZE * S_M * V_M && pong->ballVel[X] > 0)
+    //     else if (pong->ballLoc.x >= BOARD_SIZE * S_M * V_M && pong->ballVel.x > 0)
     //     {
     //         ResetGame(0, 0);
     //     }
@@ -275,30 +285,30 @@ static void pongMainLoop(int64_t elapsedUs)
  * @param isInit
  * @param whoWon
  */
-void ResetGame(uint8_t isInit, uint8_t whoWon)
+void ResetGame(bool isInit, uint8_t whoWon)
 {
     if (isInit)
     {
         pong->paddleLocL = (FIELD_HEIGHT - PADDLE_HEIGHT) / 2;
         pong->paddleLocR = (FIELD_HEIGHT - PADDLE_HEIGHT) / 2;
     }
-    pong->restartTimer = IRQ_HZ * 3;
-    pong->ballLoc[X]   = (FIELD_WIDTH) / 2;
-    pong->ballLoc[Y]   = (FIELD_HEIGHT) / 2;
+    pong->restartTimerUs = 1000000;
+    pong->ballLoc.x      = (FIELD_WIDTH) / 2;
+    pong->ballLoc.y      = (FIELD_HEIGHT) / 2;
     if (whoWon)
     {
-        pong->ballVel[X] = 5;
+        pong->ballVel.x = (5 << DECIMAL_BITS);
     }
     else
     {
-        pong->ballVel[X] = 5;
+        pong->ballVel.x = -(5 << DECIMAL_BITS);
     }
     uint8_t initY = esp_random() % 8;
     if (initY == 4)
     {
         initY++;
     }
-    pong->ballVel[Y] = (initY - 4) * V_M;
+    pong->ballVel.y = (initY - 4) << DECIMAL_BITS;
     DrawField();
 }
 
@@ -309,11 +319,11 @@ void ResetGame(uint8_t isInit, uint8_t whoWon)
  */
 void RotateBall(int16_t degree)
 {
-    if (degree < 0)
+    while (degree < 0)
     {
         degree += 360;
     }
-    if (degree > 359)
+    while (degree > 359)
     {
         degree -= 360;
     }
@@ -321,97 +331,56 @@ void RotateBall(int16_t degree)
     int16_t sin = getSin1024(degree);
     int16_t cos = getCos1024(degree);
 
-    pong->ballVel[X] = (pong->ballVel[X] * (int32_t)cos - pong->ballVel[Y] * (int32_t)sin) / 32;
-    pong->ballVel[Y] = (pong->ballVel[X] * (int32_t)sin + pong->ballVel[Y] * (int32_t)cos) / 32;
+    pong->ballVel.x = (pong->ballVel.x * cos - pong->ballVel.y * sin) / 1024;
+    pong->ballVel.y = (pong->ballVel.x * sin + pong->ballVel.y * cos) / 1024;
 }
 
 /**
- * @brief Apply a multiplier to the velocity. make this additive instead?
+ * @brief Apply a multiplier to the velocity.
+ * TODO make this additive instead?
  *
  * @param speedM
  */
 void IncreaseSpeed(int16_t speedM)
 {
-    pong->ballVel[X] = (pong->ballVel[X] * speedM) / V_M;
-    if (pong->ballVel[X] > SPEED_LIMIT)
+    int32_t denom      = ABS(pong->ballVel.x) + ABS(pong->ballVel.y);
+    int32_t xComponent = (speedM * ABS(pong->ballVel.x)) / denom;
+    if (pong->ballVel.x < 0)
     {
-        pong->ballVel[X] = SPEED_LIMIT;
+        xComponent = -xComponent;
     }
-    else if (pong->ballVel[X] < -SPEED_LIMIT)
+    int32_t yComponent = (speedM * ABS(pong->ballVel.y)) / denom;
+    if (pong->ballVel.y < 0)
     {
-        pong->ballVel[X] = -SPEED_LIMIT;
+        yComponent = -yComponent;
     }
 
-    pong->ballVel[Y] = (pong->ballVel[Y] * speedM) / V_M;
-    if (pong->ballVel[Y] > SPEED_LIMIT)
-    {
-        pong->ballVel[Y] = SPEED_LIMIT;
-    }
-    else if (pong->ballVel[Y] < -SPEED_LIMIT)
-    {
-        pong->ballVel[Y] = -SPEED_LIMIT;
-    }
+    printf("Speedup [%d %d] [%d %d]\n", xComponent, yComponent, pong->ballVel.x, pong->ballVel.y);
+    pong->ballVel.x = CLAMP((pong->ballVel.x + xComponent), -SPEED_LIMIT, SPEED_LIMIT);
+    pong->ballVel.y = CLAMP((pong->ballVel.y + yComponent), -SPEED_LIMIT, SPEED_LIMIT);
 }
 
 /**
- * @brief TODO
+ * @brief Draw the Pong field to the TFT
  */
 void DrawField(void)
 {
-    printf("%d, %d\n", pong->ballLoc[X], pong->ballLoc[Y]);
-
     clearPxTft();
-    drawCircleFilled(pong->ballLoc[X], pong->ballLoc[Y], BALL_RADIUS, c555);
-    drawRect(0, pong->paddleLocL, PADDLE_WIDTH, pong->paddleLocL + PADDLE_HEIGHT, c050);
-    drawRect(TFT_WIDTH - PADDLE_WIDTH, pong->paddleLocR, TFT_WIDTH, pong->paddleLocR + PADDLE_HEIGHT, c005);
-}
-
-/**
- * @brief TODO
- *
- * @param p1ay
- * @param p2ay
- */
-void ProcessInput(__attribute__((unused)) int32_t p1ax, int32_t p1ay, __attribute__((unused)) int8_t p1bl,
-                  __attribute__((unused)) int8_t p1br, __attribute__((unused)) int8_t p1bu,
-                  __attribute__((unused)) int8_t p1bd, __attribute__((unused)) int32_t p2ax, int32_t p2ay,
-                  __attribute__((unused)) int8_t p2bl, __attribute__((unused)) int8_t p2br,
-                  __attribute__((unused)) int8_t p2bu, __attribute__((unused)) int8_t p2bd)
-{
-    if (p1ay < 512 - DEAD_ZONE || 512 + DEAD_ZONE < p1ay)
-    {
-        pong->paddleLocL += ((512 - p1ay) * 3);
-        if (pong->paddleLocL < 0)
-        {
-            pong->paddleLocL = 0;
-        }
-        else if (pong->paddleLocL > 12288)
-        {
-            pong->paddleLocL = 12288;
-        }
-    }
-
-    if (p2ay < 512 - DEAD_ZONE || 512 + DEAD_ZONE < p2ay)
-    {
-        pong->paddleLocR += ((512 - p2ay) * 3);
-        if (pong->paddleLocR < 0)
-        {
-            pong->paddleLocR = 0;
-        }
-        else if (pong->paddleLocR > 12288)
-        {
-            pong->paddleLocR = 12288;
-        }
-    }
+    drawCircleFilled((pong->ballLoc.x) >> DECIMAL_BITS, (pong->ballLoc.y) >> DECIMAL_BITS, BALL_RADIUS >> DECIMAL_BITS,
+                     c555);
+    drawRect(0, (pong->paddleLocL >> DECIMAL_BITS), PADDLE_WIDTH >> DECIMAL_BITS,
+             (pong->paddleLocL + PADDLE_HEIGHT) >> DECIMAL_BITS, c050);
+    drawRect((FIELD_WIDTH - PADDLE_WIDTH) >> DECIMAL_BITS, (pong->paddleLocR) >> DECIMAL_BITS,
+             FIELD_WIDTH >> DECIMAL_BITS, (pong->paddleLocR + PADDLE_HEIGHT) >> DECIMAL_BITS, c005);
 }
 
 /**
  * This function is called whenever an ESP-NOW packet is received.
  *
  * @param esp_now_info Information about the transmission, including The MAC addresses
- * @param data     A pointer to the data received
- * @param len      The length of the data received
- * @param rssi     The RSSI for this packet, from 1 (weak) to ~90 (touching)
+ * @param data A pointer to the data received
+ * @param len The length of the data received
+ * @param rssi The RSSI for this packet, from 1 (weak) to ~90 (touching)
  */
 static void pongEspNowRecvCb(const esp_now_recv_info_t* esp_now_info, const uint8_t* data, uint8_t len, int8_t rssi)
 {
@@ -424,7 +393,7 @@ static void pongEspNowRecvCb(const esp_now_recv_info_t* esp_now_info, const uint
  * This will be called after calling espNowSend()
  *
  * @param mac_addr The MAC address which the data was sent to
- * @param status   The status of the transmission
+ * @param status The status of the transmission
  */
 static void pongEspNowSendCb(const uint8_t* mac_addr, esp_now_send_status_t status)
 {
