0aa93bb976254c1d7d23450aefbea0d37c5b4acb
AEFeinstein <gelakinetic@gmail.com>
2023-03-16 00:43:51 -0400

    Mock more IDF stuff for sleep & USB

---
+++ emulator/idf-inc/soc/rtc_cntl_reg.h
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2017-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef _SOC_RTC_CNTL_REG_H_
+#define _SOC_RTC_CNTL_REG_H_
+
+#define REG_WRITE(r, v)
+
+#define RTC_CNTL_OPTION1_REG 0
+
+#endif /*_SOC_RTC_CNTL_REG_H_ */
