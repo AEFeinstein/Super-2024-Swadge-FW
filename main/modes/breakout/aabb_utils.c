b2baf8e608bacfd121b199ef98e48a04a98be8a4
JVeg199X <swadgedev@github.com>
2023-07-04 10:18:38 -0400

    Add aabb utils

---
+++ main/modes/breakout/aabb_utils.c
@@ -0,0 +1,56 @@
+//==============================================================================
+// Includes
+//==============================================================================
+
+#include "aabb_utils.h"
+//#include <stdint.h>
+#include "fill.h"
+
+//==============================================================================
+// Functions
+//==============================================================================
+
+/**
+ * @brief Draw a box
+ *
+ * @param disp The display to draw the box to
+ * @param box The box to draw
+ * @param color The color of the box to draw
+ * @param isFilled true to draw a filled box, false to draw an outline
+ * @param scalingFactor The scaling factor to apply before drawing the box
+ */
+void drawBox(box_t box, paletteColor_t color, bool isFilled, int32_t scalingFactor)
+{
+    if(isFilled)
+    {
+        fillDisplayArea(box.x0 >> scalingFactor,
+                        box.y0 >> scalingFactor,
+                        box.x1 >> scalingFactor,
+                        box.y1 >> scalingFactor,
+                        color);
+    }
+    else
+    {
+        /*plotRect(box.x0 >> scalingFactor,
+                 box.y0 >> scalingFactor,
+                 box.x1 >> scalingFactor,
+                 box.y1 >> scalingFactor,
+                 color);*/
+    }
+}
+
+/**
+ * @brief
+ *
+ * @param box0 A box to check for collision
+ * @param box1 The other box to check for collision
+ * @param scalingFactor The factor to scale the boxes by before checking for collision
+ * @return true if the boxes collide, false if they do not
+ */
+bool boxesCollide(box_t box0, box_t box1, int32_t scalingFactor)
+{
+    return (box0.x0 >> scalingFactor) < (box1.x1 >> scalingFactor) &&
+           (box0.x1 >> scalingFactor) > (box1.x0 >> scalingFactor) &&
+           (box0.y0 >> scalingFactor) < (box1.y1 >> scalingFactor) &&
+           (box0.y1 >> scalingFactor) > (box1.y0 >> scalingFactor);
+}
