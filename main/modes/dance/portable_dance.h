9cbe539abce7ba20354998e913b1eed7cb7eab19
AEFeinstein <gelakinetic@gmail.com>
2023-08-14 22:49:07 -0400

    Port LED animations to this Swadge Mock esp_light_sleep_start()

---
+++ main/modes/dance/portable_dance.h
@@ -0,0 +1,36 @@
+#ifndef _PORTABLE_DANCE_H_
+#define _PORTABLE_DANCE_H_
+
+#include "swadge2024.h"
+#include "mode_dance.h"
+
+typedef struct
+{
+    const ledDanceArg* dance;
+    bool enable;
+} ledDanceOpt_t;
+
+typedef struct
+{
+    // List of dances to loop through
+    ledDanceOpt_t* dances;
+
+    // Set when dance needs to be reset
+    bool resetDance;
+
+    uint8_t danceIndex;
+
+    // If non-NULL, the dance index will be saved/loaded from this nvs key
+    const char* nvsKey;
+} portableDance_t;
+
+portableDance_t* initPortableDance(const char* nvsKey);
+void freePortableDance(portableDance_t* dance);
+void portableDanceMainLoop(portableDance_t* dance, int64_t elapsedUs);
+bool portableDanceSetByName(portableDance_t* dance, const char* danceName);
+void portableDanceNext(portableDance_t* dance);
+void portableDancePrev(portableDance_t* dance);
+bool portableDanceDisableDance(portableDance_t* dance, const char* danceName);
+const char* portableDanceGetName(portableDance_t* dance);
+
+#endif
