18a11e7de7efff84f4754c0980e3a4622cb01f27
Dylan Whichard <dylan@whichard.com>
2023-08-19 11:24:45 -0700

    Add circular touchpad extension

--- emulator/src/extensions/touch/ext_touch.c
+++ emulator/src/extensions/touch/ext_touch.c
@@ -4,6 +4,7 @@
 
 #include "ext_touch.h"
 #include "macros.h"
+#include "trigonometry.h"
 
 #include <stddef.h>
 #include <stdbool.h>
@@ -22,23 +23,40 @@
 
 // A nice gray for inactive touchpads
 #define COLOR_TOUCHPAD_INACTIVE 0x337722FF
-// Some sort of dark yellow, I think, for active touchpads
-#define COLOR_TOUCHPAD_ACTIVE   0x88FF00FF
 // Like almost gray, but a bit green?
 #define COLOR_TOUCHPAD_SEP      0x336633FF
+// Some sort of dark yellow, I think, for active touch
+#define COLOR_TOUCH   0x88FF00FF
+// Some light gray for hover, maybe transparent??
+#define COLOR_HOVER   0x44884488
 
 #define TOUCHPAD_SPACING 4
-#define TOUCHPAD_COUNT   5
 
-#define TOUCH_STRIP
-//#define TOUCH_STICK
+/**
+ * @brief Macro for generating vertices for a polygon approximating a circle with `tris` sides
+ *
+ * @param buf  A buffer of RDPoint[] with at least `tris` of space
+ * @param tris The number of triangles to use to approximate a circle. Best if `(360 % tris) == 0`
+ * @param xo   The X-offset of the center of the circle
+ * @param yo   The Y-offset of the center of the circle
+ * @param r    The radius of the circle
+ */
+#define CALC_CIRCLE_POLY(buf, tris, xo, yo, r)                           \
+    do                                                                   \
+    {                                                                    \
+        for (int i = 0; i < (tris); i++)                                 \
+        {                                                                \
+            buf[i].x = (xo) + getCos1024(i * 360 / (tris)) * (r) / 1024; \
+            buf[i].y = (yo) + getSin1024(i * 360 / (tris)) * (r) / 1024; \
+        }                                                                \
+    } while (false)
 
 //==============================================================================
 // Function Prototypes
 //==============================================================================
 
 static bool isInTouchBounds(int32_t x, int32_t y);
-static void calculateTouch(int32_t x, int32_t y, int16_t* centroid, int16_t* intensity);
+static void calculateTouch(int32_t x, int32_t y, int16_t* angle, int16_t* radius, int16_t* intensity);
 static bool updateTouch(int32_t x, int32_t y, bool clicked);
 
 static bool touchInit(emuArgs_t* emuArgs);
@@ -62,7 +80,8 @@ typedef struct
     int32_t lastHoverX;
     int32_t lastHoverY;
 
-    int16_t lastTouchCentroid;
+    int16_t lastTouchAngle;
+    int16_t lastTouchRadius;
     int16_t lastTouchIntensity;
     buttonBit_t lastTouchButtons;
 
@@ -79,8 +98,8 @@ typedef struct
 emuCallback_t touchEmuCallback = {
     .name            = "touch",
     .paneLocation    = PANE_BOTTOM,
-    .minPaneW        = 192,
-    .minPaneH        = 64,
+    .minPaneW        = 128,
+    .minPaneH        = 128,
     .fnInitCb        = touchInit,
     .fnPreFrameCb    = NULL,
     .fnPostFrameCb   = NULL,
@@ -98,22 +117,36 @@ static emuTouch_t emuTouch = {0};
 
 static bool isInTouchBounds(int32_t x, int32_t y)
 {
-    return emuTouch.paneX <= x && x <= emuTouch.paneX + emuTouch.paneW
-           && emuTouch.paneY <= y && y <= emuTouch.paneY + emuTouch.paneH;
+    int32_t ringSize = MIN(emuTouch.paneW, emuTouch.paneH);
+    int32_t xMin = emuTouch.paneX + (emuTouch.paneW - ringSize) / 2;
+    int32_t yMin = emuTouch.paneY + (emuTouch.paneH - ringSize) / 2;
+
+    return xMin <= x && x <= xMin + ringSize
+           && yMin <= y && y <= yMin + ringSize;
 }
 
-static void calculateTouch(int32_t x, int32_t y, int16_t* centroid, int16_t* intensity)
+static void calculateTouch(int32_t x, int32_t y, int16_t* angle, int16_t* radius, int16_t* intensity)
 {
+    int32_t ringSize = MIN(emuTouch.paneW, emuTouch.paneH);
+
     // Clamp the touch offset to within the pane bounds, in case we're still clicking but left the box
     uint32_t xOffset = (x < emuTouch.paneX) ? 0 : MIN(x - emuTouch.paneX, emuTouch.paneW);
     uint32_t yOffset = (y < emuTouch.paneY) ? 0 : MIN(y - emuTouch.paneY, emuTouch.paneH);
 
-    // Just do a linear interpolation to get the magnitude and centroid
+    // Get the x/y offsets normalized to [-1024, 1023]
+    int32_t xCenterOff = CLAMP(1024 * ((int32_t)xOffset - ((int32_t)emuTouch.paneW / 2)) / (ringSize / 2), -1023, 1023);
+    int32_t yCenterOff = CLAMP(1024 * -((int32_t)yOffset - ((int32_t)emuTouch.paneH / 2)) / (ringSize / 2), -1023, 1023);
 
-    // The touch controls go right-to-left, I think? TODO compare with real swadge
-    *centroid = CLAMP(1024 - xOffset * 1024 / emuTouch.paneX, 0, 1023);
-    // Towards the top of the pane, higher intensity
-    *intensity = CLAMP(1024 - (yOffset * 1024 / emuTouch.paneY), 0, 1023);
+    int16_t rawAngle = getAtan2(yCenterOff, xCenterOff); //(int16_t)(360 * atan2(1.0 * yCenterOff, 1.0 * xCenterOff) / (2 * M_PI));
+
+    // convert phi to be in [0, 360) instead of [-180, 180]
+    *angle = ((rawAngle % 360) + 360) % 360;
+
+    // Calculate the radius from the offsets
+    *radius = CLAMP((xCenterOff * xCenterOff + yCenterOff * yCenterOff) / 1024, 0, 1023);
+
+    // The intensity changes elsewhere so just grab it
+    *intensity = CLAMP(emuTouch.lastTouchIntensity, 0, 1023);
 }
 
 /**
@@ -169,8 +202,8 @@ static bool updateTouch(int32_t x, int32_t y, bool clicked)
             emuTouch.lastClickY = y;
 
             // Calculate the new touch values and save them
-            calculateTouch(x, y, &(emuTouch.lastTouchCentroid), &(emuTouch.lastTouchIntensity));
-            emulatorSetTouchCentroid(emuTouch.lastTouchCentroid, emuTouch.lastTouchIntensity);
+            calculateTouch(x, y, &(emuTouch.lastTouchAngle), &(emuTouch.lastTouchRadius), &(emuTouch.lastTouchIntensity));
+            emulatorSetTouchAngleRadius(emuTouch.lastTouchAngle, emuTouch.lastTouchRadius, emuTouch.lastTouchIntensity);
         }
         else
         {
@@ -178,7 +211,7 @@ static bool updateTouch(int32_t x, int32_t y, bool clicked)
             emuTouch.dragging = false;
 
             // ... and reset the touch centroid to 0, since we're not touching the touchpad
-            emulatorSetTouchCentroid(0, 0);
+            emulatorSetTouchAngleRadius(0, 0, 0);
         }
 
         // Consume this mouse event, since did something with it
@@ -204,6 +237,8 @@ static bool touchInit(emuArgs_t* emuArgs)
     emuTouch.lastClickX = 0;
     emuTouch.lastClickY = 0;
 
+    emuTouch.lastTouchIntensity = 512;
+
     return emuArgs->emulateTouch;
 }
 
@@ -214,8 +249,21 @@ static bool touchMouseMove(int32_t x, int32_t y, mouseButton_t buttonMask)
 
 static bool touchMouseButton(int32_t x, int32_t y, mouseButton_t button, bool down)
 {
-    // Return true only for left-button events that actually generate a change
-    return (button == EMU_MOUSE_LEFT) && updateTouch(x, y, down);
+    if (button == EMU_MOUSE_LEFT)
+    {
+        return updateTouch(x, y, down);
+    }
+    else if (button == EMU_SCROLL_UP && isInTouchBounds(x, y))
+    {
+        emuTouch.lastTouchIntensity = CLAMP(emuTouch.lastTouchIntensity << 1, 1, 1023);
+        return true;
+    }
+    else if (button == EMU_SCROLL_DOWN && isInTouchBounds(x, y))
+    {
+        emuTouch.lastTouchIntensity = CLAMP((emuTouch.lastTouchIntensity + 1) >> 1, 1, 1023);
+        return true;
+    }
+    return false;
 }
 
 static void touchRender(uint32_t winW, uint32_t winH, uint32_t paneW, uint32_t paneH, uint32_t paneX, uint32_t paneY)
@@ -226,35 +274,46 @@ static void touchRender(uint32_t winW, uint32_t winH, uint32_t paneW, uint32_t p
     emuTouch.paneH = paneH;
     emuTouch.paneY = paneY;
 
-    // Get the current button state and filter out the touch values
-    buttonBit_t buttonState = emulatorGetButtonState();
-    buttonBit_t touchBits = buttonState & (TB_0 | TB_1 | TB_2 | TB_3 | TB_4);
+    // outer radius of the "ring" touchpad
+    uint32_t outerR = MIN(paneW, paneH) / 2;
 
-    for (int i = 0; i < TOUCHPAD_COUNT; i++)
+    // radius of the center touchpad
+    uint32_t innerR = outerR / 3;
+
+    // radius of the circle separating the
+    uint32_t spaceR = innerR + TOUCHPAD_SPACING;
+
+    uint32_t centerX = paneX + paneW / 2;
+    uint32_t centerY = paneY + paneH / 2;
+
+    RDPoint points[32] = {0};
+
+    // Draw the outer ring touchpad
+    CALC_CIRCLE_POLY(points, ARRAY_SIZE(points), centerX, centerY, outerR);
+    CNFGColor(COLOR_TOUCHPAD_INACTIVE);
+    CNFGTackPoly(points, ARRAY_SIZE(points));
+
+    // Draw the space between the center and outer touchpads
+    CALC_CIRCLE_POLY(points, ARRAY_SIZE(points), centerX, centerY, spaceR);
+    CNFGColor(COLOR_TOUCHPAD_SEP);
+    CNFGTackPoly(points, ARRAY_SIZE(points));
+
+    // Draw the center touchpad
+    CALC_CIRCLE_POLY(points, ARRAY_SIZE(points), centerX, centerY, innerR);
+    CNFGColor(COLOR_TOUCHPAD_INACTIVE);
+    CNFGTackPoly(points, ARRAY_SIZE(points));
+
+    // Draw a circle for the touch location
+    // If the user is dragging anywhere, draw the circle at the location from the actual touch data
+    // If the user is hovering within the touch area, draw a lighter circle underneath the mouse
+    if (emuTouch.dragging || (emuTouch.hovering && isInTouchBounds(emuTouch.lastHoverX, emuTouch.lastHoverY)))
     {
-        bool first = (i == 0);
-        bool last = ((i + 1) == TOUCHPAD_COUNT);
+        int32_t x = emuTouch.dragging ? (int32_t)centerX + getCos1024(emuTouch.lastTouchAngle) * emuTouch.lastTouchRadius * (int32_t)outerR / 1024 / 1024 : emuTouch.lastHoverX;
+        int32_t y = emuTouch.dragging ? (int32_t)centerY - getSin1024(emuTouch.lastTouchAngle) * emuTouch.lastTouchRadius * (int32_t)outerR / 1024 / 1024 : emuTouch.lastHoverY;
 
-        // Horizontal space available for pads after taking dividers into account
-        uint32_t padSpace = (paneW - TOUCHPAD_SPACING * (TOUCHPAD_COUNT - 1));
-
-        // Calculate the pad X first
-        int padX = paneX + i * (TOUCHPAD_SPACING + padSpace) / TOUCHPAD_COUNT;
-
-        // Calculate the pad width by first calculating the next pad X and subtracting this pad's X from that
-        // Then calculate the pad width as the distance to the next pad X (minus spacing)
-        int padW = paneX + (i + 1) * (TOUCHPAD_SPACING + padSpace) / TOUCHPAD_COUNT - (last ? TOUCHPAD_SPACING : 0) - padX;
-
-        if (!first)
-        {
-            // Draw the divider to the left of this touchpad
-            CNFGColor(COLOR_TOUCHPAD_SEP);
-            CNFGTackRectangle(padX - TOUCHPAD_SPACING, paneY, padX - 1, paneY + paneH);
-        }
-
-        // Draw the touchpad now
-        // Set the color based on whether the touchpad is considered active
-        CNFGColor((touchBits & (TB_4 >> i)) ? COLOR_TOUCHPAD_ACTIVE : COLOR_TOUCHPAD_INACTIVE);
-        CNFGTackRectangle(padX, paneY, padX + padW, paneY + paneH);
+        // Draw the touch circle
+        CALC_CIRCLE_POLY(points, ARRAY_SIZE(points), x, y, emuTouch.lastTouchIntensity * innerR / 1024);
+        CNFGColor(emuTouch.dragging ? COLOR_TOUCH : COLOR_HOVER);
+        CNFGTackPoly(points, ARRAY_SIZE(points));
     }
 }
