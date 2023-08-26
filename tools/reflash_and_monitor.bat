f39d3667a7f96a93562a2b425030cfb8fa3d63a2
AEFeinstein <gelakinetic@gmail.com>
2023-08-26 10:41:15 -0400

    Fix reflash_and_monitor.bat for my machine Add .gitignore for bootload_reboot_stub Use better makefile vars for calling $(MAKE) Add documentation

--- tools/reflash_and_monitor.bat
+++ tools/reflash_and_monitor.bat
@@ -1,7 +1,16 @@
-make -C tools/reboot_into_bootloader || true
+REM Make sure the tools are built first
+make -C .\tools\reboot_into_bootloader\ reboot_swadge_into_bootloader.exe
+make -C .\tools\bootload_reboot_stub\ bootload_reboot_stub.bin
+make -C .\tools\swadgeterm\
+
+REM Reboot the swadge into bootloader mode
+.\tools\reboot_into_bootloader\reboot_swadge_into_bootloader.exe || true
 sleep 1.2
+REM Flash the swadge
 idf.py flash
 sleep 1.2
-make -C tools/bootload_reboot_stub reboot
+REM Reboot the swadge again
+esptool.exe --before no_reset --after no_reset load_ram tools\bootload_reboot_stub\bootload_reboot_stub.bin || true
 sleep 2.5
-make -C tools/swadgeterm monitor
+REM Open a serial monitor
+make -C tools\swadgeterm monitor
