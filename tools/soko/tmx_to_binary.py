0e845b9a55f49c8f38b8206a8c613d2c8664fbdf
Gavin Granath <granath.gavin@gmail.com>
2023-08-26 22:54:26 -0400

    Progress Commit backing up incomplete python script for tile export

--- tools/soko/tmx_to_binary.py
+++ tools/soko/tmx_to_binary.py
@@ -2,6 +2,10 @@ import tkinter as tk
 from tkinter import filedialog
 from xml.dom.minidom import parse,parseString
 
+SIG_BYTE_SIMPLE_SEQUENCE = 200 #This byte will capture long strings of bytes in compact form. Example [12][12][12]...[12] => [200][#][12] where # is number of 12 tiles
+
+def insert_position(position, sourceList, insertionList):
+    return sourceList[:position] + insertionList + sourceList[position:]
 
 root = tk.Tk()
 root.withdraw()
@@ -22,7 +26,8 @@ scrub = "".join(dataText.split()) #Remove all residual whitespace in data block
 scrub2 = [int(i) for i in scrub.split(",")] #Convert all tileIDs to int.
 print(scrub)
 print(scrub2)
-scrub3 = list([mapHeaderWidth,mapHeaderHeight]) + scrub2
+#scrub3 = list([mapHeaderWidth,mapHeaderHeight]) + scrub2
+scrub3 = insert_position(0,scrub2,list([mapHeaderWidth,mapHeaderHeight]))
 print(scrub3)
 rawBytes = bytearray(scrub3)
 rawBytesImmut = bytes(rawBytes)
@@ -30,3 +35,5 @@ outfile_path = "".join([file_path.split(".")[0],".bin"])
 print(outfile_path)
 with open(outfile_path,"wb") as binary_file:
     binary_file.write(rawBytesImmut)
+
+
