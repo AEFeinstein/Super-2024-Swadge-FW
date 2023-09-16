33b01c68b37119f39f7b58d683261347e68d821d
Dylan Whichard <dylan@whichard.com>
2023-09-15 20:52:07 -0700

    Fix warning

--- main/modes/mfpaint/wheel_menu.c
+++ main/modes/mfpaint/wheel_menu.c
@@ -270,7 +270,7 @@ void drawWheelMenu(menu_t* menu, wheelMenuRenderer_t* renderer, int64_t elapsedU
             // Copy the real label first if we're using a static string
             if (label != buffer)
             {
-                strncpy(buffer, sizeof(buffer) - 1, label);
+                strncpy(buffer, label, sizeof(buffer));
                 label = buffer;
             }
 
