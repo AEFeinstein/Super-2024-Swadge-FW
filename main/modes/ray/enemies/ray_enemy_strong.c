8c49e27f4c6f2d31bf35637fab62ad748d68f927
gelakinetic <gelakinetic@gmail.com>
2023-10-11 00:32:19 -0400

    Make charge enemy die to charge

--- main/modes/ray/enemies/ray_enemy_strong.c
+++ main/modes/ray/enemies/ray_enemy_strong.c
@@ -22,5 +22,9 @@ void rayEnemyStrongMoveAnimate(ray_t* ray, rayEnemy_t* enemy, uint32_t elapsedUs
  */
 bool rayEnemyStrongGetShot(ray_t* ray, rayEnemy_t* enemy, rayMapCellType_t bullet)
 {
+    if (OBJ_BULLET_CHARGE == bullet)
+    {
+        return true;
+    }
     return false;
 }
