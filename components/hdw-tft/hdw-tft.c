012f4ff2216b59231e6e1ae0b38f243f046f81dd
Dylan Whichard <dylan@whichard.com>
2023-10-17 21:22:46 -0700

    Actually pass brightness when initializing TFT

--- components/hdw-tft/hdw-tft.c
+++ components/hdw-tft/hdw-tft.c
@@ -115,7 +115,7 @@ static ledc_timer_t tftLedcTimer;
 static ledc_channel_t tftLedcChannel;
 static gpio_num_t tftBacklightPin;
 static bool tftBacklightIsPwm;
-static uint8_t tftBacklightIntensity = MAX_TFT_BRIGHTNESS;
+static uint8_t tftBacklightIntensity;
 
 static esp_lcd_panel_io_handle_t tft_io_handle = NULL;
 
@@ -261,6 +261,7 @@ void initTFT(spi_host_device_t spiHost, gpio_num_t sclk, gpio_num_t mosi, gpio_n
 #endif
 
     // Enable the backlight
+    setTFTBacklightBrightness(brightness);
     enableTFTBacklight();
 
     if (NULL == pixels)
