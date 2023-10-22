357c840a10eef6770a66800ff7cddbf44410a302
Dylan Whichard <dylan@whichard.com>
2023-10-21 17:04:16 -0700

    Add missing include guard

--- main/asset_loaders/spiffs_model.h
+++ main/asset_loaders/spiffs_model.h
@@ -1,6 +1,11 @@
+#ifndef _SPIFFS_MODEL_H_
+#define _SPIFFS_MODEL_H_
+
 #include "model.h"
 
 #include <stdbool.h>
 
 bool loadModel(const char* name, model_t* model, bool useSpiRam);
 void freeModel(model_t* model);
+
+#endif
