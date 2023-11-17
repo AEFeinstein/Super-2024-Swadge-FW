3ab86374156e61d01a8428fefe5985236d2c1ec8
Brycey92 <bbrowner@optonline.net>
2023-11-17 02:00:52 -0500

    Dynamically load Jukebox songs

--- main/modes/jukebox/jukebox.c
+++ main/modes/jukebox/jukebox.c
@@ -47,21 +47,21 @@ typedef enum
 } jukeboxScreen_t;
 
 /*==============================================================================
- * Prototypes
- *============================================================================*/
-
-void jukeboxEnterMode(void);
-void jukeboxExitMode(void);
-void jukeboxButtonCallback(buttonEvt_t* evt);
-void jukeboxMainLoop(int64_t elapsedUs);
-void jukeboxMainMenuCb(const char* label, bool selected, uint32_t settingVal);
-void jukeboxBackgroundDrawCb(int16_t x, int16_t y, int16_t w, int16_t h, int16_t up, int16_t upNum);
-
-void setJukeboxMainMenu(bool resetPos);
-
-/*==============================================================================
  * Structs
  *============================================================================*/
+
+typedef struct
+{
+    char* name;
+    song_t song;
+} jukeboxSong;
+
+typedef struct
+{
+    const char* categoryName;
+    uint8_t numSongs;
+    jukeboxSong* songs;
+} jukeboxCategory;
 
 typedef struct
 {
@@ -78,10 +78,14 @@ typedef struct
     wsg_t arrow;
     wsg_t jukeboxSprite;
 
-    // Midis
-    song_t pongHit1;
-    song_t pongHit2;
-    song_t breakoutBgm;
+    // Music Midis and Jukebox Structs
+    jukeboxCategory* musicCategories;
+    uint8_t numMusicCategories;
+
+    // SFX Midis and Jukebox Structs
+    jukeboxCategory* tunernomeSfxCategory;
+    jukeboxCategory* sfxCategories;
+    uint8_t numSfxCategories;
 
     // Touch
     int32_t touchAngle;
@@ -102,18 +106,18 @@ typedef struct
 
 jukebox_t* jukebox;
 
-typedef struct
-{
-    char* name;
-    song_t* song;
-} jukeboxSong;
+/*==============================================================================
+ * Prototypes
+ *============================================================================*/
 
-typedef struct
-{
-    const char* categoryName;
-    uint8_t numSongs;
-    const jukeboxSong* songs;
-} jukeboxCategory;
+void jukeboxEnterMode(void);
+void jukeboxExitMode(void);
+void jukeboxButtonCallback(buttonEvt_t* evt);
+void jukeboxMainLoop(int64_t elapsedUs);
+void jukeboxMainMenuCb(const char* label, bool selected, uint32_t settingVal);
+void jukeboxBackgroundDrawCb(int16_t x, int16_t y, int16_t w, int16_t h, int16_t up, int16_t upNum);
+
+void jukeboxFreeCategories(jukeboxCategory** categoryArray, uint8_t numCategories);
 
 /*==============================================================================
  * Variables
@@ -155,40 +159,6 @@ static const char str_play[]       = ": Play";
 
 // Arrays
 
-// TODO: get these midis from Dac
-// static const jukeboxSong jukeboxMusic[] =
-// {
-//     {.name = "Hot Rod", .song = &hotrod},
-//     {.name = "Fauxrio Kart", .song = &fauxrio_kart},
-//     {.name = "The Lake", .song = &the_lake},
-//     {.name = "Ya like jazz?", .song = &herecomesthesun},
-//     {.name = "Banana", .song = &bananaphone},
-// };
-
-static jukeboxSong breakoutMusic[] = {
-    {.name = "???", .song = NULL}, // TODO
-};
-
-static const jukeboxCategory musicCategories[] = {
-    //{.categoryName = "Jukebox", .numSongs = ARRAY_SIZE(jukeboxMusic), .songs = jukeboxMusic},
-    {.categoryName = breakoutMode.modeName, .numSongs = ARRAY_SIZE(breakoutMusic), .songs = breakoutMusic},
-};
-
-static const jukeboxSong breakoutSfx[] = {
-    {.name = "Primary", .song = &metronome_primary},
-    {.name = "Secondary", .song = &metronome_secondary},
-};
-
-static const jukeboxSong tunernomeSfx[] = {
-    {.name = "Primary", .song = &metronome_primary},
-    {.name = "Secondary", .song = &metronome_secondary},
-};
-
-static const jukeboxCategory sfxCategories[] = {
-    {.categoryName = breakoutMode.modeName, .numSongs = ARRAY_SIZE(breakoutSfx), .songs = breakoutSfx},
-    {.categoryName = "Tunernome", .numSongs = ARRAY_SIZE(tunernomeSfx), .songs = tunernomeSfx},
-};
-
 /*============================================================================
  * Functions
  *==========================================================================*/
@@ -198,30 +168,137 @@ static const jukeboxCategory sfxCategories[] = {
  */
 void jukeboxEnterMode()
 {
-    // Allocate zero'd memory for the mode
+    ///// Allocate zero'd memory for the mode /////
     jukebox = calloc(1, sizeof(jukebox_t));
 
-    // Load fonts
+    ///// Load fonts /////
     loadFont("ibm_vga8.font", &jukebox->ibm_vga8, false);
     loadFont("radiostars.font", &jukebox->radiostars, false);
     loadFont("logbook.font", &jukebox->logbook, false);
 
-    // Load images
+    ///// Load images /////
     loadWsg("arrow12.wsg", &jukebox->arrow, false);
     loadWsg("jukebox.wsg", &jukebox->jukeboxSprite, false);
 
-    // Load midis
-    loadSong("gmcc.sng", &jukebox->breakoutBgm, false); // TODO: use correct sng
-    jukebox->breakoutBgm.shouldLoop = true;
-    breakoutMusic[0].song           = &jukebox->breakoutBgm;
+    ///// Load music midis /////
 
-    // Initialize menu
+    // Initialize array of music categories
+    int catIdx = 0;
+    jukebox->numMusicCategories = 2;
+    // TODO: change this number   ^ as categories (modes) with music are added
+    jukebox->musicCategories = calloc(jukebox->numMusicCategories, sizeof(jukeboxCategory));
+
+    // Breakout category
+    int songIdx = 0;
+    jukebox->musicCategories[catIdx].categoryName = breakoutMode.modeName;
+    jukebox->musicCategories[catIdx].numSongs = 1;
+    jukebox->musicCategories[catIdx].songs = calloc(jukebox->musicCategories[catIdx].numSongs, sizeof(jukeboxSong));
+
+    // Breakout songs
+
+    jukebox->musicCategories[catIdx].songs[songIdx].name = "BGM";
+    loadSong("gmcc.sng", &jukebox->musicCategories[catIdx].songs[songIdx].song, false); // TODO: use correct sng once it's done being composed
+    jukebox->musicCategories[catIdx].songs[songIdx].song.shouldLoop = true;
+    songIdx++;
+
+    catIdx++;
+
+    // Jukebox category
+    songIdx = 0;
+    jukebox->musicCategories[catIdx].categoryName = jukeboxMode.modeName;
+    jukebox->musicCategories[catIdx].numSongs = 5;
+    jukebox->musicCategories[catIdx].songs = calloc(jukebox->musicCategories[catIdx].numSongs, sizeof(jukeboxSong));
+
+    // Jukebox songs
+
+    // TODO: get these midis from Dac
+    // jukebox->musicCategories[catIdx].songs[songIdx].name = "Hot Rod";
+    // jukebox->musicCategories[catIdx].songs[songIdx].song = &jukebox->hotrod;
+    // songIdx++;
+    
+    // jukebox->musicCategories[catIdx].songs[songIdx].name = "Fauxrio Kart";
+    // jukebox->musicCategories[catIdx].songs[songIdx].song = &jukebox->fauxrio_kart;
+    // songIdx++;
+
+    // jukebox->musicCategories[catIdx].songs[songIdx].name = "The Lake";
+    // jukebox->musicCategories[catIdx].songs[songIdx].song = &jukebox->the_lake;
+    // songIdx++;
+
+    // jukebox->musicCategories[catIdx].songs[songIdx].name = "Ya like jazz?";
+    // jukebox->musicCategories[catIdx].songs[songIdx].song = &jukebox->herecomesthesun;
+    // songIdx++;
+
+    // jukebox->musicCategories[catIdx].songs[songIdx].name = "Banana";
+    // jukebox->musicCategories[catIdx].songs[songIdx].song = &jukebox->bananaphone;
+    // songIdx++;
+
+    catIdx++;
+
+    // TODO: remainder of categories (modes) with music
+
+    ///// Load SFX midis /////
+
+    // Initialize array of music categories
+    catIdx = 0;
+    jukebox->numSfxCategories = 2;
+    // TODO: change this number ^ as categories (modes) with SFX are added
+    jukebox->sfxCategories = calloc(2, sizeof(jukeboxCategory));
+
+    // Breakout category
+    songIdx = 0;
+    jukebox->sfxCategories[catIdx].categoryName = breakoutMode.modeName;
+    jukebox->sfxCategories[catIdx].numSongs = 10;
+    jukebox->sfxCategories[catIdx].songs = calloc(jukebox->sfxCategories[catIdx].numSongs, sizeof(jukeboxSong));
+
+    // Breakout SFX
+
+    jukebox->sfxCategories[catIdx].songs[songIdx].name = "Bounce";
+    loadSong("sndBounce.sng", &jukebox->sfxCategories[catIdx].songs[songIdx].song, false);
+    jukebox->sfxCategories[catIdx].songs[songIdx].song.shouldLoop = false;
+    songIdx++;
+
+    jukebox->sfxCategories[catIdx].songs[songIdx].name = "Break";
+    loadSong("sndBreak.sng", &jukebox->sfxCategories[catIdx].songs[songIdx].song, false);
+    jukebox->sfxCategories[catIdx].songs[songIdx].song.shouldLoop = false;
+    songIdx++;
+
+    // TODO: remainder of breakout SFX
+
+    catIdx++;
+    
+    // Tunernome category
+    songIdx = 0;
+    jukebox->sfxCategories[catIdx].categoryName = tunernomeMode.modeName;
+    jukebox->sfxCategories[catIdx].numSongs = 2;
+    jukebox->sfxCategories[catIdx].songs = calloc(jukebox->sfxCategories[catIdx].numSongs, sizeof(jukeboxSong));
+    // Special handling so we can easily clean this up later
+    jukebox->tunernomeSfxCategory = &jukebox->sfxCategories[catIdx];
+
+    // Tunernome SFX
+
+    // Special handling since these SFX are defined in code, not loaded from .sng files
+    jukebox->sfxCategories[catIdx].songs[songIdx].name = "Primary";
+    memcpy(&jukebox->sfxCategories[catIdx].songs[songIdx].song, &metronome_primary, sizeof(song_t));
+    songIdx++;
+
+    // Special handling since these SFX are defined in code, not loaded from .sng files
+    jukebox->sfxCategories[catIdx].songs[songIdx].name = "Secondary";
+    memcpy(&jukebox->sfxCategories[catIdx].songs[songIdx].song, &metronome_secondary, sizeof(song_t));
+    songIdx++;
+
+    catIdx++;
+
+    // TODO: remainder of categories (modes) with SFX
+
+    ///// Initialize menu /////
     jukebox->menu                = initMenu(str_jukebox, &jukeboxMainMenuCb);
     jukebox->menuLogbookRenderer = initMenuLogbookRenderer(&jukebox->logbook);
     addSingleItemToMenu(jukebox->menu, str_bgm);
     addSingleItemToMenu(jukebox->menu, str_sfx);
     addSingleItemToMenu(jukebox->menu, str_exit);
     jukebox->screen = JUKEBOX_MENU;
+
+    ///// Initialize portable dances /////
 
     jukebox->portableDances = initPortableDance(NULL);
 
@@ -249,17 +326,29 @@ void jukeboxExitMode(void)
 {
     bzrStop(true);
 
+    // Free fonts
     freeFont(&jukebox->ibm_vga8);
     freeFont(&jukebox->radiostars);
     freeFont(&jukebox->logbook);
 
+    // Free images
     freeWsg(&jukebox->arrow);
     freeWsg(&jukebox->jukeboxSprite);
 
-    freeSong(&jukebox->breakoutBgm);
-    freeSong(&jukebox->pongHit1);
-    freeSong(&jukebox->pongHit2);
-    // TODO: free allocated songs
+    // Free allocated music midis, song arrays, and category arrays
+    jukeboxFreeCategories(&jukebox->musicCategories, jukebox->numMusicCategories);
+
+    // Free allocated SFX midis, song arrays, and category arrays
+
+    // Tunernome's SFX are declared in code, not loaded as .sng files, so we need to prevent them from being freed
+    for(int i = 0; i < jukebox->tunernomeSfxCategory->numSongs; i++)
+    {
+        memset(&jukebox->tunernomeSfxCategory->songs[i].song, 0, sizeof(song_t));
+    }
+
+    jukeboxFreeCategories(&jukebox->sfxCategories, jukebox->numSfxCategories);
+
+    // Free dances and menu
 
     freePortableDance(jukebox->portableDances);
 
@@ -294,11 +383,11 @@ void jukeboxButtonCallback(buttonEvt_t* evt)
             {
                 if (jukebox->inMusicSubmode)
                 {
-                    bzrPlayBgm(musicCategories[jukebox->categoryIdx].songs[jukebox->songIdx].song, BZR_STEREO);
+                    bzrPlayBgm(&jukebox->musicCategories[jukebox->categoryIdx].songs[jukebox->songIdx].song, BZR_STEREO);
                 }
                 else
                 {
-                    bzrPlaySfx(sfxCategories[jukebox->categoryIdx].songs[jukebox->songIdx].song, BZR_STEREO);
+                    bzrPlaySfx(&jukebox->sfxCategories[jukebox->categoryIdx].songs[jukebox->songIdx].song, BZR_STEREO);
                 }
                 jukebox->isPlaying = true;
             }
@@ -325,11 +414,11 @@ void jukeboxButtonCallback(buttonEvt_t* evt)
             uint8_t length;
             if (jukebox->inMusicSubmode)
             {
-                length = ARRAY_SIZE(musicCategories);
+                length = jukebox->numMusicCategories;
             }
             else
             {
-                length = ARRAY_SIZE(sfxCategories);
+                length = jukebox->numSfxCategories;
             }
 
             if (jukebox->categoryIdx == 0)
@@ -347,11 +436,11 @@ void jukeboxButtonCallback(buttonEvt_t* evt)
             uint8_t length;
             if (jukebox->inMusicSubmode)
             {
-                length = ARRAY_SIZE(musicCategories);
+                length = jukebox->numMusicCategories;
             }
             else
             {
-                length = ARRAY_SIZE(sfxCategories);
+                length = jukebox->numSfxCategories;
             }
 
             jukebox->categoryIdx = (jukebox->categoryIdx + 1) % length;
@@ -365,11 +454,11 @@ void jukeboxButtonCallback(buttonEvt_t* evt)
             uint8_t length;
             if (jukebox->inMusicSubmode)
             {
-                length = musicCategories[jukebox->categoryIdx].numSongs;
+                length = jukebox->musicCategories[jukebox->categoryIdx].numSongs;
             }
             else
             {
-                length = sfxCategories[jukebox->categoryIdx].numSongs;
+                length = jukebox->sfxCategories[jukebox->categoryIdx].numSongs;
             }
 
             if (jukebox->songIdx == 0)
@@ -385,11 +474,11 @@ void jukeboxButtonCallback(buttonEvt_t* evt)
             uint8_t length;
             if (jukebox->inMusicSubmode)
             {
-                length = musicCategories[jukebox->categoryIdx].numSongs;
+                length = jukebox->musicCategories[jukebox->categoryIdx].numSongs;
             }
             else
             {
-                length = sfxCategories[jukebox->categoryIdx].numSongs;
+                length = jukebox->sfxCategories[jukebox->categoryIdx].numSongs;
             }
 
             jukebox->songIdx = (jukebox->songIdx + 1) % length;
@@ -497,10 +586,10 @@ void jukeboxMainLoop(int64_t elapsedUs)
                 }
                 else
                 {
-                    categoryName = musicCategories[jukebox->categoryIdx].categoryName;
-                    songName     = musicCategories[jukebox->categoryIdx].songs[jukebox->songIdx].name;
+                    categoryName = jukebox->musicCategories[jukebox->categoryIdx].categoryName;
+                    songName     = jukebox->musicCategories[jukebox->categoryIdx].songs[jukebox->songIdx].name;
                     songTypeName = "Music";
-                    numSongs     = musicCategories[jukebox->categoryIdx].numSongs;
+                    numSongs     = jukebox->musicCategories[jukebox->categoryIdx].numSongs;
                     drawNames    = true;
                 }
             }
@@ -514,10 +603,10 @@ void jukeboxMainLoop(int64_t elapsedUs)
                 }
                 else
                 {
-                    categoryName = sfxCategories[jukebox->categoryIdx].categoryName;
-                    songName     = sfxCategories[jukebox->categoryIdx].songs[jukebox->songIdx].name;
+                    categoryName = jukebox->sfxCategories[jukebox->categoryIdx].categoryName;
+                    songName     = jukebox->sfxCategories[jukebox->categoryIdx].songs[jukebox->songIdx].name;
                     songTypeName = "SFX";
-                    numSongs     = sfxCategories[jukebox->categoryIdx].numSongs;
+                    numSongs     = jukebox->sfxCategories[jukebox->categoryIdx].numSongs;
                     drawNames    = true;
                 }
             }
@@ -530,7 +619,7 @@ void jukeboxMainLoop(int64_t elapsedUs)
                 int16_t yOff  = (TFT_HEIGHT - jukebox->radiostars.height) / 2 - jukebox->radiostars.height * 0;
                 drawText(&(jukebox->radiostars), c313, text, (TFT_WIDTH - width) / 2, yOff);
                 // Draw category arrows if this submode has more than 1 category
-                if ((jukebox->inMusicSubmode && ARRAY_SIZE(musicCategories) > 1) || ARRAY_SIZE(sfxCategories) > 1)
+                if ((jukebox->inMusicSubmode && jukebox->numMusicCategories > 1) || jukebox->numSfxCategories > 1)
                 {
                     drawWsg(&jukebox->arrow, ((TFT_WIDTH - width) / 2) - 8 - jukebox->arrow.w, yOff, false, false, 0);
                     drawWsg(&jukebox->arrow, ((TFT_WIDTH - width) / 2) + width + 8, yOff, false, false, 180);
@@ -617,4 +706,24 @@ void jukeboxBackgroundDrawCb(int16_t x, int16_t y, int16_t w, int16_t h, int16_t
             break;
         }
     }
+}
+
+void jukeboxFreeCategories(jukeboxCategory** categoryArray, uint8_t numCategories)
+{
+    for(int categoryIdx = 0; categoryIdx < numCategories; categoryIdx++)
+    {
+        for(int songIdx = 0; songIdx < categoryArray[categoryIdx]->numSongs; songIdx++)
+        {
+            // Avoid freeing songs we never loaded
+            if(categoryArray[categoryIdx]->songs[songIdx].song.tracks != NULL)
+            {
+                freeSong(&categoryArray[categoryIdx]->songs[songIdx].song);
+            }
+        }
+
+        free(categoryArray[categoryIdx]->songs);
+        free(categoryArray[categoryIdx]);
+    }
+
+    free(categoryArray);
 }
