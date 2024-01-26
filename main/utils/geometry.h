ca96a550c133181df897f67e2b07bb74f66552a5
gelakinetic <gelakinetic@gmail.com>
2024-01-26 11:29:55 -0500

    Use vectors for geometry Add more intersection functions

--- main/utils/geometry.h
+++ main/utils/geometry.h
@@ -39,14 +39,14 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include "vector2d.h"
 
 /**
  * @brief Signed integer representation of a circle
  */
 typedef struct
 {
-    int32_t x;      ///< The X coordinate of the center of the circle
-    int32_t y;      ///< The Y coordinate of the center of the circle
+    vec_t pos;      ///< The position of the center of the circle
     int32_t radius; ///< The radius of the circle
 } circle_t;
 
@@ -55,14 +55,25 @@ typedef struct
  */
 typedef struct
 {
-    int32_t x;      ///< The X coordinate of the top left corner of the rectangle
-    int32_t y;      ///< The Y coordinate of the top left corner of the rectangle
+    vec_t pos;      ///< The position the top left corner of the rectangle
     int32_t width;  ///< The width of the rectangle
     int32_t height; ///< The height of the rectangle
 } rectangle_t;
 
+/**
+ * @brief Signed integer representation of a line segment
+ *
+ */
+typedef struct
+{
+    vec_t p1; ///< One end point of the line segment
+    vec_t p2; ///< The other end point of the line segment
+} line_t;
+
 bool circleCircleIntersection(circle_t circle1, circle_t circle2);
-bool rectRectIntersection(rectangle_t rect1, rectangle_t rect2);
+bool circlePointIntersection(circle_t circle, vec_t point);
 bool circleRectIntersection(circle_t circle, rectangle_t rect);
+bool circleLineIntersection(circle_t circle, line_t line);
+bool rectRectIntersection(rectangle_t rect1, rectangle_t rect2);
 
 #endif
