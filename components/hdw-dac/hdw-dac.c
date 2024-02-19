d4b85983300204fff5eba1048dad38e23b739953
gelakinetic <gelakinetic@gmail.com>
2024-02-18 22:18:15 -0500

    Polyphony!

--- components/hdw-dac/hdw-dac.c
+++ components/hdw-dac/hdw-dac.c
@@ -2,6 +2,7 @@
 // Includes
 //==============================================================================
 
+#include <inttypes.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
 #include "driver/dac_continuous.h"
@@ -10,19 +11,53 @@
 // Defines
 //==============================================================================
 
-#define AUDIO_SAMPLE_RATE_HZ 48000
+#define AUDIO_SAMPLE_RATE_HZ 32768
 #define BUF_SIZE             2048
-#define DMA_DESCRIPTORS      8
+#define DMA_DESCRIPTORS      4
+
+//==============================================================================
+// Unions
+//==============================================================================
+
+typedef union
+{
+    uint32_t accum32; /** The oscillator value is accumulated in a 32 bit integer */
+    uint8_t bytes[4]; /** The index into the sine table is accessed as a single byte */
+} oscAccum_t;
 
 //==============================================================================
 // Variables
 //==============================================================================
+
+/** A 256 point sine wave */
+static const uint8_t DRAM_ATTR sinTab[] = {
+    127, 131, 134, 137, 140, 143, 146, 149, 152, 155, 158, 161, 164, 167, 170, 173, 176, 179, 182, 185, 187, 190,
+    193, 195, 198, 201, 203, 206, 208, 210, 213, 215, 217, 219, 222, 224, 226, 228, 230, 231, 233, 235, 236, 238,
+    240, 241, 242, 244, 245, 246, 247, 248, 249, 250, 251, 251, 252, 253, 253, 254, 254, 254, 254, 254, 255, 254,
+    254, 254, 254, 254, 253, 253, 252, 251, 251, 250, 249, 248, 247, 246, 245, 244, 242, 241, 240, 238, 236, 235,
+    233, 231, 230, 228, 226, 224, 222, 219, 217, 215, 213, 210, 208, 206, 203, 201, 198, 195, 193, 190, 187, 185,
+    182, 179, 176, 173, 170, 167, 164, 161, 158, 155, 152, 149, 146, 143, 140, 137, 134, 131, 128, 124, 121, 118,
+    115, 112, 109, 106, 103, 100, 97,  94,  91,  88,  85,  82,  79,  76,  73,  70,  68,  65,  62,  60,  57,  54,
+    52,  49,  47,  45,  42,  40,  38,  36,  33,  31,  29,  27,  25,  24,  22,  20,  19,  17,  15,  14,  13,  11,
+    10,  9,   8,   7,   6,   5,   4,   4,   3,   2,   2,   1,   1,   1,   1,   1,   0,   1,   1,   1,   1,   1,
+    2,   2,   3,   4,   4,   5,   6,   7,   8,   9,   10,  11,  13,  14,  15,  17,  19,  20,  22,  24,  25,  27,
+    29,  31,  33,  36,  38,  40,  42,  45,  47,  49,  52,  54,  57,  60,  62,  65,  68,  70,  73,  76,  79,  82,
+    85,  88,  91,  94,  97,  100, 103, 106, 109, 112, 115, 118, 121, 124,
+};
 
 /** The handle created for the DAC */
 static dac_continuous_handle_t dac_handle;
 
 /** A queue to move dac_event_data_t from the interrupt to the main loop*/
 static QueueHandle_t que;
+
+/** Accumulators for oscillators */
+static oscAccum_t osc1 = {0};
+static oscAccum_t osc2 = {0};
+
+/** Step size for the accumulators */
+static uint32_t oscStep1 = ((uint64_t)(sizeof(sinTab) * 440) << 16) / (AUDIO_SAMPLE_RATE_HZ);
+static uint32_t oscStep2 = ((uint64_t)(sizeof(sinTab) * 523) << 16) / (AUDIO_SAMPLE_RATE_HZ);
 
 //==============================================================================
 // Functions
@@ -95,11 +130,17 @@ void dacPoll(void)
     dac_event_data_t evt_data;
     if (xQueueReceive(que, &evt_data, 0))
     {
-        /* Generate a sawtooth on the fly */
+        /* Generate a wave on the fly */
         static uint8_t sawtooth[BUF_SIZE];
         for (int32_t i = 0; i < BUF_SIZE; i++)
         {
-            sawtooth[i] = (i * 255) / BUF_SIZE;
+            /* Oscillate two waves and add them together */
+            osc1.accum32 += oscStep1;
+            osc2.accum32 += oscStep2;
+            sawtooth[i] = (sinTab[osc1.bytes[2]] + sinTab[osc2.bytes[2]]) / 2;
+
+            /* Or generate a sawtooth */
+            /* sawtooth[i] = (i * 255) / BUF_SIZE; */
         }
 
         /* Write the data to the DAC */
