6bd6736e89fd9e1d9461f580aee686c64784322f
Dylan Whichard <dylan@whichard.com>
2024-03-15 15:58:57 -0700

    Add more examples to hash map documentation

--- main/utils/hashMap.h
+++ main/utils/hashMap.h
@@ -6,19 +6,27 @@
  *
  * \section hashMap_design Design Philosophy
  *
- * This is a generic hash map data structure, which supports the basic put, get, remove, and iterate operations.
- * By default, keys are assumed to be null-terminated strings, but any type can be used if alternate hash and
- * equality functions are supplied. The hash map will automatically be resized once its item count reaches 75%
- * of its array size, but setting a sufficiently large initial size will improve efficiency overall.
+ * This is a generic hash map data structure, which supports the basic put, get, and remove operations, as well
+ * as iteration. By default, keys are assumed to be nul-terminated strings, but any type can be used if
+ * alternate hash and equality functions are supplied. The hash map will automatically resize itself once it
+ * becomes about 75% full, but setting a sufficiently large initial capacity will improve efficiency overall.
  *
- * Basic operations against the hash map generally have O(1) time in the average case, with O(n) time in the worst case.
- * The exception is hashIterRemove(), which has O(1) time in the worst case. Hash collisions are handled by storing
- * multiple items within the same bucket in a linked list. Resizing is O(k), where k is the number of buckets in the
- * hash map, and should occur once in every k put operations.
+ * Keys inserted into the map must remain valid for the duration they are in the map, as the map maintains a reference
+ * rather than copying them. This also means that modifying the key itself after it has been inserted into the
+ * map will cause it to become inaccessible, so using immutable data as pointers is also recommended. To change
+ * the key of an entry in the map, remove it and add it with the new key instead.
+ *
+ * Basic operations against the hash map have O(1) time in the average case, with O(n) time in the worst case.
+ * Hash collisions are handled by storing multiple items within the same bucket in a linked list. Resizing is O(k),
+ * where k is the number of buckets in the hash map, and should occur once in every k put operations.
+ *
+ * Iteration over the map is O(k) best case, O(k+n) worst case, and O(k) average case, as we do not know ahead of time
+ * which buckets or keys are in use. This could theoretically be improved to O(n) worst case if keys were also kept in
+ * a separate array, but this would be a marginal improvement with a lot of complexity.
  *
  * \section hashMap_usage Usage
  *
- * hashInit() allocates a new hash map for string keys, and hashInit
+ * hashInit() allocates a new hash map for use with string keys.
  *
  * hashPut() adds a new entry to the map or updates the value of an existing one.
  *
@@ -28,18 +36,311 @@
  *
  * hashDeinit() deallocates a hash map and all its entries.
  *
- * \section hashMap_example Example
+ * hashIterate() can be used to loop over a hash map's entries.
+ *
+ * hashIterReset() is used to reset an iterator if iteration stopped before hashIterate() returned false.
+ *
+ * hashIterRemove() can be used to safely remove entries during iteration.
+ *
+ * hashPutBin(), hashGetBin(), and hashRemoveBin() are variants of the normal hash functions which accept
+ * a void pointer in the \c key argument, rather than a char pointer. You must provide hash and equality
+ * functions to hashInitBin() in order to safely use keys which are not nul-terminated strings.
+ *
+ * hashString(), strEq(), hashBytes(), and bytesEq() are helper functions useful when implementing custom
+ * hash and comparison functions.
+ *
+ * \section hashMap_example Examples
+ *
+ * Create a hash map, add and retrieve some values:
+ * \code{.c}
+ * // Declare a hash map
+ * hashMap_t map = {0};
+ * // Initialize it with an initial capacity of 16
+ * hashInit(&map, 16);
+ *
+ * hashPut(&map, "greeting", "Hello");
+ * hashPut(&map, "name", "Swadgeman");
+ * // Prints 'Hello! Your name is Swadgeman!'
+ * printf("%s! Your name is %s!\n", hashGet(&map, "greeting"), hashGet(&map, "name"));
+ *
+ * hashPut(&map, "greeting", "Goodbye");
+ * // Prints 'Goodbye, Swadgeman.'
+ * printf("%s, %s.", hashGet(&map, "greeting"), hashGet(&map, "name"));
+ * \endcode
  *
  * \subsection hashMap_example_iteration Iteration
+ * Iterate over items in the map:
+ * \code{.c}
+ * hashMap_t map = {0};
+ * hashInit(&map, 16);
  *
- * \section hashMap_caveats Caveats
- * This utility has some drawbacks, mainly
+ * hashPut(&map, "title", "HashMap");
+ * hashPut(&map, "description", "A cool new data structure");
+ * hashPut(&map, "filename", "hashMap.h");
+ * hashPut(&map, "directory", "utils");
+ * hashPut(&map, "somethingElse", "donut");
+ * hashPut(&map, "tag-0", "data-structure");
+ * hashPut(&map, "tag-1", "map");
+ * hashPut(&map, "tag-2", "dictionary");
+ *
+ * hashIterator_t iter = {0};
+ * while (hashIterate(&map, &iter))
+ * {
+ *     printf("%s: %s\n", iter.key, iter.value);
+ * }
+ * // No need to use hashIterReset() here as the loop finished normally
+ * hashDeinit(&map);
+ * \endcode
+ *
+ * Remove items while iterating:
+ * \code{.c}
+ * while (hashIterate(&map, &iter))
+ * {
+ *     // Remove any items with a key starting with "tag-"
+ *     if (!strncmp(iter.key, "tag-", 4))
+ *     {
+ *         printf("Removing tag %s\n", iter.value);
+ *         if (!hashIterRemove(&map, &iter))
+ *         {
+ *             //
+ *             break;
+ *         }
+ *     }
+ * }
+ * // Still no need to use hashIterReset(), as it will be handled
+ * // automatically by either hashIterate() or hashIterRemove()
+ * \endcode
+ *
+ * Breaking iteration early:
+ * \code{.c}
+ * // Find and print any tag, then stop
+ * while (hashIterate(&map, &iter))
+ * {
+ *     if (!strncmp(iter.key, "tag-", 4))
+ *     {
+ *         printf("Found a tag: %s\n", iter.key);
+ *         break;
+ *     }
+ * }
+ *
+ * // This time, hashIterReset() is necessary!
+ * // We might not have reached the end of iteration, so the iterator might not have reset automatically
+ * // If we did happen to reach the end anyway, resetting it again won't hurt anything
+ * hashIterReset(&iter);
+ *
+ * \subsection hashMap_example_nonStringKeys Non-string Keys
+ *
+ * The hash map also supports using structs or even primitives as key types, instead of strings.
+ * This can be done using hashInitBin() to supply custom hashing and comparison functions
+ * appropriate for the type of key being used.
+ *
+ * \subsubsection hashMap_example_nonStringKeys_enum
+ *
+ * This example shows how to use a hash map where the keys are enum values. This method will
+ * work with any numeric type, as long as \c{sizeof(type) <= sizeof(void*)} -- that is, as
+ * long as it fits inside a pointer. In practice this means an \c int32_t can be used as a
+ e* key in a hash map in this way, but an \c int64_t cannot.
+ *
+ * \code{.c}
+ * #include "hdw-btn.h"
+ * #include "spiffs_wsg.h"
+ * #include <stdbool.h>
+ * #include <stdint.h>
+ *
+ * // Hash function for using ::buttonBit_t as a key
+ * uint32_t hashEnum(const void* data)
+ * {
+ *     // This might be cursed but the C spec says it's totally fine
+ *     buttonBit_t enumValue = (buttonBit_t)(data);
+ *     return hashData(&enumValue, sizeof(buttonBit_t));
+ * }
+ *
+ * // Compare ::buttonBit_t values stuffed into void pointers
+ * bool enumEq(const void* a, const void* b)
+ * {
+ *     return a == b;
+ * }
+ *
+ * // Just a simple struct to hold a WSG and its rotation for the values
+ * typedef struct
+ * {
+ *     int16_t rot;
+ *     wsg_t wsg;
+ * } wsgRot_t;
+ *
+ * hashmap_t map = {0};
+ * wsgRot_t* wsgs = NULL;
+ *
+ * void setupButtonIconMap(void)
+ * {
+ *     hashInitBin(&map, 16, hashEnum, enumEq);
+ *
+ *     wsgs = calloc(8, sizeof(wsgRot_t));
+ *     wsgRot_t* wsg = wsgs;
+ *
+ *     // Load up a bunch of WSGs
+ *     loadWsg("button_up.wsg", &wsg->wsg, false);
+ *     hashPutBin(&map, PB_UP, wsg++);
+ *
+ *     loadWsg("button_up.wsg", &wsg->wsg, false);
+ *     wsg->rot = 180;
+ *     hashPutBin(&map, PB_DOWN, wsg++);
+ *
+ *     loadWsg("button_up.wsg", &wsg->wsg, false);
+ *     wsg->rot = 90;
+ *     hashPutBin(&map, PB_LEFT, wsg++);
+ *
+ *     loadWsg("button_up.wsg", &wsg->wsg, false);
+ *     wsg->rot = 270;
+ *     hashPutBin(&map, PB_RIGHT, wsg++);
+ *
+ *     loadWsg("button_a.wsg", &wsg->wsg, false);
+ *     hashPutBin(&map, PB_A, wsg++);
+ *
+ *     loadWsg("button_b.wsg", &wsg->wsg, false);
+ *     hashPutBin(&map, PB_B, wsg++);
+ *
+ *     loadWsg("button_menu.wsg", &wsg->wsg, false);
+ *     hashPutBin(&map, PB_MENU, wsg++);
+ *
+ *     loadWsg("button_pause.wsg", &wsg->wsg, false);
+ *     hashPutBin(&map, PB_PAUSE, wsg++);
+ * }
+ *
+ * void runButtonIconMap(void)
+ * {
+ *     // Use the WSGs
+ *     buttonEvt_t evt = {0};
+ *     while (checkButtonQueueWrapper(&evt))
+ *     {
+ *         if (evt.down)
+ *         {
+ *             wsgRot_t* draw = hashGet(&map, evt.button);
+ *             drawWsg(&draw->wsg, (TFT_WIDTH - draw->w) / 2, (TFT_HEIGHT - draw->h) / 2, false, false, draw->rot);
+ *         }
+ *     }
+ * }
+ *
+ * void cleanupButtonIconMap(void)
+ * {
+ *     // Clean up the WSGs using the hash map
+ *     hashIterator_t iter = {0};
+ *     while (hashIterate(&map, &iter))
+ *     {
+ *         wsgRot_t* value = iter.value;
+ *         freeWsg(&value->wsg);
+ *     }
+ *
+ *     // Clean up the hash map and WSG struct
+ *     hashDeinit(&map);
+ *
+ *     // Clean up the values
+ *     free(wsgs);
+ * }
+ * \endcode
+ *
+ * \subsubsection hashMap_example_nonStringKeys_struct
+ * This example shows how to use
+ * \code{.c}
+ * // A struct to use for the key
+ * typedef struct
+ * {
+ *     int32_t x, y;
+ * } keyStruct_t;
+ *
+ * int32_t hashStruct(const void* data)
+ * {
+ *     // Helper function provided with hash map
+ *     return hashBytes((const uint8_t*)data, sizeof(keyStruct_t));
+ * }
+ *
+ * bool structEq(const void* a, const void* b)
+ * {
+ *     // Another hash map helper function
+ *     return bytesEq((const uint8_t*)a, sizeof(keyStruct_t), (const uint8_t*)b, sizeof(keyStruct_t));
+ * }
+ *
+ * hashMap_t map = {0};
+ * keyStruct_t keys[5] = {0};
+ *
+ * void setupStructKeyExample(void)
+ * {
+ *     hashInitBin(&map, 10, hashStruct, structEq);
+ *
+ *     keys[0].x = 10;
+ *     keys[0].y = 16;
+ *
+ *     keys[1].x = 5;
+ *     keys[1].y = 5;
+ *
+ *     keys[2].x = 7;
+ *     keys[2].y = 12;
+ *
+ *     keys[3].x = 1;
+ *     keys[3].y = 3;
+ *
+ *     keys[4].x = 16;
+ *     keys[4].y = 4;
+ *
+ *     hashPutBin(&map, &keys[0], "Star");
+ *     hashPutBin(&map, &keys[1], "Square");
+ *     hashPutBin(&map, &keys[2], "Circle");
+ *     hashPutBin(&map, &keys[3], "Diamond");
+ *     hashPutBin(&map, &keys[4], "Triangle");
+ * }
+ *
+ * void runStructKeyExample(void)
+ * {
+ *     static int x = 0;
+ *     static int y = 0;
+ *
+ *     buttonEvt_t evt;
+ *     while (checkButtonQueueWrapper(&evt))
+ *     {
+ *         if (evt.down)
+ *         {
+ *             if (evt.button == PB_UP)
+ *             {
+ *                 y++;
+ *             }
+ *             else if (evt.button == PB_DOWN)
+ *             {
+ *                 y--;
+ *             }
+ *             else if (evt.button == PB_LEFT)
+ *             {
+ *                 x--;
+ *             }
+ *             else if (evt.button == PB_RIGHT)
+ *             {
+ *                 x++;
+ *             }
+ *             else if (evt.button == PB_A)
+ *             {
+ *                 keyStruct_t key;
+ *                 key.x = x;
+ *                 key.y = y;
+ *                 const char* found = hashGetBin(&map, &key);
+ *                 if (found)
+ *                 {
+ *                     printf("Found a %s at %d, %d!\n", found, x, y);
+ *                 }
+ *                 else
+ *                 {
+ *                     printf("Didn't find anything at %d, %d :(\n", x, y);
+ *                 }
+ *             }
+ *         }
+ *     }
+ * }
+ * \endcode
  */
 #ifndef _HASH_MAP_H_
 #define _HASH_MAP_H_
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
 #include "linked_list.h"
 
@@ -95,6 +396,7 @@ typedef struct
     eqFunction_t eqFunc;
 } hashMap_t;
 
+// Default hash functions
 uint32_t hashString(const char* str);
 bool strEq(const void* a, const void* b);
 
