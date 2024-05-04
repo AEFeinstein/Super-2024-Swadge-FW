3388c3b9da0d8e23a54433a0c4846ac752a5b3d4
gelakinetic <gelakinetic@gmail.com>
2024-05-03 23:00:59 -0400

    Draw grid the right way Add cursor

--- main/modes/ringsAndGems/ringsAndGems.c
+++ main/modes/ringsAndGems/ringsAndGems.c
@@ -13,7 +13,14 @@ static void ragConCb(p2pInfo* p2p, connectionEvt_t evt);
 static void ragMsgRxCb(p2pInfo* p2p, const uint8_t* payload, uint8_t len);
 static void ragMsgTxCbFn(p2pInfo* p2p, messageStatus_t status, const uint8_t* data, uint8_t len);
 
-static void ragDrawGrid(int16_t x0, int16_t y0, int16_t x1, int16_t y1, paletteColor_t color);
+static void ragDrawGrid(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t m, paletteColor_t color);
+
+typedef enum __attribute__((packed))
+{
+    NO_CURSOR,
+    SELECT_SUBGAME,
+    SELECT_CELL,
+} ragCursorMode_t;
 
 typedef enum __attribute__((packed))
 {
@@ -32,6 +39,9 @@ typedef struct
 {
     p2pInfo p2p;
     ragSubgame_t subgames[3][3];
+    vec_t cursor;
+    vec_t selectedSubgame;
+    ragCursorMode_t cursorMode;
 } ringsAndGems_t;
 
 swadgeMode_t ragMode = {
@@ -63,6 +73,8 @@ static void ragEnterMode(void)
     // Allocate memory for the mode
     rag = calloc(1, sizeof(ringsAndGems_t));
 
+    rag->cursorMode = SELECT_SUBGAME;
+
     // Initialize and start p2p
     p2pInitialize(&rag->p2p, 0x25, ragConCb, ragMsgRxCb, -70);
     p2pStartConnection(&rag->p2p);
@@ -93,62 +105,130 @@ static void ragMainLoop(int64_t elapsedUs)
     while (checkButtonQueueWrapper(&evt))
     {
         // Do something?
+        if (evt.down)
+        {
+            switch (evt.button)
+            {
+                case PB_UP:
+                {
+                    if (0 == rag->cursor.y)
+                    {
+                        rag->cursor.y = 2;
+                    }
+                    else
+                    {
+                        rag->cursor.y--;
+                    }
+                    break;
+                }
+                case PB_DOWN:
+                {
+                    rag->cursor.y = (rag->cursor.y + 1) % 3;
+                    break;
+                }
+                case PB_LEFT:
+                {
+                    if (0 == rag->cursor.x)
+                    {
+                        rag->cursor.x = 2;
+                    }
+                    else
+                    {
+                        rag->cursor.x--;
+                    }
+                    break;
+                }
+                case PB_RIGHT:
+                {
+                    rag->cursor.x = (rag->cursor.x + 1) % 3;
+                    break;
+                }
+                case PB_A:
+                {
+                    if (SELECT_SUBGAME == rag->cursorMode)
+                    {
+                        rag->selectedSubgame = rag->cursor;
+                        rag->cursor.x        = 1;
+                        rag->cursor.y        = 1;
+                        rag->cursorMode      = SELECT_CELL;
+                    }
+                    else if (SELECT_CELL == rag->cursorMode)
+                    {
+                        // TODO place marker, pass the turn
+                    }
+                    break;
+                }
+                case PB_B:
+                {
+                    if (SELECT_CELL == rag->cursorMode)
+                    {
+                        rag->cursor     = rag->selectedSubgame;
+                        rag->cursorMode = SELECT_SUBGAME;
+                    }
+                    break;
+                }
+                default:
+                {
+                    break;
+                }
+            }
+        }
     }
 
     // Clear before drawing
     clearPxTft();
 
-#define MARGIN   3
-#define S_MARGIN 2
-#define C_MARGIN 1
+    // Calculate the game size based on the largest possible cell size
+    int16_t gameSize    = MIN(TFT_WIDTH, TFT_HEIGHT);
+    int16_t cellSize    = gameSize / 9;
+    int16_t subgameSize = cellSize * 3;
+    gameSize            = cellSize * 9;
 
-    // Find the size of a subgrid
-    int16_t subgameSize = TFT_HEIGHT / 3;
-    int16_t cellSize    = (subgameSize - (2 * MARGIN)) / 3;
+    // Center the game on the screen
+    int16_t gameOffsetX = (TFT_WIDTH - gameSize) / 2;
+    int16_t gameOffsetY = (TFT_HEIGHT - gameSize) / 2;
 
-    // Draw main grid
-    ragDrawGrid((TFT_WIDTH - TFT_HEIGHT) / 2, 0, //
-                (TFT_WIDTH + TFT_HEIGHT) / 2, TFT_HEIGHT, c555);
+    // Draw the main gridlines
+    ragDrawGrid(gameOffsetX, gameOffsetY, gameOffsetX + gameSize - 1, gameOffsetY + gameSize - 1, 0, c300);
 
-    // Draw subgrids
-    for (int16_t y = 0; y < 3; y++)
+    // For each subgame
+    for (int subY = 0; subY < 3; subY++)
     {
-        for (int16_t x = 0; x < 3; x++)
+        for (int subX = 0; subX < 3; subX++)
         {
-            int16_t subX0 = (TFT_WIDTH - TFT_HEIGHT) / 2 + (x * subgameSize);
-            int16_t subX1 = subX0 + subgameSize;
-            int16_t subY0 = y * subgameSize;
-            int16_t subY1 = subY0 + subgameSize;
+            int16_t sX0 = gameOffsetX + (subX * subgameSize);
+            int16_t sY0 = gameOffsetY + (subY * subgameSize);
+            int16_t sX1 = sX0 + subgameSize - 1;
+            int16_t sY1 = sY0 + subgameSize - 1;
+            // Draw the subgame grid lines
+            ragDrawGrid(sX0, sY0, sX1, sY1, 4, c330);
 
-            // Subgrid
-            ragDrawGrid(subX0 + MARGIN, //
-                        subY0 + MARGIN, //
-                        subX1 - MARGIN, //
-                        subY1 - MARGIN, //
-                        c500);
-
-            // Cursor
-            drawRect(subX0 + S_MARGIN - 1, //
-                     subY0 + S_MARGIN - 1, //
-                     subX1 - S_MARGIN,     //
-                     subY1 - S_MARGIN,     //
-                     c030);
-
-            // Cursors in the subgrid
-            for (int16_t cy = 0; cy < 3; cy++)
+            // If selected, draw the cursor on this subgame
+            if (SELECT_SUBGAME == rag->cursorMode && //
+                rag->cursor.x == subX && rag->cursor.y == subY)
             {
-                for (int16_t cx = 0; cx < 3; cx++)
-                {
-                    int16_t cellX0 = MARGIN + subX0 + (cx * (cellSize + 1));
-                    int16_t cellX1 = cellX0 + cellSize;
-                    int16_t cellY0 = MARGIN + subY0 + (cy * (cellSize + 1));
-                    int16_t cellY1 = cellY0 + cellSize;
+                drawRect(sX0, sY0, sX1, sY1, c005);
+            }
 
-                    drawRect(cellX0 + C_MARGIN, //
-                             cellY0 + C_MARGIN, //
-                             cellX1 - C_MARGIN, //
-                             cellY1 - C_MARGIN, //
-                             c002);
+            // For each cell
+            for (int cellY = 0; cellY < 3; cellY++)
+            {
+                for (int cellX = 0; cellX < 3; cellX++)
+                {
+                    int16_t cX0 = sX0 + (cellX * cellSize);
+                    int16_t cY0 = sY0 + (cellY * cellSize);
+                    int16_t cX1 = cX0 + cellSize - 1;
+                    int16_t cY1 = cY0 + cellSize - 1;
+                    // Draw sprites
+                    // fillDisplayArea(cX0, cY0, cX1, cY1, c002);
+
+                    // If selected, draw the cursor on this cell
+                    if (SELECT_CELL == rag->cursorMode &&                                   //
+                        rag->selectedSubgame.x == subX && rag->selectedSubgame.y == subY && //
+                        rag->cursor.x == cellX && rag->cursor.y == cellY)
+                    {
+                        drawRect(cX0, cY0, cX1, cY1, c005);
+                    }
                 }
             }
         }
@@ -162,23 +242,28 @@ static void ragMainLoop(int64_t elapsedUs)
  * @param y0
  * @param x1
  * @param y1
+ * @param m
  * @param color
  */
-static void ragDrawGrid(int16_t x0, int16_t y0, int16_t x1, int16_t y1, paletteColor_t color)
+static void ragDrawGrid(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t m, paletteColor_t color)
 {
     int16_t width  = x1 - x0;
     int16_t height = y1 - y0;
 
-    int16_t cellWidth  = (width - 2) / 3;
-    int16_t cellHeight = (height - 2) / 3;
+    int16_t cellWidth  = width / 3;
+    int16_t cellHeight = height / 3;
 
     // Horizontal lines
-    drawLineFast(x0, y0 + cellHeight, x1 - 1, y0 + cellHeight, color);
-    drawLineFast(x0, y0 + (2 * cellHeight) + 1, x1 - 1, y0 + (2 * cellHeight) + 1, color);
+    drawLineFast(x0 + m, y0 + cellHeight, //
+                 x1 - 1 - m, y0 + cellHeight, color);
+    drawLineFast(x0 + m, y0 + (2 * cellHeight) + 1, //
+                 x1 - 1 - m, y0 + (2 * cellHeight) + 1, color);
 
     // Vertical lines
-    drawLineFast(x0 + cellWidth, y0, x0 + cellWidth, y1 - 1, color);
-    drawLineFast(x0 + (2 * cellWidth) + 1, y0, x0 + (2 * cellWidth) + 1, y1 - 1, color);
+    drawLineFast(x0 + cellWidth, y0 + m, //
+                 x0 + cellWidth, y1 - 1 - m, color);
+    drawLineFast(x0 + (2 * cellWidth) + 1, y0 + m, //
+                 x0 + (2 * cellWidth) + 1, y1 - 1 - m, color);
 }
 
 /**
