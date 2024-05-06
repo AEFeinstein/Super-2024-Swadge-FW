7e03d0e4ab21e61960fc62d2009f46e62d21c6a6
JohnnyWycliffe <johnnywycliffe@gmail.com>
2024-05-05 21:38:58 -0400

    Added tasks file, started menu system. Still need to create Fight and Race in tasks file.

---
+++ main/modes/games/cGrove/Docs/Tasks.md
@@ -0,0 +1,257 @@
+# Tasks
+
+## Menu
+
+### Functionality
+- [] Text entry system
+- [] Notifications of changed values
+- [] Inventory viewer
+- [] Online
+    - [] Profile viewer
+        - [] Show username/pronouns/mood
+        - [] Show Chowa
+    - [] Set sharable Chowa
+    - [] See recently downloaded profiles
+    - [] Turn on/off online features
+
+### Assets
+- [] Images
+    - [] Menu BG
+- [] Audio
+    - [] Menu BGM (Borrow from other games)
+    - [] Selection SFX
+    - [] Option change SFX
+- [] Font
+    - [] Collect Sonic font
+    - [] Create/Use font closer to Sonic style for menus
+
+## Chowa
+
+### Design
+Chowa have a pudgy two-tone body and head, two arms and two legs, two eyes, a tail and a mouth. The mouth has a default expression based on personality but changes based on mood. 
+
+Need to get this drawn up as a concept.
+
+### Research
+- [] Palette for colors
+    - If not palettes, will need to use coposite shapes
+
+### Functionality
+- [] Age restrictions
+    - [] Learning slows down once an adult
+    - [] Children have nerfed stats
+    - [] Eggs (hatch)
+        - [] Throw against wall
+        - [] Timer
+        - [] Other Chawo sing next to it reduces timer
+- [] Grove
+    - [] State machine for behavior (See [Diagram](#state-diagram))
+        - [] Standing
+        - [] Eating (Requires food)
+        - [] Walking
+        - [] Running
+        - [] Swimming
+        - [] Climbing
+        - [] Singing (Solo and group)
+        - [] Talking (Group)
+        - [] Sitting
+        - [] Jump
+        - [] Sparring/Fighting/shadowboxing (solo and group)
+        - [] Item specific
+            - [] Drawing
+            - [] Attacking with toy
+            - [] Throwing object
+            - [] Reading
+    - [] Inter-Chawo Interactions
+        - [] Compatibility mechanic (Cha + how similar stats are)
+        - [] Breeding (High compatibility + adults)
+    - [] Chowa player interactions
+        - [] Kindess/Abuse scale
+        - [] Willingness to respond to player's actions and accept gifts
+
+#### State Diagram
+
+[Diagram]()
+
+### Assets
+- [] Images
+    - [] High Rez Chowa image for art
+    - [] Egg/Nut to hatch from
+    - [] Poses
+        - [] Standing
+        - [] Eating (animation cycle)
+        - [] Walking (animation cycle)
+        - [] Running (animation cycle)
+        - [] Swimming (animation cycle)
+        - [] Climbing (animation cycle)
+        - [] Singing (animation cycle)
+        - [] Talking (animation cycle)
+        - [] Dancing x 4 (animation cycle)
+        - [] Sitting
+        - [] Jump
+        - [] Figthing
+            - [] Kick
+            - [] Punch
+            - [] Heatbutt OR Tail-whack
+        - [] Item specific
+            - [] Drawing (animation cycle)
+            - [] Toy weapon slashes (animation cycle)
+            - [] Tossing
+            - [] Reading
+    - [] Facial expressions
+        - [] Smile
+        - [] Frown
+        - [] Scowl
+        - [] Open
+        - [] Open/Smile
+        - [] Closed/normal
+        - [] Smile/Teeth
+    - [] Eyes
+        - [] Open
+        - [] Closed
+- [] Audio
+    - [] Footsteps
+    - [] 2-5 Effort noises (Fighting, Swimming, Climbing, etc)
+    - [] 2-5 Pain noises (Getting hit, being tossed, etc)
+    - [] 2-5 vocalizations (Singing, talking to other Chawo, etc)
+    - [] Interaction noise (small "pok" sound. Reuse menu sound?)
+- [] Other
+    - [] Chawo State machine diagram
+
+## Items
+
+### Assets
+- [] Images
+    - [] Crayons
+        - [] Crayon Pango sprite
+        - [] Crayon Poe sprite
+        - [] Crayon Pixel sprite
+        - [] Crayon Garbotnik sprite
+    - [] Book
+    - [] Knife
+    - [] Ball
+    - [] Bat
+    - [] Sword
+
+TODO: Enumerate items
+
+## Inventory
+
+### Functionality
+- [] Holds up to 255 items
+- [] Holds a high number of rings
+    - [] Rings can be used to buy things
+    - [] Rings can be gained by Chowa gitin gthem to player when they have high kindness
+    - [] Rings are given as 1st, 2nd and 3rd place prizes in games
+
+### Assets
+- [] Images
+    - [] Rings
+
+## Grove
+
+### Functionality
+- [] Move hand around the screen
+- [] Scroll around grove by moving hand to edge of screen
+- [] Select object with A
+- [] Hold A to pick up object
+- [] Press B on item to put it away
+- [] If a held A (Grab) is released while moving, held object is tossed
+- [] Use cursor on Inventory button to take item from inventory
+- [] Use cursor on return button to return to main menu
+- [] Selecting a Chowa pets the Chowa
+- [] Chowa, food, and items can be picked up and thrown
+- [] Guest Chowa (Online)
+
+See [Chowa](#chowa) for Chowa behaviors inside the grove.
+
+### Assets
+- [] Images
+    - [] Grass tile
+    - [] Water tile
+    - [] Trees trunks as walls
+    - [] UI Hand open
+    - [] UI Hand closed
+    - [] Back icon
+    - [] Inventory icon
+- [] Audio
+    - [] Grove BGM
+    - [] Footsteps
+    - [] Splashing noises
+
+See [Chowa](#chowa) for assets required for Chowa inside Grove.
+
+## Fighting
+
+TBD
+
+### Functionality
+- 
+
+### Assets
+- 
+
+## Racing
+
+TBD
+
+### Functionality
+- 
+
+### Assets
+- 
+
+## Performance
+
+### Functionality
+- [] Play a song/part of a song
+- [] Chowa sings along
+- [] Player presses arrow keys to match with a DDR style game to dance
+    - [] SPD stat expands hit timing
+    - [] AGI Increases score gain
+- [] A button can be pressed when the A icon flashes to sing
+    - [] CHA increases score gain of this action
+- [] Pause button quits game
+- [] Score board with fake NPC scores
+
+### Assets
+- [] Images
+    - [] Stage
+        - [] Microphone
+        - [] Curtains
+    - [] Game
+        - [] Arrow
+        - [] A prompt
+- [] Audio
+    - [] 5 songs from past years/current year
+- [] Font
+    - [] Font for "Nice!" "Good!" "Awesome!" hit markers
+
+## Stores
+
+### Functionality
+- [] Show icon of item, Name, price, and amount of rings/coins
+- [] Scroll through items one at a time with up and down arrows
+- [] B button goes back to main menu
+- [] A button selects item to purchase (if have enough coin)
+
+### Assets
+- [] Images
+    - [] Grocery BG
+    - [] Toy store BG
+    - [] Book store BG
+
+## School
+
+### Functionality
+- [] Requires a fee
+- [] Chowa read a book at the desk
+- [] Display a timer until done
+- [] One Chowa at once
+- [] Selection menu
+
+### Assets
+- [] Images
+    - [] Schoolroom
+    - [] Desk
+    - [] Chalkboard
--- main/modes/games/cGrove/mode_cGrove.c
+++ main/modes/games/cGrove/mode_cGrove.c
@@ -20,23 +20,52 @@
 
 // Function Prototypes
 //==============================================================================
-static void cGroveMainLoop(int64_t elapsedUs);
+static void cGroveMainLoop(int64_t);
 static void cGroveExitMode(void);
 static void cGroveEnterMode(void);
-static void cGroveBackgroundDrawCallback(int16_t x, int16_t y, int16_t w, int16_t h, int16_t up, int16_t upNum);
-static void cGroveEspNowRecvCb(const esp_now_recv_info_t* esp_now_info, const uint8_t* data, uint8_t len, int8_t rssi);
-static void cGroveEspNowSendCb(const uint8_t* mac_addr, esp_now_send_status_t status);
+static void cGroveMenuCB(const char*, bool, uint32_t);
+static void cGroveBackgroundDrawCallback(int16_t, int16_t, int16_t, int16_t, int16_t, int16_t);
+static void cGroveToggleOnlineFeatures(void);
+static void cGroveEspNowRecvCb(const esp_now_recv_info_t*, const uint8_t*, uint8_t, int8_t);
+static void cGroveEspNowSendCb(const uint8_t*, esp_now_send_status_t);
 
 // Strings
 //==============================================================================
-static const char cGroveTitle[] = "Chowa Grove";
-/* 
-// User
+static const char cGroveTitle[] = "Chowa Grove";            // Game title
+
+// Menus
+static const char cGroveStrPlay[] = "Play with Chowa!";     // Go to grove
+static const char cGroveStrCompete[] = "Compete!";          // Go to competition menu
+static const char cGroveStrShops[] = "Go to the shops";     // Go to Shop menu
+static const char cGroveStrOnline[] = "Go Online";          // Access online function Menu
+
+static const char cGroveCompStrFight[] = "Chowa Combat";    // Go to fight
+static const char cGroveCompStrRace[] = "Quadrathalon";     // Go to Race
+static const char cGroveCompStrPerf[] = "Karaoke";          // Go to Performance
+
+static const char cGroveShopStrGroc[] = "Grocery";          // Place to buy food
+static const char cGroveShopStrToy[] = "Toy store";         // Place to buy Toys
+static const char cGroveShopStrBook[] = "Bookstore";        // Place to buy skill books
+static const char cGroveShopStrSchool[] = "School";         // Place to drop of Chowa to increase skills
+
+static const char cGroveOLStrEditProf[] = "Edit Public Profile";            // Allows user to edit their profile
+static const char cGroveOLStrEnable[] = "Enable/Disable online features";   // Enables or disables online 
+
+static const char cGrovePronoun[] = "Pronouns";
+static const char cGroveMood[] = "Mood";
+static const char cGroveUser[] = "Username";
+
 static const char cGrovePronounHe[] = "He/Him";
 static const char cGrovePronounShe[] = "She/Her";
 static const char cGrovePronounThey[] = "They/them";
+static const char cGrovePronounOther[] = "Other";
 
-// NPC data
+/* 
+// Grove
+static const char cGrovePlayStrInteract[] = "Play";         // Pet, pick up, throw, see stats
+static const char cGrovePlayStrGive[] = "Give an item";     // selects an item to drop into Grove
+
+// NPC data FIXME: Convert to struct
 static const char cGroveNPC1Name[] = "Pixel";
 static const char cGroveNPC2Name[] = "Poe";
 static const char cGroveNPC3Name[] = "Pango";
@@ -104,6 +133,7 @@ typedef enum
     HE_HIM,
     SHE_HER,
     THEY_THEM,
+    OTHER
 } Pronouns_t;
 
 // Structs
@@ -151,9 +181,15 @@ typedef struct
 {
     // Swadge data
     uint16_t buttonState;   // State of the buttons
+    bool online;            // True is online, False is offline
 
     // UI data
     uint8_t cursorPos;  // Position of the menu cursor
+
+    // Menu
+    menu_t* cGroveMenu;             // Main menu for cGrove
+    menuLogbookRenderer_t* rndr;    // Logbook renderer
+    font_t menuFont;                 // Menu font
 
     // Chowa Grove data
     playerProfile_t player;                     // Player struct
@@ -183,27 +219,117 @@ typedef struct
 
 cGrove_t* grove = NULL;
 
-// Functions
+// Main Functions
 //==============================================================================
 
 static void cGroveEnterMode(void)
 {
+    // Mode memory allocation
     grove = calloc(1, sizeof(cGrove_t));
+
+    // Load a font
+    loadFont("logbook.font", &grove->menuFont, false);
+
+    // Menu initialization
+    grove->cGroveMenu = initMenu(cGroveTitle, cGroveMenuCB);
+    addSingleItemToMenu(grove->cGroveMenu, cGroveStrPlay);
+    grove->cGroveMenu = startSubMenu(grove->cGroveMenu, cGroveStrCompete);
+    addSingleItemToMenu(grove->cGroveMenu, cGroveCompStrFight);
+    addSingleItemToMenu(grove->cGroveMenu, cGroveCompStrRace);
+    addSingleItemToMenu(grove->cGroveMenu, cGroveCompStrSing);
+    grove->cGroveMenu = endSubMenu(grove->cGroveMenu);
+    grove->cGroveMenu = startSubMenu(grove->cGroveMenu, cGroveStrShops);
+    addSingleItemToMenu(grove->cGroveMenu, cGroveShopStrGroc);
+    addSingleItemToMenu(grove->cGroveMenu, cGroveShopStrToy);
+    addSingleItemToMenu(grove->cGroveMenu, cGroveShopStrBook);
+    addSingleItemToMenu(grove->cGroveMenu, cGroveShopStrSchool);
+    grove->cGroveMenu = endSubMenu(grove->cGroveMenu);
+    grove->cGroveMenu = startSubMenu(grove->cGroveMenu, cGroveStrOnline);
+    grove->cGroveMenu = startSubMenu(grove->cGroveMenu, cGroveOLStrEditProf);
+    addSingleItemToMenu(grove->cGroveMenu, cGroveUser);
+    grove->cGroveMenu = startSubMenu(grove->cGroveMenu, cGrovePronoun);
+    addSingleItemToMenu(grove->cGroveMenu, cGrovePronounHe);
+    addSingleItemToMenu(grove->cGroveMenu, cGrovePronounShe);
+    addSingleItemToMenu(grove->cGroveMenu, cGrovePronounThey);
+    addSingleItemToMenu(grove->cGroveMenu, cGrovePronounOther);
+    grove->cGroveMenu = endSubMenu(grove->cGroveMenu);
+    grove->cGroveMenu = startSubMenu(grove->cGroveMenu, cGroveMood);
+    // TODO: Add mood selections
+    //addSingleItemToMenu(grove->cGroveMenu, );
+    grove->cGroveMenu = endSubMenu(grove->cGroveMenu);
+    grove->cGroveMenu = endSubMenu(grove->cGroveMenu);
+    addSingleItemToMenu(grove->cGroveMenu, cGroveOLStrEnable);
+    grove->cGroveMenu = endSubMenu(grove->cGroveMenu);
+    grove->rndr = initMenuLogbookRenderer(&grove->menuFont);
 }
 
 static void cGroveExitMode(void)
 {
+    deinitMenu(grove->cGroveMenu);
+    deinitMenuLogbookRenderer(grove->rndr);
+    freeFont(&grove->menuFont);
     free(grove);
 }
 
 static void cGroveMainLoop(int64_t elapsedUs) 
 {
+    // Main Menu
+    buttonEvt_t evt = {0};
+    while(checkButtonQueueWrapper(&evt))
+    {
+        grove->cGroveMenu = menuButton(grove->cGroveMenu, evt);
+    }
+    drawMenuLogbook(grove->cGroveMenu, grove->rndr, 0);
+}
+
+static void cGroveMenuCB(const char* label, bool selected, uint32_t settingVal)
+{
+    if (selected)
+    {
+        if (label == cGroveStrPlay) {
+            // TODO: Load Grove
+        } else if (label == cGroveCompStrFight) {
+            // TODO: Load Fight
+        } else if (label == cGroveCompStrRace) {
+            // TODO: Load Race
+        } else if (label == cGroveCompStrPerf) {
+            // TODO: Load Performance
+        } else if (label == cGroveShopStrGroc) {
+            // TODO: Load Store with grocery
+        } else if (label == cGroveShopStrToy) {
+            // TODO: Load Store with toys
+        } else if (label == cGroveShopStrBook) {
+            // TODO: Load Store with books
+        } else if (label == cGroveShopStrSchool) {
+            // TODO: Load School
+        } else if (label == cGroveUser) {
+            // TODO: Load Text entry
+        } else if (label == cGrovePronounHe) {
+            grove->player.pronouns = HE_HIM;
+        } else if (label == cGrovePronounShe) {
+            grove->player.pronouns = SHE_HER;
+        } else if (label == cGrovePronounThey) {
+            grove->player.pronouns = THEY_THEM;
+        } else if (label == cGrovePronounOther) {
+            grove->player.pronouns = OTHER;
+        } else if (label == cGroveOLStrEnable) {
+            cGroveToggleOnlineFeatures();
+        }
+    }
+}
+
+// Graphics Functions
+//==============================================================================
+static void cGroveBackgroundDrawCallback(int16_t x, int16_t y, int16_t w, int16_t h, int16_t up, int16_t upNum)
+{
     
 }
 
-static void cGroveBackgroundDrawCallback(int16_t x, int16_t y, int16_t w, int16_t h, int16_t up, int16_t upNum)
-{
-
+// Wireless functions
+//==============================================================================
+static void cGroveToggleOnlineFeatures(){
+    grove->online = !grove->online;
+    // TODO: Turn on and off online features
 }
 
 static void cGroveEspNowRecvCb(const esp_now_recv_info_t* esp_now_info, const uint8_t* data, uint8_t len, int8_t rssi)
