40c66a1bcd042a8abd91615974cac81505231229
hunterdyar <bloop@hdyar.com>
2024-05-12 13:00:35 -0400

    Undo Support

--- main/modes/games/soko/soko_consts.h
+++ main/modes/games/soko/soko_consts.h
@@ -8,5 +8,6 @@
 #define SOKO_MAX_GOALS        20
 #define SOKO_MAX_REDIRECTS    15 // Should be equal to MAX_ENTITY until I find an edge case
 #define SOKO_VICTORY_TIMER_US 1000000
+#define SOKO_UNDO_BUFFER_SIZE 255
 
 #endif // SOKO_CONSTS_H
