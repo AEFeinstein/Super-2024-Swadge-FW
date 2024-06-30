ac229230cd5209774cd89e1dbf5a3d9c1aed3c10
Dylan Whichard <dylan@whichard.com>
2024-06-30 15:30:59 -0700

    Remove random file

--- emulator/idf-inc/esp_timer_emu.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void emuSetUseRealTime(bool useRealTime);
-void emuSetEspTimerTime(int64_t timeUs);
-void emuTimerPause(void);
-void emuTimerUnpause(void);
-bool emuTimerIsPaused(void);
