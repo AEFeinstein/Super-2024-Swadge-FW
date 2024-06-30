fc3f671e1c3d47eea597cebb5c6c8f9db9574fbc
JohnnyWycliffe <johnnywycliffe@gmail.com>
2024-06-30 19:21:37 -0400

    Added in Game over, win, and game start screens

--- main/modes/games/2048/mode_2048.c
+++ main/modes/games/2048/mode_2048.c
@@ -14,7 +14,11 @@
 // Variables
 //==============================================================================
 
-const char modeName[] = "2048";
+const char modeName[]   = "2048";
+const char pressKey[]   = "Press any key to play";
+const char pressAB[]    = "Press A or B to reset the game";
+const char youWin[]     = "You got 2048!";
+const char continueAB[] = "Press A or B to continue";
 
 swadgeMode_t t48Mode = {
     .modeName                 = modeName,
@@ -39,63 +43,91 @@ t48_t* t48;
 // Functions
 //==============================================================================
 
-/**
- * @brief Mode setup
- * 
- */
 static void t48EnterMode(void)
 {
     setFrameRateUs(T48_US_PER_FRAME);
     t48 = calloc(sizeof(t48_t), 1);
     loadFont("ibm_vga8.font", &t48->font, false);
+    loadFont("sonic.font", &t48->titleFont, false);
 
+    t48->score = 0;
+    t48->ds = GAMESTART;
     t48StartGame();  // First run only adds one block... so just run it twice!
-    t48StartGame();
 }
 
-/**
- * @brief Mode teardown
- * 
- */
 static void t48ExitMode(void)
 {
+    freeFont(&t48->titleFont);
     freeFont(&t48->font);
     free(t48);
 }
 
-/**
- * @brief Main loop of the code
- * 
- * @param elapsedUs 
- */
 static void t48MainLoop(int64_t elapsedUs)
 {
-    if(t48CheckWin()){
-
-    }
-
-    if (t48CheckOver()){
-        // FIXME: does not actually reset?
-        t48StartGame();
-    }
-    // Input
     buttonEvt_t evt;
-    while (checkButtonQueueWrapper(&evt))
-    {
-        if (evt.down && evt.button & PB_A){
-            t48SetRandCell();
-        }
+    switch(t48->ds){
+        case GAMESTART:
+            // Check any button is pressed
+            while (checkButtonQueueWrapper(&evt))
+            {
+                if (evt.down){
+                    t48StartGame();
+                    t48->ds = GAME;
+                }
+            }
+            // Draw
+            t48StartScreen(c550);   // TODO: Make a rainbow effect
+            break;
+        case GAME:
+            // Check game is done or "done"
+            if(t48CheckWin()){
+                t48->ds = WIN;
+            }
+            if (t48CheckOver()){
+                t48->ds = GAMEOVER;
+            }
+            // Input
+            while (checkButtonQueueWrapper(&evt))
+            {
+                // TODO: Write input logic
+                // TODO: Add merge mechanic
+                if (evt.down && evt.button & PB_A){
+                    t48SetRandCell();
+                }
+            }
+            // Draw
+            t48Draw();
+            break;
+        case GAMEOVER:
+            // Check any button is pressed
+            while (checkButtonQueueWrapper(&evt))
+            {
+                if (evt.down && (evt.button & PB_A || evt.button & PB_B)){
+                    t48StartGame();
+                    t48->ds = GAME;
+                }
+            }
+            // Draw
+            t48DrawGameOverScreen(t48->score);
+            break;
+        case WIN:
+            // Check any button is pressed
+            while (checkButtonQueueWrapper(&evt))
+            {
+                if (evt.down && (evt.button & PB_A || evt.button & PB_B)){
+                    t48->ds = GAME;
+                }
+            }
+            // Draw
+            t48DrawWinScreen();
+            break;
+        default:
+            break;
     }
-
-    // Draw
-    t48Draw();
 }
 
-/**
- * @brief Sets an empty cell to 2 or 4 on 50/50 basis
- * 
- * @return int Cell used
- */
+// Game functions
+
 static int t48SetRandCell()
 {
     int8_t cell = -1;
@@ -106,10 +138,8 @@ static int t48SetRandCell()
     return cell;
 }
 
-/**
- * @brief Initializes a game
- * 
- */
+// Game state
+
 static void t48StartGame()
 {
     // clear the board
@@ -117,17 +147,12 @@ static void t48StartGame()
         t48->boardArr[i / GRID_SIZE][i % GRID_SIZE] = 0;
     }
     t48->alreadyWon = false;
+    t48->score = 0;
     // Get random places to start
     t48SetRandCell();
     t48SetRandCell();
 }
 
-/**
- * @brief Checks if the player has reached 2048 for tyhee first time
- * 
- * @return true     If this is the first time 2048 has been hit
- * @return false    Otherwise
- */
 static bool t48CheckWin()
 {
     if(t48->alreadyWon){
@@ -142,12 +167,6 @@ static bool t48CheckWin()
     return false;
 }
 
-/**
- * @brief Checks if the game can no longer be played
- * 
- * @return true     Display final score and reset
- * @return false    Continue playing
- */
 static bool t48CheckOver()
 {
     // Check if any cells are open
@@ -176,70 +195,8 @@ static bool t48CheckOver()
     return true;
 }
 
-/**
- * @brief Draws the grid, score, and tiles
- * 
- */
-static void t48Draw()
-{   
-    // Blank
-    fillDisplayArea(0, 0, TFT_WIDTH, TFT_HEIGHT, c000);
-    
-    // Draw vertrical grid line
-    for(int i = 0; i < 5; i++){
-        int16_t left = i * (T48_CELL_SIZE + T48_LINE_WEIGHT);
-        fillDisplayArea(SIDE_MARGIN + left, TOP_MARGIN, SIDE_MARGIN + left + T48_LINE_WEIGHT, TFT_HEIGHT, c111);
-    }
-    
-    // Draw horizontal grid lines
-    for(int i = 0; i < 5; i++){
-        int16_t top = i * (T48_CELL_SIZE + T48_LINE_WEIGHT);
-        fillDisplayArea(SIDE_MARGIN, top + TOP_MARGIN, TFT_WIDTH - SIDE_MARGIN, \
-        top + TOP_MARGIN + T48_LINE_WEIGHT, c111);
-    }
+// Visuals
 
-    // Score
-    static char textBuffer[32];
-    snprintf(textBuffer, sizeof(textBuffer)-1, "Score: %" PRIu32, t48->score);
-    strcpy(t48->scoreStr, textBuffer);
-    drawText(&t48->font, c555, t48->scoreStr, SIDE_MARGIN, 4);
-
-    // Cells
-    int16_t side_offset = SIDE_MARGIN + T48_LINE_WEIGHT;
-    int16_t top_offset = TOP_MARGIN + T48_LINE_WEIGHT;
-    static char buffer[16];
-    for (int col = 0; col < GRID_SIZE; col++){
-        for (int row = 0; row < GRID_SIZE; row++){
-            // Grab the current value of the cell
-            uint32_t val = t48->boardArr[col][row];
-            // Bail if 0
-            if (val == 0) {continue;}
-            // Grab the offest based on cell
-            uint16_t y_cell_offset = col * (T48_CELL_SIZE + T48_LINE_WEIGHT);
-            uint16_t x_cell_offset = row * (T48_CELL_SIZE + T48_LINE_WEIGHT);
-            // Convert int to char
-            snprintf(buffer, sizeof(buffer)-1, "%" PRIu32, val);
-            // Get the center of the text
-            uint16_t text_center = (textWidth(&t48->font, buffer))/2;
-            // Get associated color
-            uint8_t color = getColor(val);
-            fillDisplayArea(side_offset + x_cell_offset, top_offset + y_cell_offset, 
-                            side_offset + x_cell_offset + T48_CELL_SIZE, 
-                            top_offset + y_cell_offset + T48_CELL_SIZE, color);
-            // Draw the text
-            drawText(&t48->font, c555, buffer, 
-            side_offset + x_cell_offset - text_center + T48_CELL_SIZE/2, 
-            top_offset + y_cell_offset - 4 + T48_CELL_SIZE/2);
-        }
-    }
-}
-
-/**
- * @brief Get the Color for a specific value
- * 
- * @param val       Value that requires a color for its square
- * @return uint8_t  Color of the value's square
- */
 static uint8_t getColor(uint32_t val)
 {
     switch(val){
@@ -301,4 +258,95 @@ static uint8_t getColor(uint32_t val)
             return c101;
             break;
     }
+}
+
+static void t48Draw()
+{   
+    // Blank
+    fillDisplayArea(0, 0, TFT_WIDTH, TFT_HEIGHT, c000);
+    
+    // Draw vertrical grid line
+    for(int i = 0; i < 5; i++){
+        int16_t left = i * (T48_CELL_SIZE + T48_LINE_WEIGHT);
+        fillDisplayArea(SIDE_MARGIN + left, TOP_MARGIN, SIDE_MARGIN + left + T48_LINE_WEIGHT, TFT_HEIGHT, c111);
+    }
+    
+    // Draw horizontal grid lines
+    for(int i = 0; i < 5; i++){
+        int16_t top = i * (T48_CELL_SIZE + T48_LINE_WEIGHT);
+        fillDisplayArea(SIDE_MARGIN, top + TOP_MARGIN, TFT_WIDTH - SIDE_MARGIN, \
+        top + TOP_MARGIN + T48_LINE_WEIGHT, c111);
+    }
+
+    // Score
+    static char textBuffer[32];
+    snprintf(textBuffer, sizeof(textBuffer)-1, "Score: %" PRIu32, t48->score);
+    strcpy(t48->scoreStr, textBuffer);
+    drawText(&t48->font, c555, t48->scoreStr, SIDE_MARGIN, 4);
+
+    // Cells
+    int16_t side_offset = SIDE_MARGIN + T48_LINE_WEIGHT;
+    int16_t top_offset = TOP_MARGIN + T48_LINE_WEIGHT;
+    static char buffer[16];
+    for (int col = 0; col < GRID_SIZE; col++){
+        for (int row = 0; row < GRID_SIZE; row++){
+            // Grab the current value of the cell
+            uint32_t val = t48->boardArr[col][row];
+            // Bail if 0
+            if (val == 0) {continue;}
+            // Grab the offest based on cell
+            uint16_t y_cell_offset = col * (T48_CELL_SIZE + T48_LINE_WEIGHT);
+            uint16_t x_cell_offset = row * (T48_CELL_SIZE + T48_LINE_WEIGHT);
+            // Convert int to char
+            snprintf(buffer, sizeof(buffer)-1, "%" PRIu32, val);
+            // Get the center of the text
+            uint16_t text_center = (textWidth(&t48->font, buffer))/2;
+            // Get associated color
+            uint8_t color = getColor(val);
+            fillDisplayArea(side_offset + x_cell_offset, top_offset + y_cell_offset, 
+                            side_offset + x_cell_offset + T48_CELL_SIZE, 
+                            top_offset + y_cell_offset + T48_CELL_SIZE, color);
+            // Draw the text
+            drawText(&t48->font, c555, buffer, 
+                     side_offset + x_cell_offset - text_center + T48_CELL_SIZE/2, 
+                     top_offset + y_cell_offset - 4 + T48_CELL_SIZE/2);
+        }
+    }
+}
+
+static void t48StartScreen(uint8_t color)
+{
+    // Blank
+    fillDisplayArea(0, 0, TFT_WIDTH, TFT_HEIGHT, c000);
+    // Title
+    drawText(&t48->titleFont, color, modeName, 
+             (TFT_WIDTH - textWidth(&t48->titleFont, modeName))/2, 
+             TFT_HEIGHT/2 - 12);
+    // Press any key...
+    drawText(&t48->font, c555, pressKey, 
+             (TFT_WIDTH - textWidth(&t48->font, pressKey))/2, TFT_HEIGHT - 64);
+}
+
+static void t48DrawGameOverScreen(int64_t score)
+{
+    // Blank
+    fillDisplayArea(0, 0, TFT_WIDTH, TFT_HEIGHT, c000);
+
+    // Display final score
+    static char textBuffer[32];
+    snprintf(textBuffer, sizeof(textBuffer)-1, "Final score: %" PRIu64, score);
+    drawText(&t48->titleFont, c550, textBuffer, 16, TFT_HEIGHT/2 - 24);
+    drawText(&t48->font, c555, pressAB, 16, TFT_HEIGHT/2 + 24);
+}
+
+static void t48DrawWinScreen(void)
+{
+    // Blank
+    fillDisplayArea(0, 0, TFT_WIDTH, TFT_HEIGHT, c000);
+    // Title
+    drawText(&t48->titleFont, c055, youWin, 
+             (TFT_WIDTH - textWidth(&t48->titleFont, youWin))/2, 48);
+    // Press any key...
+    drawText(&t48->font, c555, continueAB, 
+             (TFT_WIDTH - textWidth(&t48->font, continueAB))/2, TFT_HEIGHT - 64);
 }
--- main/modes/games/2048/mode_2048.h
+++ main/modes/games/2048/mode_2048.h
@@ -38,6 +38,13 @@
 // Enums
 //==============================================================================
 
+typedef enum{
+    GAME,       // Game running
+    WIN,        // Display a win
+    GAMEOVER,   // Display final score and prompt a restart
+    GAMESTART,  // Splash screen after load
+} DisplayState_t;
+
 //==============================================================================
 // Structs
 //==============================================================================
@@ -47,22 +54,96 @@ typedef struct{
     uint32_t score;
     int16_t presses;
     font_t font;
+    font_t titleFont;
     char scoreStr[16];
     bool alreadyWon;
+    DisplayState_t ds;
 } t48_t;
 
 //==============================================================================
 // Function Prototypes
 //==============================================================================
 
+/**
+ * @brief Mode setup
+ * 
+ */
 static void t48EnterMode(void);
+
+/**
+ * @brief Mode teardown
+ * 
+ */
 static void t48ExitMode(void);
+
+/**
+ * @brief Main loop of the code
+ * 
+ * @param elapsedUs 
+ */
 static void t48MainLoop(int64_t elapsedUs);
+
+/**
+ * @brief Sets an empty cell to 2 or 4 on 50/50 basis
+ * 
+ * @return int Cell used
+ */
 static int t48SetRandCell(void);
+
+/**
+ * @brief Initializes a game
+ * 
+ */
 static void t48StartGame(void);
+
+/**
+ * @brief Checks if the player has reached 2048 for tyhee first time
+ * 
+ * @return true     If this is the first time 2048 has been hit
+ * @return false    Otherwise
+ */
 static bool t48CheckWin(void);
+
+/**
+ * @brief Checks if the game can no longer be played
+ * 
+ * @return true     Display final score and reset
+ * @return false    Continue playing
+ */
 static bool t48CheckOver(void);
+
+/**
+ * @brief Draws the grid, score, and tiles
+ * 
+ */
 static void t48Draw(void);
+
+/**
+ * @brief Shows the title upon booting into the mode
+ * 
+ * @param color Color of tyhe title text
+ */
+static void t48StartScreen(uint8_t color);
+
+/**
+ * @brief Draws the final score and prompts the player to play again
+ * 
+ * @param score The final value of the game
+ */
+static void t48DrawGameOverScreen(int64_t score);
+
+/**
+ * @brief Draw the win screen. It doesn't do anythiung else.
+ * 
+ */
+static void t48DrawWinScreen(void);
+
+/**
+ * @brief Get the Color for a specific value
+ * 
+ * @param val       Value that requires a color for its square
+ * @return uint8_t  Color of the value's square
+ */
 static uint8_t getColor(uint32_t val);
 
 //==============================================================================
