82001515094a376d6b55125883fdd3513aaa9838
Dylan Whichard <dylan@whichard.com>
2024-07-05 14:22:27 -0700

    Don't forget the path too

--- emulator/src-lib/platform-midi/platform_midi_coremidi.h
+++ emulator/src-lib/platform-midi/platform_midi_coremidi.h
@@ -21,7 +21,7 @@ int platform_midi_write_coremidi(unsigned char* buf, int size);
 #include <string.h>
 
 #include <MacTypes.h>
-#include <CoreMIDI.h>
+#include <CoreMIDI/CoreMIDI.h>
 
 #ifndef PLATFORM_MIDI_EVENT_BUFFER_ITEMS
 #define PLATFORM_MIDI_EVENT_BUFFER_ITEMS 32
