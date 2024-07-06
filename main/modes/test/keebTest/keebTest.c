8c8ec7a64536bbd29c909a5bc3e8d9bca0643bb6
JohnnyWycliffe <johnnywycliffe@gmail.com>
2024-07-06 13:33:24 -0400

    Refactored keeb test mode

--- main/modes/test/keebTest/keebTest.c
+++ main/modes/test/keebTest/keebTest.c
@@ -1,25 +1,26 @@
-// Includes
-//==============================================================================
+/**
+ * @file keebTest.c
+ * @author Jeremy Stintzcum (jeremy.stintzcum@gmail.com)
+ * @brief A mode designed to test keyboard variations in rapid succession 
+ * @version 1.0
+ * @date 2024-07-06
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 
+#include "keebTest.h"
 #include "textEntry.h"
 
+//==============================================================================
 // Function Prototypes
 //==============================================================================
-void keebEnterMode(void);
-void keebExitMode(void);
-void keebMainLoop(int64_t elapsedUs);
+static void keebEnterMode(void);
+static void keebExitMode(void);
+static void keebMainLoop(int64_t elapsedUs);
 
 // Variables
 //==============================================================================
-#define MAX_TEXT_LEN 128
-
-typedef struct
-{
-    wsg_t bg;
-    font_t fnt;
-    bool displayText;
-    char typedText[MAX_TEXT_LEN];
-} keebTest_t;
 
 const char keebTestName[] = "Keyboard Test";
 
@@ -45,40 +46,40 @@ keebTest_t* kbTest;
 // Function definitions
 //==============================================================================
 
-void keebEnterMode(void)
+static void keebEnterMode(void)
 {
     kbTest = (keebTest_t*)calloc(1, sizeof(keebTest_t));
-    loadWsg("menu_bg.wsg", &kbTest->bg, false);
-    loadFont("ibm_vga8.font", &kbTest->fnt, false);
-    //loadFont("radiostars.font", &kbTest->fnt, false);
+    loadWsg("kid0.wsg", &kbTest->bg, false);
+    loadFont("ibm_vga8.font", &kbTest->fnt1, false);
+    loadFont("radiostars.font", &kbTest->fnt2, false);
     kbTest->displayText = true;
-    strcpy(kbTest->typedText, "Press any key");
 }
 
-void keebExitMode(void)
+static void keebExitMode(void)
 {
-    freeFont(&kbTest->fnt);
+    freeFont(&kbTest->fnt1);
+    freeFont(&kbTest->fnt2);
     freeWsg(&kbTest->bg);
     free(kbTest);
 }
 
-void keebMainLoop(int64_t elapsedUs)
+static void keebMainLoop(int64_t elapsedUs)
 {
     if (kbTest->displayText)
     {
-        int16_t width = textWidth(&kbTest->fnt, kbTest->typedText);
+        int16_t width = textWidth(&kbTest->fnt1, kbTest->typedText);
         fillDisplayArea(0, 0, TFT_WIDTH, TFT_HEIGHT, c111);
         int16_t xOff = (TFT_WIDTH - width) / 2;
-        int16_t yOff = (TFT_HEIGHT - kbTest->fnt.height) / 2;
-        drawTextWordWrap(&kbTest->fnt, c555, kbTest->typedText, &xOff, &yOff, 180, 60);
-        drawText(&kbTest->fnt, c555, kbTest->typedText, xOff, yOff);
+        int16_t yOff = (TFT_HEIGHT - kbTest->fnt1.height) / 2;
+        drawTextWordWrap(&kbTest->fnt1, c555, kbTest->typedText, &xOff, &yOff, 180, 60);
+        drawText(&kbTest->fnt1, c555, kbTest->typedText, xOff, yOff);
         buttonEvt_t evt = {0};
         while (checkButtonQueueWrapper(&evt))
         {
             if (evt.down)
             {
                 kbTest->displayText = false;
-                textEntryInit(&kbTest->fnt, MAX_TEXT_LEN + 1, kbTest->typedText);
+                textEntryInit(&kbTest->fnt1, MAX_TEXT_LEN + 1, kbTest->typedText);
                 textEntrySetTextColor(c555, false);
                 textEntrySetEmphasisColor(c500);
                 textEntrySetShadowboxColor(true, c111);
--- main/modes/test/keebTest/keebTest.h
+++ main/modes/test/keebTest/keebTest.h
@@ -1,10 +1,38 @@
-#ifndef _KEEBTEST_H_
-#define _KEEBTEST_H_
+/**
+ * @file keebTest.h
+ * @author Jeremy Stintzcum (jeremy.stintzcum@gmail.com)
+ * @brief A mode designed to test keyboard variations in rapid succession 
+ * @version 1.0
+ * @date 2024-07-06
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#pragma once
+
+//==============================================================================
+// Includes
+//==============================================================================
 
 #include "swadge2024.h"
 
+//==============================================================================
+// Defines
+//==============================================================================
+
+#define MAX_TEXT_LEN 255
+
+//==============================================================================
+// Structs
+//==============================================================================
+
+typedef struct
+{
+    wsg_t bg;
+    font_t fnt1;
+    font_t fnt2;
+    bool displayText;
+    char typedText[MAX_TEXT_LEN];
+} keebTest_t;
+
 extern swadgeMode_t keebTestMode;
-
-extern const char keebTestName[];
-
-#endif
