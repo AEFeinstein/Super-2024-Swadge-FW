fbccc477cf40e764c574b420764b31fe11a01d88
JohnnyWycliffe <johnnywycliffe@gmail.com>
2024-07-08 22:51:32 -0400

    Updated version number, added text to documentation

--- main/modes/test/keebTest/keebTest.h
+++ main/modes/test/keebTest/keebTest.h
@@ -2,7 +2,7 @@
  * @file keebTest.h
  * @author Jeremy Stintzcum (jeremy.stintzcum@gmail.com)
  * @brief A mode designed to test keyboard variations in rapid succession
- * @version 1.0
+ * @version 1.1
  * @date 2024-07-06
  *
  * @copyright Copyright (c) 2024
