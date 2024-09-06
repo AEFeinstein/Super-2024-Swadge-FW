f63305b4c5d27d56c9c4319e66fb07765962c704
JVeg199X <97848253+JVeg199X@users.noreply.github.com>
2024-09-05 23:28:53 -0400

    Hud adjustment

--- main/modes/games/pango/pango.c
+++ main/modes/games/pango/pango.c
@@ -350,10 +350,10 @@ void drawPangoHud(font_t* font, paGameData_t* gameData)
     }
 
     drawText(font, c553, livesStr, 56, 2);
-    drawText(font, c553, coinStr, 160, 224);
+    //drawText(font, c553, coinStr, 160, 224);
     drawText(font, c553, scoreStr, 112, 2);
     drawText(font, c553, levelStr, 24, 224);
-    drawText(font, (gameData->countdown > 30) ? c553 : redColors[(gameData->frameCount >> 3) % 4], timeStr, 212, 224);
+    drawText(font, (gameData->countdown > 30) ? c553 : redColors[(gameData->frameCount >> 3) % 4], timeStr, 208, 224);
 
     if (gameData->comboTimer == 0)
     {
