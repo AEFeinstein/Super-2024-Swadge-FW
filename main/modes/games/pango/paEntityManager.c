57742ef08e87880c1e9fd541227397a549260441
JVeg199X <97848253+JVeg199X@users.noreply.github.com>
2024-10-25 23:24:44 -0400

    Fix "level ends with 1 enemy left" and "softlock: level does not end when all enemies are killed"

--- main/modes/games/pango/paEntityManager.c
+++ main/modes/games/pango/paEntityManager.c
@@ -68,22 +68,17 @@ void pa_deactivateAllEntities(paEntityManager_t* entityManager, bool excludePlay
     for (uint8_t i = 0; i < MAX_ENTITIES; i++)
     {
         paEntity_t* currentEntity = &(entityManager->entities[i]);
-        currentEntity->active     = false;
 
-        // clear out invisible block tiles that are placed for every Break Block object
-        // if(currentEntity->type == PA_ENTITY_BREAK_BLOCK){
-        //     pa_setTile(currentEntity->tilemap, PA_TO_TILECOORDS(currentEntity->x >> SUBPIXEL_RESOLUTION),
-        //     PA_TO_TILECOORDS(currentEntity->y >> SUBPIXEL_RESOLUTION), PA_TILE_EMPTY);
-        // }
+        if (currentEntity->active == false || (excludePlayer && currentEntity == entityManager->playerEntity))
+        {
+            continue;
+        }
+
+        currentEntity->active     = false;
 
         if (currentEntity->type == ENTITY_HIT_BLOCK && currentEntity->state == PA_TILE_SPAWN_BLOCK_0)
         {
             entityManager->gameData->remainingEnemies--;
-        }
-
-        if (excludePlayer && currentEntity == entityManager->playerEntity)
-        {
-            currentEntity->active = true;
         }
     }
 
