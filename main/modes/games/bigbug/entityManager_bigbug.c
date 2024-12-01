66013d918d877621c96c161d29f6a5331f98fd41
DebrisHauler <jla21@att.net>
2024-12-01 03:00:09 -0500

    more fuel

--- main/modes/games/bigbug/entityManager_bigbug.c
+++ main/modes/games/bigbug/entityManager_bigbug.c
@@ -600,7 +600,7 @@ bb_entity_t* bb_createEntity(bb_entityManager_t* entityManager, bb_animationType
         {
             bb_garbotnikData_t* gData = heap_caps_calloc(1, sizeof(bb_garbotnikData_t), MALLOC_CAP_SPIRAM);
             gData->numHarpoons        = 250;
-            gData->fuel  = 1000 * 60 * 1; // 1 thousand milliseconds in a second. 60 seconds in a minute. 1 minutes.
+            gData->fuel  = 1000 * 60 * 3; // 1 thousand milliseconds in a second. 60 seconds in a minute. 3 minutes.
             gData->yaw.x = -1;            // So he starts off facing left away from the tutorial egg.
             int16_t arraySize = sizeof(gData->landingPhrases) / sizeof(gData->landingPhrases[0]);
             // create sequential numbers of all phrase indices
