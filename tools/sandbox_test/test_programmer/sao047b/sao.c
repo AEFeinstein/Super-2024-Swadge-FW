a63faab4fafcaa74539001bf8fcf07d185780651
cnlohr <lohr85@gmail.com>
2025-01-10 03:40:16 -0500

    Add some basic swadgey stuff

---
+++ tools/sandbox_test/test_programmer/sao047b/sao.c
@@ -0,0 +1,62 @@
+#include "ch32v003fun.h"
+#include <stdio.h>
+#include <string.h>
+
+#include "graphics.h"
+
+
+int frameno;
+
+int main()
+{
+	SystemInit();
+
+	funGpioInitAll();
+	funPinMode( PD0, GPIO_CFGLR_OUT_50Mhz_PP );
+	funDigitalWrite( PD0, FUN_LOW );
+
+	// button setup
+	funPinMode( PA1, GPIO_CFGLR_IN_PUPD );
+	funPinMode( PA2, GPIO_CFGLR_IN_PUPD );
+	funDigitalWrite( PA1, FUN_HIGH );
+	funDigitalWrite( PA2, FUN_HIGH );
+
+	printf( "Setup Start\n" );
+
+	ssd1306_rst();
+	if(ssd1306_spi_init())
+	{
+		printf( "Could not connect to OLED\n" );
+	}
+	ssd1306_init();
+
+	ssd1306_cmd( SSD1306_SETMULTIPLEX );
+	ssd1306_cmd( 39 );
+	ssd1306_cmd( SSD1306_SETDISPLAYOFFSET );
+	ssd1306_cmd( 0 );
+
+	ssd1306_cmd( SSD1306_SETPRECHARGE ); ssd1306_cmd( 0xf1 );
+	ssd1306_cmd( SSD1306_SETCONTRAST ); ssd1306_cmd( 0xf1 );
+	ssd1306_cmd( SSD1306_SETVCOMDETECT ); ssd1306_cmd( 0x40 );
+	ssd1306_cmd( 0xad ); ssd1306_cmd( 0x90 ); // Set Charge pump (set to 0x90 for extra bright)
+
+	int i;
+
+
+	printf( "Setup Complete\n" );
+	while(1)
+	{
+
+		swadgeDraw( SSD1306_W/2, 0, 1, swadgeGlyphHalf, "10GRAND" );
+//		swadgeDraw( SSD1306_W/2, 16, 1, swadgeGlyph, "%d", frameno );
+
+		swapBuffer();
+
+
+		background(6);
+
+		frameno++;
+	}
+}
+
+
