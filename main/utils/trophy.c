179c7d7cb78676dd8bd7e248f42630ea1b0dadf4
Jeremy Stintzcum <johnnywycliffe@gmail.com>
2025-01-14 00:49:45 -0500

    Initial commit. Documentation mostly done, some of the drawing code has been started.

---
+++ main/utils/trophy.c
@@ -0,0 +1,387 @@
+/**
+ * @file trophy.c
+ * @author Jeremy.Stintzcum@gmail.com
+ * @brief Trophies for swadge modes
+ * @version 0.1
+ * @date 2025-01-13
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+//==============================================================================
+// Includes
+//==============================================================================
+
+#include "trophy.h"
+
+// Hardware
+#include "hdw-nvs.h"
+#include "hdw-tft.h"
+
+// Drawing
+#include "fs_font.h"
+#include "fs_wsg.h"
+#include "fill.h"
+#include "shapes.h"
+
+//==============================================================================
+// Defines
+//==============================================================================
+
+#define MAX_BANNERS     5
+#define TENTH_SECOND    100000
+#define STATIC_POSITION -1
+
+//==============================================================================
+// Consts
+//==============================================================================
+
+static const char* const systemPointsNVS[] = {"trophy", "TotalPoints"};
+
+//==============================================================================
+// Structs
+//==============================================================================
+
+typedef struct
+{
+    // Assets
+    font_t* font;      //< Font in use. Should not be set by user.
+    wsg_t* imageArray; //< WSGs loaded for next five trophies.
+
+    // Drawing
+    trophy_t tBannerList[MAX_BANNERS]; //< List of banners to display.
+    bool active;                       //< If the mode should be drawing a banner
+    bool beingDrawn;                   //< If banner is currently being drawn statically
+    bool drawFromBottom;               //< If banner should be drawn from the bottom of the screen
+    int32_t drawMaxDuration;           //< How long the banner will be drawn fully extended
+    int32_t drawTimer;                 //< Accumulates until more than Max duration
+
+    // Animation
+    bool animated;            //< If being animated
+    bool sliding;             //< If currently sliding
+    int32_t slideMaxDuration; //< How long the banner will be drawn fully extended
+    int32_t slideTimer;       //< Accumulates until more than Max duration
+    int16_t animationTick;    //< What tick of the animation is active based on timer
+} trophySystem_t;
+
+//==============================================================================
+// Variables
+//==============================================================================
+
+static trophySystem_t tSystem; //< Should be one instance per swadge, should always be in memory
+
+//==============================================================================
+// Static function declarations
+//==============================================================================
+
+/**
+ * @brief Saves a trophy. Checks data is actually fresh before saving to avoid hammering NVS
+ *
+ * @param modeName Name of the mode for namespace
+ * @param t Data to write
+ */
+static void _save(char* modeName, trophy_t t);
+
+/**
+ * @brief Loads data from NVS given a string. Returns NULL if not found
+ *
+ * @param modeName Name of the mode
+ * @param title Title of trophy to load
+ * @return trophy_t Trophy data. NULL is not found
+ */
+static trophy_t _load(char* modeName, char* title);
+
+/**
+ * @brief Grabs all trophy data from NVS
+ *
+ * @param modeName Name of the mode to grab data for. NULL is all modes
+ * @param tList Outfile of list of Trophy data from NVS. Will eat RAM
+ * @param tLen Number of trophies to grab
+ * @param offset Where to start in list
+ */
+static void _getTrophyData(char* modeName, trophy_t* tList, int tLen, int offset);
+
+/**
+ * @brief Draws the banner
+ *
+ * @param t Data to feed
+ * @param frame Which frame is being drawn. STATIC_POSITION is fully visible, other values change the offset.
+ */
+static void _draw(trophy_t t, int frame);
+
+//==============================================================================
+// Functions
+//==============================================================================
+
+// System
+
+void trophySystemInit(bool bottom, int displayDuration, bool animate, int slideDuration)
+{
+    // Defaults
+    tSystem.beingDrawn = false;
+    tSystem.drawTimer  = 0;
+    tSystem.slideTimer = 0;
+
+    // Sets the direction the banner comes from
+    tSystem.drawFromBottom = bottom;
+
+    // Set Display duration
+    tSystem.drawMaxDuration = displayDuration;
+
+    // Set if animating
+    tSystem.animated = animate;
+
+    // Set the scroll speed
+    tSystem.slideMaxDuration = slideDuration;
+}
+
+void trophySystemSetFont(char* font)
+{
+    // Sets the system font
+    // NOTE: Should load automatically on Swadge boot/loading a mode
+    loadFont(font, tSystem.font, true);
+}
+
+void trophySystemClearFont()
+{
+    // Unloads the system font
+    // NOTE: Should only be used if the trophy system isn't being used and the font is taking up too much space
+    freeFont(tSystem.font);
+}
+
+int trophySystemGetPoints(char* modeName)
+{
+    // Return the current points value for the mode.
+    int32_t points;
+    if (modeName == NULL)
+    {
+        // Return total points
+        return 0;
+    }
+    if (readNamespaceNvs32(systemPointsNVS[0], modeName, &points))
+    {
+        return points;
+    }
+    else
+    {
+        // No value found
+        return 0;
+    }
+}
+
+// Trophies
+
+void trophyInit(char* modeName, char* title, char* imgStr, char* desc, trophyTypes_t type, int8_t points, int maxVal)
+{
+    // Check if the trophy has already been initialized to avoid overwriting
+    // Save data to NVS
+    // If first trophy (namespace doesn't exist) spawn completionist trophy
+}
+
+void trophyUpdate(char* modeName, char* title, int value, bool drawUpdate)
+{
+    // Check if trophy exists, throw error if not
+    // Check if trophy is already won and bail if true
+    // Check if value is greater than previous value - Do not draw if false
+    // Check if this update has won the trophy and add to points if so
+    // Save value to NVS
+    // If drawUpdate, save trophy to list to display
+    // - Load into next slot in system
+    // - Load WSG
+    // - reset all timers, etc if not already active
+    // - Set system to active if not set
+}
+
+void trophyUpdateMilestone(char* modeName, char* title, int value)
+{
+    // Checks if first time past 25%, 50%, 75%, or 100% completion.
+    // Load saved data
+    // Compare saved MavVal to value
+    // if (value >= maxVal >> 2 && oldValue < maxVal >> 2) || (value >= maxVal >> 1 && oldValue < maxVal >> 1) || (value
+    // >= (maxVal >> 2) * 3 && oldValue < (maxVal >> 2) * 3)
+    // Load into queue to draw
+    // Else, just save data
+}
+
+void trophyClear(char* modeName, char* title)
+{
+    // If Trophy exists, reset it to zero
+    // If was won, delete points
+}
+
+// Helpers
+
+int trophyGetNumTrophies(char* modeName)
+{
+    // Gets the number of trophies associated with this mode, or all modes if NULL
+    return 0;
+}
+
+void trophyGetTrophyList(char* modeName, trophy_t* tList, int* tLen, int offset)
+{
+    // If modeName is NULL, get all trophies. Order by game, but do not sorting.
+    // Populate tList starting from offset up to tLen
+}
+
+trophy_t trophyGetData(char* modeName, char* title)
+{
+    // Check if trophy exists
+    // If so, return trophy_t
+    // Else, return null
+    trophy_t t = {};
+    return t;
+}
+
+// Draw
+
+void trophyDraw(char* modeName, char* title, int64_t elapsedUs)
+{
+    // Exit immediately if not being drawn
+    if (!tSystem.active)
+    {
+        return;
+    }
+    // Draws the trophy
+    if (tSystem.animated && tSystem.sliding) // Sliding in or out
+    {
+        tSystem.slideTimer += elapsedUs;
+        int frameLen = (tSystem.slideMaxDuration * TENTH_SECOND) / TROPHY_BANNER_HEIGHT;
+        while (tSystem.slideTimer >= frameLen)
+        {
+            tSystem.animationTick++;
+            tSystem.slideTimer -= frameLen;
+        }
+        if (tSystem.animationTick == TROPHY_BANNER_HEIGHT)
+        {
+            tSystem.sliding    = false;
+            tSystem.beingDrawn = true;
+        }
+        else if (tSystem.animationTick == TROPHY_BANNER_HEIGHT * 2)
+        {
+            tSystem.sliding = false; // Disables drawing
+            tSystem.active  = false; // Stops rawing altogether
+        }
+        _draw(tSystem.tBannerList[0], tSystem.animationTick);
+    }
+    else if (tSystem.beingDrawn) // Static on screen
+    {
+        // Regular timer
+        tSystem.drawTimer += elapsedUs;
+        if (tSystem.drawTimer >= tSystem.drawMaxDuration)
+        {
+            // Stop drawing
+            tSystem.beingDrawn = false;
+            tSystem.sliding    = tSystem.animated; // Starts sliding again if set
+        }
+        // Draw
+        _draw(tSystem.tBannerList[0], STATIC_POSITION);
+    }
+    else // Has ended
+    {
+        tSystem.active = false;
+    }
+    // TODO: play sound (?)
+}
+
+void trophyDrawListInit(void)
+{
+    // Sets the colors for the trophy screen
+}
+
+void trophyDrawList(char* modeName, int idx)
+{
+    // Draws then entire trophy list starting at idx
+    // Draws check mark nex tto completed trophies
+    // Draws special stuff if all trophies for mode are completed
+}
+
+//==============================================================================
+// Static functions
+//==============================================================================
+
+static void _save(char* modeName, trophy_t t)
+{
+    // Check if the data in the trophy is identical to incoming data, and exit if so
+    // Save the trophy
+}
+
+static trophy_t _load(char* modeName, char* title)
+{
+    // Loads trophy from NVS
+    trophy_t t = {};
+    return t;
+}
+
+static void _getTrophyData(char* modeName, trophy_t* tList, int tLen, int offset)
+{
+    // Load all trophies associated with a mode
+}
+
+static void _draw(trophy_t t, int frame)
+{
+    // Get offset
+    int yOffset;
+    if (frame == STATIC_POSITION)
+    {
+        if (tSystem.drawFromBottom)
+        {
+            yOffset = TFT_HEIGHT - TROPHY_BANNER_HEIGHT;
+        }
+        else
+        {
+            yOffset = 0;
+        }
+    }
+    else
+    {
+        if (frame < TROPHY_BANNER_HEIGHT)
+        {
+            if (tSystem.drawFromBottom)
+            {
+                yOffset = TFT_HEIGHT - frame;
+            }
+            else
+            {
+                yOffset = -TROPHY_BANNER_HEIGHT + frame;
+            }
+        }
+        else
+        {
+            if (tSystem.drawFromBottom)
+            {
+                yOffset = TFT_HEIGHT - (TROPHY_BANNER_HEIGHT * 2 - frame);
+            }
+            else
+            {
+                yOffset = TROPHY_BANNER_HEIGHT - frame;
+            }
+        }
+    }
+    // TODO: Move to secondary sub function so the draw code can be referenced in the list draw
+
+    // Draw box (Gray box, black border)
+    fillDisplayArea(0, yOffset, TFT_WIDTH, TROPHY_BANNER_HEIGHT, c111);
+    drawRect(0, yOffset, TFT_WIDTH, TROPHY_BANNER_HEIGHT, c000);
+    // Draw image if not NULL
+    int xOffset;
+    if (t.imageString == NULL)
+    {
+        // Draw text at start of buffer area
+        xOffset = TROPHY_BANNER_ICON_H_BUFFER;
+    }
+    else
+    {
+        // Draw icon
+        // TODO: If in progress, draw grayscale? Outline?
+        // Set xOffset to be 2* H_BUFFER + ICON_WIDTH
+        xOffset = TROPHY_BANNER_MAX_ICON_DIM + 2 * TROPHY_BANNER_ICON_H_BUFFER;
+    }
+    // Draw text, starting after image if present
+    // TODO: Fill out text draw commands
+    // drawText(); // Title
+    // drawText(); // Description
+
+    // If not TYPE_TRIGGER
+    // drawText(); // Curr Val / Max Val
+}
---
+++ main/utils/trophy.h
@@ -0,0 +1,342 @@
+/**
+ * @file trophy.h
+ * @author Jeremy.Stintzcum@gmail.com
+ * @brief Trophies for swadge modes
+ * @version 0.1
+ * @date 2025-01-13
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+/*! \file trophy.h
+ *
+ * \section trophy_overview Overview
+ *
+ * The trophy system should be almost 100% handled by five easy to use functions. This system is designed to be as
+ * compact as possible without compromising usability while also not hammering the NVS with every update, or requiring
+ * more overhead. Each trophy has options as shown in the initializations section.
+ *
+ * Each trophy has a "point" value associated with it which is totalled up to a "Gamer Score" type value to compare with
+ * friends.
+ *
+ * \section trophy_system The trophy system
+ *
+ * It is recommended to call `trophySystemInit()` in the mode entry function in case other modes have been accessed
+ * which changed the settings.
+ *
+ * Options:
+ * - Banner Top/Bottom: Choose if the banner is viewed from the top or bottom.
+ * - Display Duration: How long the banners display by. Units is tenths of a second.
+ * - Animate Banners: If the banners should animate by default
+ * - Slide Duration: How long the banner should take to slide in. Units is tenths of a second.
+ *
+ * Call `trophySystemGetPoints()` to retrieve the number of points the player has per mode. Use NULL to get points for
+ * whole system.
+ *
+ * \section trophy_init Initialization of each individual trophy
+ *
+ * Call `trophyInit()` near the start of the program. Only initializes once, so it can be put anywhere in the code
+ * without worry, so long as it's called before the player tries to operate on it. Recommendation is in the mode entry
+ * function so it is fully initialized before the player can run any inputs to avoid errors.
+ *
+ * Options:
+ * - Title: The name of the trophy. Make it unique! This is used as the identifier and having two of the same names will
+ * confuse the program. There's no need to worry about sharing the name with other modes though, that's handled
+ * internally.
+ * - Description: Displays text to help the player figure out how to get the trophy. Text-box size is limited, and too
+ * long descriptions will get truncated. Descriptions can be off-topic, but may confuse players.
+ * - Image: Provide a 36x36 sprite that will be displayed. If "NULL" is provided, will not draw and will give more space
+ * for text
+ * - Type: See type descriptions below
+ * - Points: Number of arbitrary points player gets for winning trophy. Like gamer score on XBOX.
+ * - Max Value: Depending on the type, this number is the value that the player is trying to get to
+ *
+ * Types of trophies:
+ * - Trigger: Only required to trigger once. These are best suited to trophies that are secrets, milestones, etc.
+ * - Additive: Each time update is called, adds to the tracker until the max value is reached. Useful for lifetime
+ * stats.
+ * - Progress: Each time the trophy is updated, it takes the highest value and uses that. This is best for trophies that
+ * are expected to be completed in a single run, such as farthest distance climbed in a race or highest amount of coins
+ * collected per life.
+ *
+ * A special "All trophies gotten" trophy is automatically generated when the first standard trophy is generated.
+ *
+ * It is encouraged for developers to equate all trophies values to add up to 1000. The default trophy has a value of
+ * 100, leaving 900 to be divide by all other trophies. This isn't a hard requirement.
+ *
+ * \section trophy_update Updating the Trophy
+ *
+ * Use either `trophyUpdate()` or `trophyUpdateMilestone()` to update the status of the trophy. If the trophy is a
+ * 'Trigger' type, it will automatically set the trophy to having been won regardless of the value entered. If the
+ * trophy is one of the other two modes, it will either add or replace the previous value if appropriate.
+ *
+ * If the developer wants to update quietly, set the drawUpdate to false. This allows smaller updates if required.
+ * Otherwise, set to true to show the banner automatically.
+ *
+ * Using the milestone system, the program will automatically display trophy progress at 25%, 50%, 75% and 100%
+ * completion rate. This is great for trophies that have frequent updates and wish to display partial milestones.
+ *
+ * If a custom milestone is desired, use `trophyGetData()` to get a struct of the trophy data, and then compare the max
+ * value inside the struct to the current value the player has, or just keep an external reference tot eh max value. Set
+ * the conditions for each milestone desired, then set the draw argument to true if a milestone has been crossed.
+ * Example code below:
+ *
+ * \code {.c}
+ * //TODO: Example of custom milestone boolean structure
+ * \endcode
+ *
+ * Updates should be as large and as infrequent as possible. Only update when the developer has to to avoid the overhead
+ * of reading from the NVS to check if it's been updated, as that's slow. The system checks if the NVS needs writing so
+ * the user don't have to worry about the NVS being destroyed by too many updates. The worst case scenario is rapidly
+ * adding 1 to a very large number constantly, as this will hit the NVS every time, so try to avoid this.
+ *
+ * \section trophy_helpers Helper functions
+ *
+ * Several helper functions exist for the developer to get critical information
+ * - trophyGetNumTrophies(): Gets the total number of trophies for a mode, or for all modes if NULL
+ * - trophyGetTrophyList(): Returns a list of trophy titles, which can then be used to pull individual data or just
+ * display on the screen. Setting NULL as the mode name will grab all trophy titles in unspecified order.
+ * - trophyGetData(): Grabs the data stored for the specified trophy, such as sprite name, description, and current
+ * value.
+ *
+ * \section trophy_draw Drawing a trophy
+ *
+ * Individual trophies can be viewed at any time by calling `trophyDraw()` and providing it an index. Set animations
+ * to false to draw the box immediately. This function should be last in the draw stack to ensure it draws on top of all
+ * other items in the mode.
+ *
+ * The standard single trophy draw function draws a banner across the top of the screen using the provided data. It will
+ * display:
+ * - An image if provided
+ * - The title text
+ * - The description
+ * - If a value is required, draw current value and progress
+ *
+ * //TODO: Example image
+ *
+ * The banner will scroll in from the top by default, but can be set to the bottom with `trophySystemSet()`. See system
+ * initialization. The banner will be fully visible for the set duration, not including scroll in and scroll out
+ * duration.
+ *
+ * There can be up to five banners in the queue, and will display one after another.
+ *
+ * \section trophy_draw_list Drawing all the trophies
+ *
+ * The trophies can be drawn in one of two ways: By calling `trophyDrawList()` or by getting the list of trophies and
+ * making a separate display function.
+ *
+ * The list draw function can be re-initialized to change the colors to theme the menu without recreating it from
+ * scratch.
+ *
+ * //TODO: Example image
+ *
+ * The colors can be changed to theme the display with `trophyDrawListInit()`.
+ *
+ * \section trophy_clear Clearing a trophy
+ *
+ * Reset a trophy to 0 (but don't remove it from the list of trophies) with `trophyClear()`.
+ *
+ * Trophies should only be cleared under abnormal circumstances, such as the player having been caught cheating.
+ * Consider that the Steamworks API (Valve) has a "Remove trophy" function, yet most people have never lost an trophy.
+ *
+ * Clearing a trophy will delete the points acquired if it was previously won.
+ *
+ * \code {.C}
+ * // TODO: Examples of all code
+ * \endcode
+ *
+ */
+#pragma once
+
+//==============================================================================
+// Includes
+//==============================================================================
+
+#include <stdint.h>
+#include <stdbool.h>
+
+//==============================================================================
+// Defines
+//==============================================================================
+
+// String lengths
+#define TROPHY_MAX_TITLE_LEN 24
+#define TROPHY_MAX_DESC_LEN  96
+#define TROPHY_MAX_WSG_LEN   24
+
+// Visuals
+#define TROPHY_BANNER_HEIGHT        48
+#define TROPHY_BANNER_MAX_ICON_DIM  36
+#define TROPHY_BANNER_ICON_H_BUFFER 4
+
+//==============================================================================
+// Enum
+//==============================================================================
+
+typedef enum
+{
+    TROPHY_TYPE_TRIGGER,  //< Only needs to be triggered once (Did a handstand, found a hidden message)
+    TROPHY_TYPE_ADDITIVE, //< Each update adds to teh previous (Lifetime distance, number of cakes eaten)
+    TROPHY_TYPE_PROGRESS, //< Tracks how far a user got per update (How long player survived, how far player ran)
+} trophyTypes_t;
+
+//==============================================================================
+// Structs
+//==============================================================================
+
+// TODO: Determine if there's a better way (read: not storing a string) to pull WSGs.
+// TODO: Find size of NVS to see how fast these will fill it up.
+// - 157 Bytes
+// - Usable NVS space is ~8kb
+// - If only using NVS for trophies, max num of trophies would be 50. Unacceptable.
+// TODO: See if SPIFFS can be used to save some of this data.
+typedef struct
+{
+    char title[TROPHY_MAX_TITLE_LEN];      //< Name of the Trophy, used as ID
+    char description[TROPHY_MAX_DESC_LEN]; //< Short description of task required
+    char imageString[TROPHY_MAX_WSG_LEN];  //< String leading to the .wsg file.
+    trophyTypes_t type;                    //< Type of trophy. See "trophy.h" for descriptions
+    int8_t points;                         //< How many points the trophy is worth
+    int currentValue;                      //< Current status of the trophy
+    int maxValue;                          //< The value that
+} trophy_t;
+
+//==============================================================================
+// Functions
+//==============================================================================
+
+// Trophy system
+
+/**
+ * @brief Initializes the Trophy system settings. The system is a global setting and every mode needs to set this when
+ * entered to avoid copying from other modes.
+ *
+ * @param bottom True if the banner should appear from the bottom, false if should appear from the top.
+ * @param displayDuration Time in tenths of a second that the banner should display for. Default is 3 seconds (30)
+ * @param animate If the banner should scroll into view
+ * @param scrollSpeed Time in tenths of a second for banner to appear. Default is half a second (5)
+ */
+void trophySystemInit(bool bottom, int displayDuration, bool animate, int scrollSpeed);
+
+/**
+ * @brief Loads the selected font into the struct
+ *
+ * @param font String name of the font to load
+ *
+ * @note Should only be used in Swadge initialization. Not for general use.
+ */
+void trophySystemSetFont(char* font);
+
+/**
+ * @brief Clears out the font from the struct. Possibly unnecessary.
+ *
+ * @note Should only be used in Swadge de-initialization. Not for general use.
+ */
+void trophySystemClearFont(void);
+
+/**
+ * @brief Loads the current number of points
+ *
+ * @param modeName Name of the made for namespace. NULL gets total of all modes.
+ * @return int Number of points total for the provided mode
+ */
+int trophySystemGetPoints(char* modeName);
+
+// Utilize trophies
+
+/**
+ * @brief Initializes a new Trophy
+ *
+ * @param modeName Mode to attach trophy to
+ * @param title Name of the Trophy
+ * @param imgStr String leading to WSG
+ * @param desc String containing description of trophy
+ * @param type Type of trophy. See Initialization section above
+ * @param points Number of poi9nts gained by winning trophy
+ * @param maxVal Value required to trigger win in non-single trigger mode
+ */
+void trophyInit(char* modeName, char* title, char* imgStr, char* desc, trophyTypes_t type, int8_t points, int maxVal);
+
+/**
+ * @brief Updates specifed trophy if required
+ *
+ * @param modeName Name of the mode
+ * @param title Title fo the trophy to update
+ * @param value New value to try to set. Behavior is set by trophy type
+ * @param drawUpdate If this update should be drawn to the screen
+ */
+void trophyUpdate(char* modeName, char* title, int value, bool drawUpdate);
+
+/**
+ * @brief Updates just like trophyUpdate(), but automatically mutes updates that aren't breaking
+ *
+ * @param modeName Name of the mode
+ * @param title Title of trophy to update
+ * @param value New value to assign
+ */
+void trophyUpdateMilestone(char* modeName, char* title, int value);
+
+/**
+ * @brief Erases completion data from swadge. Only use in extreme circumstances.
+ *
+ * @param modeName Name of the mode
+ * @param title Title of trophy to clear.
+ */
+void trophyClear(char* modeName, char* title);
+
+// Helpers
+
+/**
+ * @brief Gets the total number of trophies from a specified mode.
+ *
+ * @param modeName Name of the mode from which to grab trophy quantity. NULL returns quantity of all modes.
+ * @return int total number of trophies in given mode.
+ */
+int trophyGetNumTrophies(char* modeName);
+
+/**
+ * @brief Grabs a list of titles from specified mode.
+ *
+ * @param modeName Name of the mode from which to grab trophies. NULL returns all values in unspecified order
+ * @param tList outFile that contains a list of strings which are the titles of the trophies
+ * @param tLen Max number of trophies to grab. If actual total is less than provided tLen, value is set to total
+ * @param offset Where to start grabbing from. Useful in batch mode
+ */
+void trophyGetTrophyList(char* modeName, trophy_t* tList, int* tLen, int offset);
+
+/**
+ * @brief Grabs data for specified Trophy.
+ *
+ * @param modeName Name of the mode
+ * @param title Title of the Trophy
+ * @return trophy_t trophy data struct
+ */
+trophy_t trophyGetData(char* modeName, char* title);
+
+// Drawing functions
+
+/**
+ * @brief Draws the banner if one is queued
+ *
+ * @param modeName Name of the mode being used
+ * @param title Title of the trophy
+ * @param elapsedUs TIme since last frame
+ */
+void trophyDraw(char* modeName, char* title, int64_t elapsedUs);
+
+/**
+ * @brief Changes the colors of the default list of trophies.
+ *
+ * // TODO: Define color variables to set. Should not take void as argument.
+ */
+void trophyDrawListInit(void);
+
+/**
+ * @brief Draws a list of trophies for provided mode.
+ *
+ * @param modeName Name of the Mode. Set to NULL to get all trophies in an undefined order.
+ * @param idx Index to start displaying at. Program will ensure it's not out of bounds, but developer is required to
+ * update index as they see fit.
+ */
+void trophyDrawList(char* modeName, int idx);
