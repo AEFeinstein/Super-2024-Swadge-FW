c98d936412b1e00015ac0cc13e9242778db5e05c
Jeremy Stintzcum <johnnywycliffe@gmail.com>
2025-02-25 22:10:16 -0500

    Made simple mode include list .h file.

--- emulator/src/extensions/modes/ext_modes.c
+++ emulator/src/extensions/modes/ext_modes.c
@@ -10,35 +10,7 @@
 
 #include <stdlib.h>
 
-// Mode Includes
-/*
- Quickly regenerate with:
-   grep -lirE '^extern swadgeMode_t (.*);' main | grep -oE '([^/]+\.h)' \
-    | grep -v quickSettings | awk '{printf "#include \"%s\"\n",$1 }' | sort
-*/
-#include "accelTest.h"
-#include "bongoTest.h"
-#include "colorchord.h"
-#include "dance.h"
-#include "factoryTest.h"
-#include "gamepad.h"
-#include "introMode.h"
-#include "jukebox.h"
-#include "keebTest.h"
-#include "mainMenu.h"
-#include "mode_2048.h"
-#include "mode_bigbug.h"
-#include "mode_cGrove.h"
-#include "mode_credits.h"
-#include "mode_swadgeHero.h"
-#include "mode_synth.h"
-#include "modeTimer.h"
-#include "pango.h"
-#include "sequencerMode.h"
-#include "soko.h"
-#include "touchTest.h"
-#include "tunernome.h"
-#include "ultimateTTT.h"
+#include "modeIncludeList.h"
 
 //==============================================================================
 // Defines
---
+++ main/modeIncludeList.h
@@ -0,0 +1,30 @@
+// Mode Includes
+/*
+ Quickly regenerate with:
+   grep -lirE '^extern swadgeMode_t (.*);' main | grep -oE '([^/]+\.h)' \
+    | grep -v quickSettings | awk '{printf "#include \"%s\"\n",$1 }' | sort
+*/
+
+#include "accelTest.h"
+#include "bongoTest.h"
+#include "colorchord.h"
+#include "dance.h"
+#include "factoryTest.h"
+#include "gamepad.h"
+#include "introMode.h"
+#include "jukebox.h"
+#include "keebTest.h"
+#include "mainMenu.h"
+#include "modeTimer.h"
+#include "mode_2048.h"
+#include "mode_bigbug.h"
+#include "mode_cGrove.h"
+#include "mode_credits.h"
+#include "mode_swadgeHero.h"
+#include "mode_synth.h"
+#include "pango.h"
+#include "sequencerMode.h"
+#include "soko.h"
+#include "touchTest.h"
+#include "tunernome.h"
+#include "ultimateTTT.h"
