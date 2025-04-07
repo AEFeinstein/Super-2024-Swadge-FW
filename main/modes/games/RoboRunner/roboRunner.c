fa345d81796c225345dc4b04b84b607bcfea6026
Jeremy Stintzcum <johnnywycliffe@gmail.com>
2025-04-06 22:23:51 -0400

    Refactoring covered, draw obstacles

--- main/modes/games/RoboRunner/roboRunner.c
+++ main/modes/games/RoboRunner/roboRunner.c
@@ -1,27 +1,44 @@
 #include "roboRunner.h"
+#include "geometry.h"
 
 #define JUMP_HEIGHT          -12
 #define Y_ACCEL              1
 #define GROUND_HEIGHT        184
 #define PLAYER_GROUND_OFFSET (GROUND_HEIGHT - 56)
 #define PLAYER_X             32
+#define MAX_OBSTACLES        5
 
 const char runnerModeName[] = "Robo Runner";
 
 typedef struct
 {
-    // Assets
-    wsg_t character;
+    rectangle_t rect; // Contains the x, y, width and height
+    wsg_t img;        // The image
+    bool onGround;    // If the player is touching the ground
+    int ySpeed;       // The vertical speed. negative numbers are up.
+} player_t;
 
+typedef struct
+{
+    rectangle_t rect; // Contains the x, y, width and height
+    wsg_t img;        // Image to display
+    int speed;        // Speed of the obstacle
+} obstacle_t;
+
+typedef struct
+{
     // Robot
-    bool onGround;
-    int ySpeed;
-    int yPos;
+    player_t robot;
+
+    // Obstacles
+    obstacle_t obstacles[MAX_OBSTACLES];
 } runnerData_t;
 
 static void runnerEnterMode(void);
 static void runnerExitMode(void);
 static void runnerMainLoop(int64_t elapsedUs);
+static void runnerLogic(int64_t elapsedUS);
+static void draw(void);
 
 swadgeMode_t roboRunnerMode = {
     .modeName                 = runnerModeName,
@@ -45,38 +62,53 @@ runnerData_t* rd;
 static void runnerEnterMode()
 {
     rd = (runnerData_t*)heap_caps_calloc(1, sizeof(runnerData_t), MALLOC_CAP_8BIT);
-    loadWsg("RoboStanding.wsg", &rd->character, true);
+    loadWsg("RoboStanding.wsg", &rd->robot.img, true);
 }
 
 static void runnerExitMode()
 {
-    freeWsg(&rd->character);
+    freeWsg(&rd->robot.img);
     free(rd);
 }
 
 static void runnerMainLoop(int64_t elapsedUs)
 {
+    // Check input
     buttonEvt_t evt;
     while (checkButtonQueueWrapper(&evt))
     {
         if (evt.down)
         {
-            if ((evt.button & PB_A || evt.button & PB_UP) && rd->onGround)
+            if ((evt.button & PB_A || evt.button & PB_UP) && rd->robot.onGround)
             {
-                rd->ySpeed   = JUMP_HEIGHT;
-                rd->onGround = false;
+                rd->robot.ySpeed   = JUMP_HEIGHT;
+                rd->robot.onGround = false;
             }
         }
     }
-    rd->yPos += rd->ySpeed;
-    rd->ySpeed += Y_ACCEL;
-    if (rd->yPos > PLAYER_GROUND_OFFSET)
+
+    // Update player position
+    runnerLogic(elapsedUs);
+
+    // Draw screen
+    draw();
+}
+
+static void runnerLogic(int64_t elapsedUS)
+{
+    rd->robot.rect.pos.y += rd->robot.ySpeed;
+    rd->robot.ySpeed += Y_ACCEL;
+    if (rd->robot.rect.pos.y > PLAYER_GROUND_OFFSET)
     {
-        rd->onGround = true;
-        rd->yPos     = PLAYER_GROUND_OFFSET;
-        rd->ySpeed   = 0;
+        rd->robot.onGround   = true;
+        rd->robot.rect.pos.y = PLAYER_GROUND_OFFSET;
+        rd->robot.ySpeed     = 0;
     }
+}
+
+static void draw()
+{
     fillDisplayArea(0, 0, TFT_WIDTH, TFT_HEIGHT, c001);
     drawLine(0, GROUND_HEIGHT, TFT_WIDTH, GROUND_HEIGHT, c555, 0);
-    drawWsgSimple(&rd->character, PLAYER_X, rd->yPos);
+    drawWsgSimple(&rd->robot.img, PLAYER_X, rd->robot.rect.pos.y);
 }
