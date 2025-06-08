1980b0bbbfdcff5007a904c95db14f47f8cd90ae
Dylan Whichard <dylan@whichard.com>
2025-06-08 00:25:18 -0700

    Fix duplicate sudokus generating

--- tools/generate_sudokus.sh
+++ tools/generate_sudokus.sh
@@ -3,10 +3,17 @@
 PUZZLE_COUNT=50
 
 # Generate 10 simple, 15 easy, 15 intermediate, and 10 expert
-EASY_THRESH=10
-INTERMEDIATE_THRESH=25
-EXPERT_THRESH=40
 
+SIMPLE_COUNT=10
+EASY_COUNT=15
+INTERMEDIATE_COUNT=15
+EXPERT_COUNT=10
+
+EASY_THRESH=$SIMPLE_COUNT
+INTERMEDIATE_THRESH=$(($EASY_THRESH+$EASY_COUNT))
+EXPERT_THRESH=$(($INTERMEDIATE_THRESH+$INTERMEDIATE_COUNT))
+
+COUNT=$SIMPLE_COUNT
 DIFF="simple"
 
 qqwing --version >/dev/null
@@ -20,16 +27,30 @@ I=0
 while [ "${I}" -lt "${PUZZLE_COUNT}" ]; do
 	if [ "${I}" -eq "${EASY_THRESH}" ]; then
 		DIFF="easy"
+		COUNT=$EASY_COUNT
 	elif [ "${I}" -eq "${INTERMEDIATE_THRESH}" ]; then
 		DIFF="intermediate"
+		COUNT=$INTERMEDIATE_COUNT
 	elif [ "${I}" -eq "${EXPERT_THRESH}" ]; then
 		DIFF="expert"
+		COUNT=$EXPERT_COUNT
 	fi
 
-	OUTPUT="$(printf './assets/sudoku/sudoku_puz_%03d.sdk' ${I})"
 
-	PUZZLE="$(qqwing --generate 1 --difficulty $DIFF)"
-	cat <<EOF > $OUTPUT
+
+	# APPARENTLY qqwing uses srand(time()), so if you generate two puzzles in the same second they're the same
+	# So we have to use the --generate <n> option to do multiple ones, with this monstrosity
+	PUZZLE=
+	while REPLY=; read -r || [[ $REPLY ]]; do
+		if [[ $REPLY ]]; then
+			if [[ $PUZZLE ]]; then
+				PUZZLE+=$'\n'"$REPLY"
+			else
+				PUZZLE+=$REPLY
+			fi
+		else
+			OUTPUT="$(printf './assets/sudoku/sudoku_puz_%03d.sdk' ${I})"
+			cat <<EOF > $OUTPUT
 #Adylwhich
 #DA randomly-generated ${DIFF} puzzle
 #B$(date -I)
@@ -38,5 +59,8 @@ while [ "${I}" -lt "${PUZZLE_COUNT}" ]; do
 ${PUZZLE}
 EOF
 
-	I=$(($I + 1))
+			I=$(($I + 1))
+			PUZZLE=
+		fi
+	done < <(qqwing --generate $COUNT --difficulty $DIFF | tee qqqwing_$I)
 done
