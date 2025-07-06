2dab1a97cf1d6ff5704b8dd62b69f689238adb6f
Jeremy Stintzcum <johnnywycliffe@gmail.com>
2025-07-05 22:39:23 -0400

    Updated chicken brain and added bait box, but now the chicken teleports

--- main/modes/games/JerkChicken/jerkChicken.c
+++ main/modes/games/JerkChicken/jerkChicken.c
@@ -2,7 +2,6 @@
  * @file jerkChicken.c
  * @author Jeremy Stintzcum (jeremy.stintzcum@gmail.com)
  * @brief A mode about jerking the chicken. No, not like the food. Not that way either.
- * @version 0.1
  * @date 2025-06-03
  *
  * @copyright Copyright (c) 2025
@@ -21,27 +20,22 @@
 //==============================================================================
 
 // Settings
-#define JERK_VALUE    1024
-#define STEP_SIZE     32
-#define PLAYER_SPEED  3
-#define CHICKEN_SPEED 2
-
-// Chicken brain chances
-#define CHANCE_WALK      10
-#define CHANCE_WALK_BACK 5
-#define CHANCE_LUNGE     10
-#define CHANCE_PECK      35
-
+#define JERK_VALUE           1024
+#define STEP_SIZE            32
+#define PLAYER_SPEED         3
+#define CHICKEN_SPEED        2
+#define BOX_MAX_DISTANCE     40
+#define CHICKEN_ATTACK_RANGE 80
 // Timers
 #define ANIM_TIMER_PERIOD   16667  // Assumes 60Hz
-#define CHICKEN_STATIC_BASE 250000 // 1/4 sec
-
+#define CHICKEN_STATIC_BASE 500000 // 1/2 sec
 // Pixel offsets
-#define PLAYER_X_POS     32
-#define PLAYER_Y_POS     64
-#define CHICKEN_X_OFFSET 200
-#define CHICKEN_Y_POS    160
-// #define BAIT_BOX_Y_POS   168
+#define PLAYER_X_POS      32
+#define PLAYER_Y_POS      64
+#define CHICKEN_X_OFFSET  200
+#define CHICKEN_Y_POS     160
+#define BAIT_BOX_X_OFFSET 108
+#define BAIT_BOX_Y_POS    168
 
 //==============================================================================
 // Consts
@@ -85,6 +79,7 @@ typedef struct
 {
     // Chicken brain
     chickenStates_t state; // Current chicken brain
+    int diffMod;           // The current difficulty modifier (50 is easiest, 0 is hardest)
 
     // Movement
     int8_t moveSpeed;  // Current move speed
@@ -96,6 +91,9 @@ typedef struct
 
 typedef struct
 {
+    // Box
+    int8_t HP;
+
     // Movement
     int8_t speed;
     int32_t position;
@@ -109,6 +107,13 @@ typedef struct
 
     // Chicken
     chicken_t chicken;
+
+    // Bait Box
+    baitBox_t box;
+
+    // Mode
+    int score; // Score
+    // state; // Splash/Game/End
 
     // Debug
     bool debug; // Displays debug info
@@ -128,6 +133,7 @@ static void initGame(void);
 static void jerkRod(void);
 static void chickenHandleInputs(void);
 static bool movePos(int32_t* position, int32_t* target, int8_t speed);
+static void increaseDifficulty(void);
 static void chickenLogic(int64_t elapsedUs);
 
 // Draw routines
@@ -182,6 +188,9 @@ static void initGame()
 {
     cd->player.position  = 0;
     cd->chicken.position = 0;
+    cd->chicken.diffMod  = 50;
+    cd->box.position     = 0;
+    cd->box.HP           = 3;
 }
 
 static void jerkRod()
@@ -256,6 +265,14 @@ static bool movePos(int32_t* position, int32_t* target, int8_t speed)
     return false;
 }
 
+static void increaseDifficulty()
+{
+    if (cd->chicken.diffMod > 0)
+    {
+        cd->chicken.diffMod--;
+    }
+}
+
 static void chickenLogic(int64_t elapsedUs)
 {
     // Handle chicken brain
@@ -264,7 +281,7 @@ static void chickenLogic(int64_t elapsedUs)
         case CHICKEN_LUNGE_PREPARE:
         {
             cd->chicken.animTimer -= elapsedUs;
-            if(cd->chicken.animTimer <= 0)
+            if (cd->chicken.animTimer <= 0)
             {
                 cd->chicken.state = CHICKEN_LUNGE;
             }
@@ -276,15 +293,16 @@ static void chickenLogic(int64_t elapsedUs)
             RUN_TIMER_EVERY(cd->chicken.animTimer, ANIM_TIMER_PERIOD, elapsedUs,
                             cd->chicken.state
                             = movePos(&cd->chicken.position, &cd->chicken.targetPos, cd->chicken.moveSpeed)
-                                  ? CHICKEN_THINKING
+                                  ? CHICKEN_IDLE
                                   : CHICKEN_WALKING;);
+            cd->chicken.animTimer = CHICKEN_STATIC_BASE;
             break;
         }
         case CHICKEN_IDLE:
         case CHICKEN_PECK:
         {
             cd->chicken.animTimer -= elapsedUs;
-            if(cd->chicken.animTimer <= 0)
+            if (cd->chicken.animTimer <= 0)
             {
                 cd->chicken.state = CHICKEN_THINKING;
             }
@@ -295,35 +313,67 @@ static void chickenLogic(int64_t elapsedUs)
         {
             // Decide next action in a sane manner
             cd->chicken.animFrame = 0; // Reset regardless to avoid bugs
-            int choiceIdx         = esp_random() % 100;
-            if (choiceIdx < CHANCE_WALK)
+
+            // If close enough to the box, peck at it and damage box
+            if (cd->chicken.position > cd->box.position + CHICKEN_ATTACK_RANGE
+                && cd->chicken.position < cd->box.position + CHICKEN_ATTACK_RANGE + 24)
+            {
+                cd->chicken.state     = CHICKEN_PECK;
+                cd->chicken.animTimer = CHICKEN_STATIC_BASE * 2;
+                cd->box.HP--;
+                break;
+            }
+            // If past box, always go backwards
+            else if (cd->chicken.position > cd->box.position + CHICKEN_ATTACK_RANGE + 24)
             {
                 cd->chicken.moveSpeed = CHICKEN_SPEED;
-                cd->chicken.targetPos = cd->chicken.position + 16 + (esp_random() % 32);
+                cd->chicken.targetPos = cd->box.position + CHICKEN_ATTACK_RANGE + 12;
                 cd->chicken.state     = CHICKEN_WALKING;
+                break;
             }
-            else if (choiceIdx < CHANCE_WALK + CHANCE_WALK_BACK)
-            {
-                cd->chicken.moveSpeed = CHICKEN_SPEED;
-                cd->chicken.targetPos = cd->chicken.position - (16 + (esp_random() % 32));
-                cd->chicken.state     = CHICKEN_WALKING;
-            }
-            else if (choiceIdx < CHANCE_WALK + CHANCE_WALK_BACK + CHANCE_LUNGE)
+            // Chance to lunge if in range
+            int choiceIdx = esp_random() % (50 + cd->chicken.diffMod);
+            if (choiceIdx < 25 && cd->chicken.position > cd->box.position + CHICKEN_ATTACK_RANGE - 48)
             {
                 cd->chicken.moveSpeed = CHICKEN_SPEED * 2;
                 cd->chicken.targetPos = cd->chicken.position + 32 + (esp_random() % 48);
                 cd->chicken.state     = CHICKEN_LUNGE_PREPARE;
-                cd->chicken.animTimer = CHICKEN_STATIC_BASE;
+                cd->chicken.animTimer = CHICKEN_STATIC_BASE * 2;
+                increaseDifficulty();
+                break;
             }
-            else if (choiceIdx < CHANCE_WALK + CHANCE_WALK_BACK + CHANCE_LUNGE + CHANCE_PECK)
+            // More likely to walk forward if box is closer
+            choiceIdx = esp_random() % (50 + cd->chicken.diffMod);
+            if (choiceIdx < 10 + (cd->chicken.position - cd->box.position))
             {
-                cd->chicken.state = CHICKEN_PECK;
-                cd->chicken.animTimer = CHICKEN_STATIC_BASE * 2; // 1/2 sec
+                cd->chicken.moveSpeed = CHICKEN_SPEED;
+                cd->chicken.targetPos = cd->chicken.position + 16 + (esp_random() % 32);
+                cd->chicken.state     = CHICKEN_WALKING;
+                break;
+            }
+
+            // Move backwards if too far away
+            choiceIdx = esp_random() % (50 + cd->chicken.diffMod);
+            if (choiceIdx < 10 + (cd->box.position - cd->chicken.position))
+            {
+                cd->chicken.moveSpeed = CHICKEN_SPEED;
+                cd->chicken.targetPos = cd->chicken.position - (16 + (esp_random() % 32));
+                cd->chicken.state     = CHICKEN_WALKING;
+                break;
+            }
+
+            // Randomly peck or stand still
+            choiceIdx = esp_random() % 100;
+            if (choiceIdx < 50)
+            {
+                cd->chicken.state     = CHICKEN_PECK;
+                cd->chicken.animTimer = CHICKEN_STATIC_BASE * 2;
+                break;
             }
             else
             {
-                cd->chicken.state = CHICKEN_IDLE;
-                cd->chicken.animTimer = CHICKEN_STATIC_BASE * 2 + (esp_random() % CHICKEN_STATIC_BASE); // 1/2 - 3/4 sec
+                cd->chicken.state     = CHICKEN_IDLE;
+                cd->chicken.animTimer = CHICKEN_STATIC_BASE * 2 + (esp_random() % CHICKEN_STATIC_BASE);
             }
             break;
         }
@@ -334,7 +384,25 @@ static void chickenLogic(int64_t elapsedUs)
     RUN_TIMER_EVERY(cd->player.animTimer, ANIM_TIMER_PERIOD, elapsedUs,
                     movePos(&cd->player.position, &cd->player.targetPosition, cd->player.moveSpeed););
 
+    // Move box
+    if (cd->box.position + BOX_MAX_DISTANCE < cd->player.position)
+    {
+        cd->box.position = cd->player.position - BOX_MAX_DISTANCE;
+    }
+    else if (cd->box.position - BOX_MAX_DISTANCE > cd->player.position)
+    {
+        cd->box.position = cd->player.position + BOX_MAX_DISTANCE;
+    }
+
+    // Update score
+    if (cd->chicken.position > cd->score)
+    {
+        cd->score = cd->chicken.position;
+    }
+
     // TODO: evaluate lose conditions
+    // If Chicken is too far off screen
+    // If box has 0 HP
 }
 
 static void drawChicken(int64_t elapsedUs)
@@ -361,19 +429,37 @@ static void drawChicken(int64_t elapsedUs)
     }
 
     // Player
-    drawRect(PLAYER_X_POS, PLAYER_Y_POS, PLAYER_X_POS + 64, PLAYER_Y_POS + 128, c050);
+
+    // Box
 
     // Chicken
-    int16_t chickenXOffset = -cd->chicken.position + CHICKEN_X_OFFSET + cd->player.position;
-    drawRect(chickenXOffset, CHICKEN_Y_POS, chickenXOffset + 32, CHICKEN_Y_POS + 32, c500);
 
     // UI
 
     // Test
     if (cd->debug)
     {
+        // Player
+        drawRect(PLAYER_X_POS, PLAYER_Y_POS, PLAYER_X_POS + 64, PLAYER_Y_POS + 128, c050);
+
+        // Box
+        int16_t boxXOffset = -cd->box.position + BAIT_BOX_X_OFFSET + cd->player.position;
+        drawRect(boxXOffset, BAIT_BOX_Y_POS, boxXOffset + 24, BAIT_BOX_Y_POS + 24, c055);
+
+        // Chicken
+        int16_t chickenXOffset = -cd->chicken.position + CHICKEN_X_OFFSET + cd->player.position;
+        drawRect(chickenXOffset, CHICKEN_Y_POS, chickenXOffset + 32, CHICKEN_Y_POS + 32, c500);
+
         char buffer[32];
         snprintf(buffer, sizeof(buffer) - 1, "State: %" PRId16, cd->chicken.state);
         drawText(getSysFont(), c550, buffer, chickenXOffset, CHICKEN_Y_POS - 16);
+
+        // UI
+        snprintf(buffer, sizeof(buffer) - 1, "Score: %" PRId8, cd->score);
+        drawText(getSysFont(), c550, buffer, 32, 16);
+        snprintf(buffer, sizeof(buffer) - 1, "HP: %" PRId8, cd->box.HP);
+        drawText(getSysFont(), c550, buffer, 32, 32);
+        snprintf(buffer, sizeof(buffer) - 1, "Difficulty Mod: %" PRId8, cd->chicken.diffMod);
+        drawText(getSysFont(), c550, buffer, 32, 48);
     }
 }
