e555d945c9063e8fc87420ac71bff7b09a7e8acf
gelakinetic <gelakinetic@gmail.com>
2025-07-08 11:37:51 +0000

    Change menu callback to go up Update all menus Add submenu for ammo loading

--- main/modes/utilities/gamepad/gamepad.c
+++ main/modes/utilities/gamepad/gamepad.c
@@ -92,7 +92,7 @@ void gamepadMainLoop(int64_t elapsedUs);
 void gamepadButtonCb(buttonEvt_t* evt);
 void gamepadReportStateToHost(void);
 
-void gamepadMainMenuCb(const char* label, bool selected, uint32_t settingVal);
+bool gamepadMainMenuCb(const char* label, bool selected, uint32_t settingVal);
 void gamepadMenuLoop(int64_t elapsedUs);
 void gamepadStart(gamepadType_t type);
 
@@ -314,7 +314,7 @@ void gamepadExitMode(void)
     heap_caps_free(gamepad);
 }
 
-void gamepadMainMenuCb(const char* label, bool selected, uint32_t settingVal)
+bool gamepadMainMenuCb(const char* label, bool selected, uint32_t settingVal)
 {
     if (selected)
     {
@@ -322,19 +322,19 @@ void gamepadMainMenuCb(const char* label, bool selected, uint32_t settingVal)
         {
             gamepadStart(GAMEPAD_GENERIC);
             gamepad->screen = GAMEPAD_MAIN;
-            return;
+            return false;
         }
         else if (label == str_ns)
         {
             gamepadStart(GAMEPAD_NS);
             gamepad->screen = GAMEPAD_MAIN;
-            return;
+            return false;
         }
         else if (label == str_exit)
         {
             // Exit to main menu
             switchToSwadgeMode(&mainMenuMode);
-            return;
+            return false;
         }
     }
     else
@@ -348,6 +348,7 @@ void gamepadMainMenuCb(const char* label, bool selected, uint32_t settingVal)
             setGamepadTouchSetting(settingVal);
         }
     }
+    return false;
 }
 
 void gamepadStart(gamepadType_t type)
