08167eeb415cf580959e3938dba7f75568cf5095
Jeremy Stintzcum <johnnywycliffe@gmail.com>
2025-07-12 19:56:48 -0400

    Initial commit

---
+++ emulator/src/components/hdw-led-matrices/hdw-led-matrices.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "hdw-led-matrices.h"
+
+void matrixLEDInit()
+{
+    // Set up pin
+    // load some code into the CH32 if required
+    // If required, send default expressions
+}
+
+void matrixLEDSendExpression(ledMatrix_t* mat)
+{
+    // Sets the expression to the brightnesses in the matrix
+}
+
+void matrixLEDSetExpression(defaultExpressions_t expr)
+{
+    // Sets a default expression
+}
+
+void matrixLEDSetBrightness(int8_t brightness)
+{
+    // Sets the max brightness that the eyes can be set to. 0 is off, 7 is max
+}
+
+void matrixLEDDeinit(void)
+{
+    // Teardown
+}
