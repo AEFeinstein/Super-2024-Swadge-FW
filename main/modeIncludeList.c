4d0d83edd539088d0caa85dfb64f9e18d51a40a0
cnlohr <lohr85@gmail.com>
2025-07-12 19:45:44 -0700

    First commit for tooling surrounding ch32v003

--- main/modeIncludeList.c
+++ main/modeIncludeList.c
@@ -16,10 +16,11 @@
 
 /// @brief Add swadgeMode_t pointers to this struct to include them in emulator and main menu
 swadgeMode_t* const allSwadgeModes[] = {
-    &accelTestMode,      &bouncyMode,  &colorchordMode, &cosCrunchMode,  &danceMode,      &factoryTestMode,
-    &gamepadMode,        &introMode,   &jukeboxMode,    &keebTestMode,   &mainMenuMode,   &modeCredits,
-    &modeDiceRoller,     &modePicross, &nameTestMode,   &roboRunnerMode, &sequencerMode,  &swadgeItMode,
-    &swadgePassTestMode, &synthMode,   &tCaseMode,      &touchTestMode,  &trophyTestMode, &tunernomeMode,
+    &accelTestMode,   &bouncyMode,       &colorchordMode, &cosCrunchMode,  &danceMode,
+    &factoryTestMode, &gamepadMode,      &introMode,      &jukeboxMode,    &keebTestMode,
+    &mainMenuMode,    &modeCh32v003test, &modeCredits,    &modeDiceRoller, &modePicross,
+    &nameTestMode,    &roboRunnerMode,   &sequencerMode,  &swadgeItMode,   &swadgePassTestMode,
+    &synthMode,       &tCaseMode,        &touchTestMode,  &trophyTestMode, &tunernomeMode,
 };
 
 //==============================================================================
@@ -57,6 +58,7 @@ void modeListSetMenu(menu_t* menu)
     addSingleItemToMenu(menu, introMode.modeName);
     addSingleItemToMenu(menu, modeDiceRoller.modeName);
     addSingleItemToMenu(menu, bouncyMode.modeName);
+    addSingleItemToMenu(menu, modeCh32v003test.modeName);
     menu = endSubMenu(menu);
 
     // Trophy Case
--- main/modeIncludeList.h
+++ main/modeIncludeList.h
@@ -41,6 +41,7 @@
 #include "keebTest.h"
 #include "mainMenu.h"
 #include "mode_credits.h"
+#include "mode_ch32v003test.h"
 #include "mode_diceroller.h"
 #include "mode_synth.h"
 #include "nameTest.h"
