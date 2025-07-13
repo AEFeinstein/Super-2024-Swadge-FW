9b0a0f2a0626ef6c3d198b193364b8068ce16cf9
DebrisHauler <DebrisHauler@users.noreply.github.com>
2025-07-12 20:07:39 -0400

    Dance network (#464)
    * mode added
    
    * Added CMAKE entry, background fill, grid graphics.
    
    * fun tile selection
    
    * Keep the board in one place generally.
    
    * Less spring force on tiles further from selection.
    
    * More dampen force further from selection.
    
    * Floor bounces downwards for impact and wave propogates faster.
    
    * movement added
    
    * multiplayer not working
    
    * grabbing p2p code from UTTT
    
    * menu stuff
    
    * You can get to the game from the menu.
    
    * bucketHat graphic
    
    * Simpler drawing
    
    * Got some text showing for connection.
    
    * Some character selection is happening.
    
    * Better drawing.
    
    * character selection work
    
    * Show selected pieces.
    
    * bug fix alpha not showing in game.
    
    * Picked pieces make it into the game.
    
    * Made shiny text
    
    * Setting up an entity manager.
    
    * Setting up things as entities.
    
    * checking in
    
    * It builds again.
    
    * Set camera pos. Store elapsed time. Store entity pointers on each tile. Bug fixes to board drawing. Allocate board data.
    
    * Fixed some offsets.
    
    * Draw white chess set. Moved all palettes to EntityManager. Added art assets.
    
    * curtain thing
    
    * Entities can be destroyed.
    
    * better curtains
    
    * Added albums.
    
    * Cleaner loading, and support for rotated albums.
    
    * Added an albums entity that contains 3 albums and draws text. Reposition things. Recolor some text. Simplified entity creation. Add support for entities with no WSGs.
    
    * Turning character selection into an entity.
    
    * Fixing up the character select.
    
    * Get pieces centered on tiles.
    
    * Flashy disco floor.
    
    * All the floor patterns!
    
    * better floor colors
    
    * Fixed flashy arrows.
    
    * make format
    
    * shiny floor
    
    * better floor
    
    * bug fixes
    
    * make format
    
    * Don't add up inputs in character select.
    
    * make format
    
    * Fix CICD error message, hopefully.
    
    * Fix another CICD error, hopefully.
    
    * Fix another CICD error, hopefully.
    
    * Fix another CICD error, hopefully.
    
    * Fix another CICD error, hopefully.
    
    * Fix more CICD errors.
    
    * make format version 17.0.6. Album blinky light. Move shared font.c code to one function. Increment the generaTimer in the MainLoop. Add functions for converting between track coord and wsg index colors. Album screens can turn on and off. Need to adjust colors more. Ensure data is freed if destroyFlag is set on an entity. Clean up switch statement.
    
    * Album graphic without using indexed colors outside of the index squares. Righteous outline the correct way. Added an intro timer to the albums turning on. P2's album is shown upside down on purpose. More discernable track coloring.
    
    * Added a tile selector. Will make it look nice later.
    
    * make format
    
    * make format?

---
+++ main/modes/games/danceNetwork/dn_typedef.h
@@ -0,0 +1,111 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <wsg.h>
+
+#define DN_DECIMAL_BITS    4
+#define DN_BOARD_SIZE      5
+#define DN_TILE_WIDTH      51
+#define DN_TILE_HEIGHT     13
+#define NUM_ASSETS         14 // The number of dn_asset_t (last accounted for DN_ALBUM_ASSET)
+#define NUM_PALETTES       7  // The number of wsgPalette_t (last accounted for DN_PURPLE_FLOOR_PALETTE)
+#define NUM_SELECTOR_LINES 32 // Creates more chaotic lines in the selector graphic
+
+typedef struct dn_entity_t dn_entity_t;
+typedef struct dn_gameData_t dn_gameData_t;
+
+typedef enum __attribute__((packed))
+{
+    DN_ALPHA_DOWN_ASSET,
+    DN_ALPHA_ORTHO_ASSET,
+    DN_ALPHA_UP_ASSET,
+    DN_KING_ASSET,
+    DN_KING_SMALL_ASSET,
+    DN_PAWN_ASSET,
+    DN_PAWN_SMALL_ASSET,
+    DN_BUCKET_HAT_DOWN_ASSET,
+    DN_BUCKET_HAT_UP_ASSET,
+    DN_GROUND_TILE_ASSET,
+    DN_CURTAIN_ASSET,
+    DN_CHESS_ORTHO_ASSET,
+    DN_ALBUM_ASSET,
+    DN_STATUS_LIGHT_ASSET,
+    DN_NO_ASSET, // Keep this one at the end of the enum. Used for entities with no wsgs.
+} dn_assetIdx_t;
+
+typedef enum __attribute__((packed))
+{
+    DN_ALPHA_SET,
+    DN_CHESS_SET,
+} dn_characterSet_t;
+
+typedef enum __attribute__((packed))
+{
+    DN_ONESHOT_ANIMATION,
+    DN_LOOPING_ANIMATION,
+    DN_NO_ANIMATION,
+} dn_animationType_t;
+
+typedef enum __attribute__((packed))
+{
+    DN_WHITE_CHESS_PALETTE,
+    DN_RED_FLOOR_PALETTE,
+    DN_ORANGE_FLOOR_PALETTE,
+    DN_YELLOW_FLOOR_PALETTE,
+    DN_GREEN_FLOOR_PALETTE,
+    DN_BLUE_FLOOR_PALETTE,
+    DN_PURPLE_FLOOR_PALETTE,
+} dn_paletteIdx_t;
+
+typedef struct __attribute__((packed))
+{
+    uint8_t originX;   // A point inside the sprite measured in pixels from the top left of the sprite
+    uint8_t originY;   // A point inside the sprite measured in pixels from the top left of the sprite
+    uint8_t numFrames; // Just use 1 for no animation
+    wsg_t* frames;     // Can hold 1 or more pointers to wsg's
+    bool allocated;    // Whether the frames are allocated.
+} dn_asset_t;
+
+typedef struct __attribute__((packed))
+{
+    int8_t x;
+    int8_t y;
+} dn_boardPos_t;
+
+typedef struct __attribute__((packed))
+{
+    paletteColor_t lit;
+    paletteColor_t unlit;
+} dn_twoColors_t;
+
+typedef enum __attribute__((packed))
+{
+    DN_UP,
+    DN_DOWN
+} dn_facingDir;
+
+typedef enum __attribute__((packed))
+{
+    DN_PAWN,
+    DN_KING
+} dn_unitRank;
+
+typedef enum __attribute__((packed))
+{
+    DN_P1_PICK_MOVE_OR_GAIN_REROLL_PHASE,
+    DN_P1_MOVE_PHASE,
+    DN_P1_UPGRADE_PHASE,
+    DN_P1_SWAP_PHASE,
+    DN_P2_PICK_MOVE_OR_GAIN_REROLL_PHASE,
+    DN_P2_MOVE_PHASE,
+    DN_P2_UPGRADE_PHASE,
+    DN_P2_SWAP_PHASE,
+} dn_phase_t;
+
+typedef enum __attribute__((packed))
+{
+    DN_NONE_TRACK,
+    DN_RED_TRACK,
+    DN_BLUE_TRACK,
+} dn_track_t;
---
+++ main/modes/games/danceNetwork/dn_utility.c
@@ -0,0 +1,50 @@
+//==============================================================================
+// Includes
+//==============================================================================
+#include "dn_utility.h"
+
+//==============================================================================
+// Functions
+//==============================================================================
+vec_t dn_boardToWorldPos(dn_boardPos_t boardPos)
+{
+    vec_t worldPos;
+    worldPos.x = 0xFFFF + ((((boardPos.x - boardPos.y - 1) * (DN_TILE_WIDTH >> 1)) << DN_DECIMAL_BITS));
+    worldPos.y = 0xFFFF + ((((boardPos.x + boardPos.y) * DN_TILE_HEIGHT)) << DN_DECIMAL_BITS);
+    return worldPos;
+}
+
+dn_assetIdx_t dn_getAssetIdx(dn_characterSet_t characterSet, dn_unitRank rank, dn_facingDir facingDir)
+{
+    switch (characterSet)
+    {
+        case DN_ALPHA_SET:
+        {
+            switch (rank)
+            {
+                case DN_PAWN:
+                    return facingDir ? DN_BUCKET_HAT_DOWN_ASSET : DN_BUCKET_HAT_UP_ASSET;
+                case DN_KING:
+                    return facingDir ? DN_ALPHA_DOWN_ASSET : DN_ALPHA_UP_ASSET;
+            }
+            break;
+        }
+        case DN_CHESS_SET:
+        {
+            switch (rank)
+            {
+                case DN_PAWN:
+                    return DN_PAWN_ASSET;
+                case DN_KING:
+                    return DN_KING_ASSET;
+            }
+        }
+    }
+    return -1; // Invalid asset index
+}
+
+void dn_setFloorPalette(wsgPalette_t* palette, paletteColor_t color)
+{
+    wsgPaletteSet(palette, c112, color);
+    wsgPaletteSet(palette, c223, color);
+}
