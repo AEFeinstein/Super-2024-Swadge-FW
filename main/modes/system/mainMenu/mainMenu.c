f50666132ddcbd46341132a87712c82971ee0a49
cnlohr <lohr85@gmail.com>
2025-07-18 23:13:13 -0700

    Fix CI

--- main/modes/system/mainMenu/mainMenu.c
+++ main/modes/system/mainMenu/mainMenu.c
@@ -275,8 +275,7 @@ static void mainMenuEnterMode(void)
     // Allocate the menu
     mainMenu->menu = initMenu(mainMenuTitle, mainMenuCb);
 
-    int ch32v003LoadReply = ch32v003RunBinaryAsset(MATRIX_DROPS_CFUN_BIN);
-    ch32v003LoadReply     = ch32v003LoadReply;
+    /*int ch32v003LoadReply = */ ch32v003RunBinaryAsset(MATRIX_DROPS_CFUN_BIN);
 
 #ifdef CONFIG_FACTORY_TEST_NORMAL
     // Initialize all the modes in modeList
