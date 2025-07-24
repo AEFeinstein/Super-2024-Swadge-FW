d439f0e9853de723d043bbcd5143208adf7feeee
gelakinetic <gelakinetic@gmail.com>
2025-07-24 11:30:30 +0000

    Add p2p connection to Vector Tanks

--- main/utils/p2pConnection.h
+++ main/utils/p2pConnection.h
@@ -392,6 +392,7 @@ typedef struct
 
 void p2pInitialize(p2pInfo* p2p, uint8_t modeId, p2pConCbFn conCbFn, p2pMsgRxCbFn msgRxCbFn, int8_t connectionRssi);
 void p2pSetAsymmetric(p2pInfo* p2p, uint8_t incomingModeId);
+void p2pRestart(p2pInfo* p2p);
 void p2pDeinit(p2pInfo* p2p);
 
 void p2pStartConnection(p2pInfo* p2p);
