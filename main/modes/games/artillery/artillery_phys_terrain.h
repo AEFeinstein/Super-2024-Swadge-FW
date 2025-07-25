10136cd50e2b9098dcf2d40510bcf92def90f016
gelakinetic <gelakinetic@gmail.com>
2025-07-25 11:55:26 +0000

    Add randomized terrain with flats under player

--- main/modes/games/artillery/artillery_phys_terrain.h
+++ main/modes/games/artillery/artillery_phys_terrain.h
@@ -2,5 +2,7 @@
 
 #include "artillery_phys.h"
 
+void physGenerateTerrain(physSim_t* phys);
+void flattenTerrainUnderPlayer(physSim_t* phys, physCirc_t* player);
 void explodeShell(physSim_t* phys, node_t* shellNode);
 bool moveTerrainPoints(physSim_t* phys, int32_t elapsedUs);
