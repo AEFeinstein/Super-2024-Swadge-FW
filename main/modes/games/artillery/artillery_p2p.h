ccd72e1a75283333f573dda136d3c95f10305e7a
gelakinetic <gelakinetic@gmail.com>
2025-07-30 11:23:00 +0000

    Build out P2P TX queue for multi-packet data Round terrain heights to integer values

---
+++ main/modes/games/artillery/artillery_p2p.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "p2pConnection.h"
+#include "artillery.h"
+
+void artillery_p2pConCb(p2pInfo* p2p, connectionEvt_t evt);
+void artillery_p2pMsgRxCb(p2pInfo* p2p, const uint8_t* payload, uint8_t len);
+void artillery_p2pMsgTxCb(p2pInfo* p2p, messageStatus_t status, const uint8_t* data, uint8_t len);
+
+void artilleryTxState(artilleryData_t* ad);
+void artilleryCheckTxQueue(artilleryData_t* ad);
