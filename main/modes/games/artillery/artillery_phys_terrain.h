0ef24c8c047408088e30a97ca03f206fd7b354b2
gelakinetic <gelakinetic@gmail.com>
2025-07-31 11:45:58 +0000

    Start building out terrain transmission

--- main/modes/games/artillery/artillery_phys_terrain.h
+++ main/modes/games/artillery/artillery_phys_terrain.h
@@ -2,6 +2,9 @@
 
 #include "artillery_phys.h"
 
+#define TERRAIN_ITERATIONS 7
+#define NUM_TERRAIN_POINTS ((1 << TERRAIN_ITERATIONS) + 1)
+
 void physGenerateTerrain(physSim_t* phys);
 void flattenTerrainUnderPlayer(physSim_t* phys, physCirc_t* player);
 void explodeShell(physSim_t* phys, node_t* shellNode);
