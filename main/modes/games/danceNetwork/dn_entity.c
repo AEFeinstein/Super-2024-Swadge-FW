b26a701a2224993bfed87bca615c449850c1e183
DebrisHauler <jla21@att.net>
2025-08-22 03:48:40 -0400

    You can win by moving a king to the opponent's throne.

--- main/modes/games/danceNetwork/dn_entity.c
+++ main/modes/games/danceNetwork/dn_entity.c
@@ -112,6 +112,7 @@ void dn_updateBoard(dn_entity_t* self)
                         //A unit dies
                         if(tileData->unit == ((dn_boardData_t*)self->gameData->entityManager.board->data)->p1Units[0] || tileData->unit == ((dn_boardData_t*)self->gameData->entityManager.board->data)->p1Units[1])//king is captured
                         {
+                            self->gameData->gameOver = true;
                             ///////////////////////////////
                             // Make the prompt Game Over //
                             ///////////////////////////////
@@ -2655,6 +2656,7 @@ void dn_updateBullet(dn_entity_t* self)
             else if((dn_belongsToP1(targetTile->unit) && self->gameData->phase >= DN_P2_TURN_START_PHASE) ||
                (!dn_belongsToP1(targetTile->unit) && self->gameData->phase < DN_P2_TURN_START_PHASE))
             {
+                self->gameData->gameOver = true;
                 ///////////////////////////////////
                 // Make the prompt enemy captured//
                 ///////////////////////////////////
@@ -2789,12 +2791,48 @@ void dn_moveUnit(dn_entity_t* self)
         bData->impactPos = uData->moveTo;
         bData->tiles[bData->impactPos.y][bData->impactPos.x].yVel = -700;
 
+
+        if((self == ((dn_boardData_t*)self->gameData->entityManager.board->data)->p1Units[0] && bData->impactPos.y == 0 && bData->impactPos.x == 2)|| 
+           (self == ((dn_boardData_t*)self->gameData->entityManager.board->data)->p1Units[1] && bData->impactPos.y == 4 && bData->impactPos.x == 2))//king moved to the opponent's throne
+        {
+            self->gameData->gameOver = true;
+            ///////////////////////////////
+            // Make the prompt Game Over //
+            ///////////////////////////////
+            dn_entity_t* promptGameOver = dn_createEntitySpecial(&self->gameData->entityManager, 0, DN_NO_ANIMATION, true, DN_NO_ASSET, 0, (vec_t){0xffff,0xffff}, self->gameData);
+            promptGameOver->data         = heap_caps_calloc(1, sizeof(dn_promptData_t), MALLOC_CAP_SPIRAM);
+            dn_promptData_t* promptData = (dn_promptData_t*)promptGameOver->data;
+            promptData->animatingIntroSlide = true;
+            promptData->yOffset = 320;//way off screen to allow more time to look at albums.
+            promptData->usesTwoLinesOfText = true;
+            char text[40] = self->gameData->playerNames[bData->tiles[bData->impactPos.y][bData->impactPos.x].timeout ? self != ((dn_boardData_t*)self->gameData->entityManager.board->data)->p1Units[0] : self == ((dn_boardData_t*)self->gameData->entityManager.board->data)->p1Units[0]];
+            strcat(text, " wins!");
+            strcpy(promptData->text, text);
+            promptData->options = heap_caps_calloc(1, sizeof(list_t), MALLOC_CAP_8BIT);
+            
+            dn_promptOption_t* option1 = heap_caps_malloc(sizeof(dn_promptOption_t), MALLOC_CAP_8BIT);
+            strcpy(option1->text, "OK");
+            option1->callback = NULL;
+            option1->downPressDetected = false;
+            push(promptData->options, (void*)option1);
+            promptData->numOptions = 1;
+
+            promptGameOver->dataType     = DN_PROMPT_DATA;
+            promptGameOver->updateFunction = dn_updatePrompt;
+            promptGameOver->drawFunction = dn_drawPrompt;
+        }
+
+
+
         if(self->gameData->phase != DN_P1_UPGRADE_PHASE && self->gameData->phase != DN_P2_UPGRADE_PHASE)
         {
             dn_incrementPhase(self);//now the upgrade phase
         }
-        dn_startUpgradeMenu(self, 2 << 20);
-
+        if(!self->gameData->gameOver)
+        {
+            dn_startUpgradeMenu(self, 2 << 20);
+        }
+        
         self->updateFunction = NULL;
     }
 }
