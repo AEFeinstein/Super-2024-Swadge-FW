b2a1d6332f0b9ad58da1385e3951fc17983a79dc
DebrisHauler <jla21@att.net>
2025-08-24 07:35:34 -0400

    Improved red track animation.

--- main/modes/games/danceNetwork/danceNetwork.c
+++ main/modes/games/danceNetwork/danceNetwork.c
@@ -567,7 +567,7 @@ static void dn_initializeGame(void)
 
     dn_loadAsset(DN_NUMBER_0_WSG, 10, &gameData->assets[DN_NUMBER_ASSET]);
 
-    dn_loadAsset(DN_ALBUM_EXPLOSION_0_WSG, 6, &gameData->assets[DN_ALBUM_EXPLOSION_ASSET]);
+    dn_loadAsset(DN_ALBUM_EXPLOSION_0_WSG, 7, &gameData->assets[DN_ALBUM_EXPLOSION_ASSET]);
 
     dn_loadAsset(MMM_UP_WSG, 1, &gameData->assets[DN_MMM_UP_ASSET]);
 
