56c5e7424f8692db51880f9dc946d868554cfc02
Pokesalot <athankinson@gmail.com>
2025-09-23 18:12:14 -0400

    Final Commit before migration to an official branch

--- main/modes/utilities/cipher/cipher.c
+++ main/modes/utilities/cipher/cipher.c
@@ -1,10 +1,23 @@
 #include "cipher.h"
+#include "math.h"
 
 static const char DR_NAMESTRING[] = "Caesar Cipher";
+//static const char ALPHABET[] = "ABCDEFG";
+
+typedef struct 
+{
+    list_t* elements;
+    bool rotating;
+    bool direction;
+    int32_t raceDiam;
+} cipherRace_t;
+
 
 static void cipherEnterMode(void);
 static void cipherExitMode(void);
 static void cipherMainLoop(int64_t elapsedUs);
+static vec_t RThetaToXY(vec_t);
+
 
 swadgeMode_t cipherMode = {
     .modeName                 = DR_NAMESTRING,  // Assign the name we created here
@@ -26,15 +39,25 @@ swadgeMode_t cipherMode = {
     .fnEspNowSendCb           = NULL,            // If using Wifi, add the send function here
     .fnAdvancedUSB            = NULL,            // If using advanced USB things.
 };
+cipherRace_t* innerRace;
+cipherRace_t* outerRace;
+
 
 static void cipherEnterMode()
 {
+    innerRace = (cipherRace_t*)heap_caps_calloc(1, sizeof(cipherRace_t), MALLOC_CAP_8BIT);
+    outerRace = (cipherRace_t*)heap_caps_calloc(1, sizeof(cipherRace_t), MALLOC_CAP_8BIT);
+
+    ESP_LOGI("info","This is some text  %f",sin(3.14f));
 }
  
 static void cipherExitMode()
 {
+    heap_caps_free(innerRace);
+    heap_caps_free(outerRace);
 }
  
 static void cipherMainLoop(int64_t elapsedUs)
 {
+    
 }
