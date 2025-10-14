11b8264286d02376745c9f6043ce95496985c0e7
gelakinetic <gelakinetic@gmail.com>
2025-10-14 23:35:28 +0000

    Draw total turns

--- main/modes/games/artillery/artillery_game.h
+++ main/modes/games/artillery/artillery_game.h
@@ -2,6 +2,8 @@
 
 #include "artillery.h"
 
+#define MAX_TURNS 7
+
 bool artilleryGameInput(artilleryData_t* ad, buttonEvt_t evt);
 void artilleryGameLoop(artilleryData_t* ad, uint32_t elapsedUs, bool barrelChanged);
 void artillerySwitchToGameState(artilleryData_t* ad, artilleryGameState_t newState);
