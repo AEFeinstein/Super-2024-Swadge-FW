a3c4abb797b9cc5bd12d12772445f4ca286001a3
j.vega <jveg199x@gmail.com>
2025-11-12 20:48:17 -0500

    Update level files

--- main/modes/games/platformer/mega_pulse_ex_typedef.h
+++ main/modes/games/platformer/mega_pulse_ex_typedef.h
@@ -1053,7 +1053,7 @@ static const mgLeveldef_t leveldef[]
        {.filename = LEVEL_02_BIN, .timeLimit = 180, .defaultWsgSetIndex = MG_WSGSET_KINETIC_DONUT, .mainBgmIndex = MG_BGM_KINETIC_DONUT, .bossBgmIndex = MG_BGM_BOSS_SEVER_YAGATA, .bgColors = bgGradientBlue},
        {.filename = GRIND_BIN, .timeLimit = 180, .defaultWsgSetIndex = MG_WSGSET_KINETIC_DONUT, .mainBgmIndex = MG_BGM_GRIND_PANGOLIN, .bossBgmIndex = MG_BGM_BOSS_GRIND_PANGOLIN, .bgColors = bgGradientPurple},
        {.filename = SEVER_BIN, .timeLimit = 180, .defaultWsgSetIndex = MG_WSGSET_KINETIC_DONUT, .mainBgmIndex = MG_BGM_SEVER_YAGATA, .bossBgmIndex = MG_BGM_BOSS_SEVER_YAGATA, .bgColors = bgGradientCyan},
-       {.filename = LEVEL_02_BIN, .timeLimit = 180, .defaultWsgSetIndex = MG_WSGSET_KINETIC_DONUT, .mainBgmIndex = MG_BGM_RIP_BARONESS, .bossBgmIndex = MG_BGM_BOSS_SEVER_YAGATA, .bgColors = bgGradientGreen},
+       {.filename = DUMP_BIN, .timeLimit = 180, .defaultWsgSetIndex = MG_WSGSET_KINETIC_DONUT, .mainBgmIndex = MG_BGM_RIP_BARONESS, .bossBgmIndex = MG_BGM_BOSS_TRASH_MAN, .bgColors = bgGradientGreen},
        {.filename = LEVEL_02_BIN, .timeLimit = 180, .defaultWsgSetIndex = MG_WSGSET_KINETIC_DONUT, .mainBgmIndex = MG_BGM_NULL, .bossBgmIndex = MG_BGM_BOSS_BIGMA, .bgColors = bgGradientMenu},
        {.filename = SMASH_BIN, .timeLimit = 180, .defaultWsgSetIndex = MG_WSGSET_KINETIC_DONUT, .mainBgmIndex = MG_BGM_SMASH_GORILLA, .bossBgmIndex = MG_BGM_BOSS_SEVER_YAGATA, .bgColors = bgGradientYellow},
        {.filename = DEADEYE_BIN, .timeLimit = 180, .defaultWsgSetIndex = MG_WSGSET_KINETIC_DONUT, .mainBgmIndex = MG_BGM_DEADEYE_CHIRPZI, .bossBgmIndex = MG_BGM_BOSS_DEADEYE_CHIRPZI, .bgColors = bgGradientOrange},
