6c7b307af18ce32e9f0461982e06bf4e34abd5f7
j.vega <jveg199x@gmail.com>
2025-12-03 23:28:43 -0500

    Add smash gorilla wsgSet

--- main/modes/games/platformer/mgWsgManager.c
+++ main/modes/games/platformer/mgWsgManager.c
@@ -847,6 +847,36 @@ void mg_loadWsgSet(mgWsgManager_t* self, mgWsgSetIndex_t index)
                 self->sprites[MG_SP_BOSS_0 + i].hitBox = mg_kineticDounutBossSpriteMetadataSet[i].hitBox;
             }
             break;
+        case MG_WSGSET_SMASH_GORILLA:
+            for (uint16_t i = 0; i < MG_KINETIC_DONUT_TILESET_MAP_LENGTH; i++)
+            {
+                uint16_t wsgIndex = mg_smashGorillaTileset[i * MG_TILESET_MAP_ROW_LENGTH + MG_WSG_INDEX_LOOKUP_OFFSET];
+
+                if (self->wsgs[wsgIndex].w && self->wsgs[wsgIndex].h)
+                {
+                    freeWsg(&self->wsgs[wsgIndex]);
+                }
+
+                loadWsg(mg_smashGorillaTileset[i * MG_TILESET_MAP_ROW_LENGTH + MG_IMAGE_FILENAME_LOOKUP_OFFSET],
+                        &self->wsgs[wsgIndex], false);
+                self->tiles[mg_smashGorillaTileset[(i * MG_TILESET_MAP_ROW_LENGTH + MG_TILE_INDEX_LOOKUP_OFFSET)] - 32]
+                    = &self->wsgs[wsgIndex];
+                self->transparencyFunction = &mg_kineticDonutTileset_needsTransparency;
+            }
+
+            for (uint16_t i = 0; i < 8; i++)
+            {
+                uint16_t wsgIndex = MG_WSG_TILE_NONSOLID_VISIBLE_NONINTERACTIVE_F0 + i;
+                if(!(self->wsgs[wsgIndex].w && self->wsgs[wsgIndex].h))
+                {
+                    continue;
+                }
+
+                self->sprites[MG_SP_BOSS_0 + i].wsg = &self->wsgs[wsgIndex];
+                self->sprites[MG_SP_BOSS_0 + i].origin = mg_smashGorillaBossSpriteMetadataSet[i].origin;
+                self->sprites[MG_SP_BOSS_0 + i].hitBox = mg_smashGorillaBossSpriteMetadataSet[i].hitBox;
+            }
+            break;
     }
 
     self->wsgSetIndex = index;
