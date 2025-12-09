cf8ed03719a4ba33eeddb2db27bc69256a28f2bf
DebrisHauler <jla21@att.net>
2025-12-09 07:52:41 -0500

    bug fix

--- main/modes/games/platformer/megaPulseEx.c
+++ main/modes/games/platformer/megaPulseEx.c
@@ -270,10 +270,10 @@ void platformerEnterMode(void)
         setMidiParams(platformer->gameData.cutscene, 1, 81, 1, 100, false);
         //Bigma
         addCutsceneStyle(platformer->gameData.cutscene, c310, BIGMA_PORTRAIT_0_WSG, TEXTBOX_CORRUPTED_WSG, "Bigma", 2, false, true, true);
-        setMidiParams(platformer->gameData.cutscene, 3, 80, -2, 2000, true);
+        setMidiParams(platformer->gameData.cutscene, 2, 80, -2, 2000, true);
         //TrashMan
         addCutsceneStyle(platformer->gameData.cutscene, c414, OVO_PORTRAIT_0_WSG, TEXTBOX_OVO_WSG, "Trash Man", 7, false, true, true);
-        setMidiParams(platformer->gameData.cutscene, 2, 36, -2, 250, false);
+        setMidiParams(platformer->gameData.cutscene, 3, 36, -2, 250, false);
         ///AbilityUnlocked
         addCutsceneStyle(platformer->gameData.cutscene, c454, ABILITY_UNLOCKED_WSG, TEXTBOX_PULSE_WSG, "", 1, true, true, false);
         setMidiParams(platformer->gameData.cutscene, 4, 11, 0, 250, false);
