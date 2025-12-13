135a86c29f8d07fe0534d5b5bf6e44da96f89fdc
DebrisHauler <jla21@att.net>
2025-12-12 19:05:42 -0500

    Make Format

--- main/modeIncludeList.c
+++ main/modeIncludeList.c
@@ -16,11 +16,11 @@
 
 /// @brief Add swadgeMode_t pointers to this struct to include them in emulator and main menu
 swadgeMode_t* const allSwadgeModes[] = {
-    &accelTestMode,      &artilleryMode,    &bouncyMode,         &canvasTestMode,  &colorchordMode, &cosCrunchMode,
-    &danceMode,          &danceNetworkMode, &factoryTestMode,    &gamepadMode,     &introMode,      &jukeboxMode,
-    &keebTestMode,       &mainMenuMode,     &modeCh32v003test,   &modeCredits,     &modeDiceRoller, &modePicross,
-    &modePlatformer,     &nameTestMode,     &roboRunnerMode,     &sequencerMode,   &sonaTestMode,   &swadgeItMode,
-    &swadgePassTestMode, &swadgedokuMode,   &swadgetamatoneMode, &swsnCreatorMode, &synthMode,      &tCaseMode,
+    &accelTestMode,      &artilleryMode,    &bouncyMode,         &canvasTestMode,  &colorchordMode,   &cosCrunchMode,
+    &danceMode,          &danceNetworkMode, &factoryTestMode,    &gamepadMode,     &introMode,        &jukeboxMode,
+    &keebTestMode,       &mainMenuMode,     &modeCh32v003test,   &modeCredits,     &modeDiceRoller,   &modePicross,
+    &modePlatformer,     &nameTestMode,     &roboRunnerMode,     &sequencerMode,   &sonaTestMode,     &swadgeItMode,
+    &swadgePassTestMode, &swadgedokuMode,   &swadgetamatoneMode, &swsnCreatorMode, &synthMode,        &tCaseMode,
     &touchTestMode,      &trophyTestMode,   &tunernomeMode,      &cipherMode,      &findingFacesMode,
 };
 
