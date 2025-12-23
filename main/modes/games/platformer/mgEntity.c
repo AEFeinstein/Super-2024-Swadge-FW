4f9807395e480eeb46143053f401c61ac65c94b5
gelakinetic <gelakinetic@gmail.com>
2025-12-22 22:23:21 -0500

    Formatting

--- main/modes/games/platformer/mgEntity.c
+++ main/modes/games/platformer/mgEntity.c
@@ -5901,8 +5901,9 @@ void mg_updateBossBigma(mgEntity_t* self)
     if (self->type == ENTITY_DEAD && self->gameData->level != 11)
     {
         mg_deactivateAllEntities(self->entityManager, true);
-        
-        self->linkedEntity = createMixtape(self->entityManager, self->entityManager->bossSpawnX - 100, self->entityManager->bossSpawnY - 20);
+
+        self->linkedEntity = createMixtape(self->entityManager, self->entityManager->bossSpawnX - 100,
+                                           self->entityManager->bossSpawnY - 20);
         startOutroCutscene(self);
     }
     // despawnWhenOffscreen(self);
