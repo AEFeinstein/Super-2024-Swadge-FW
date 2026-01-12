5387812db84ea2591cbfaad02bc0a7cd8f9aef72
gelakinetic <gelakinetic@gmail.com>
2026-01-12 09:39:30 -0500

    #612 Add scripts to read and write NVS

---
+++ tools/nvs-readback.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PORT=/dev/ttyACM0
+OPTS="--chip esp32s2 --port $PORT --before no_reset --after no_reset read_flash --flash_size 4MB"
+
+# esptool.py $OPTS 0x1000    0x4000 fb_bootloader.bin
+# esptool.py $OPTS 0x8000    0x1000 fb_partition-table.bin
+esptool.py $OPTS 0x9000    0x6000 fb_nvs.bin
+# esptool.py $OPTS 0xf000    0x1000 fb_phy.bin
+# Note, this one hangs before it finishes
+# esptool.py $OPTS 0x10000 0x3F0000 fb_app.bin
+
+# Flashing param addresses
+# "--flash-size", "4MB",
+# "0x1000",  "bootloader.bin",
+# "0x8000",  "partition-table.bin",
+# "0x10000", "swadge2024.bin"
+
+# Partition table addresses
+# nvs,      data, nvs,     0x9000,  0x6000,
+# phy_init, data, phy,     0xf000,  0x1000,
+# factory,  app,  factory, 0x10000, 0x3F0000,
