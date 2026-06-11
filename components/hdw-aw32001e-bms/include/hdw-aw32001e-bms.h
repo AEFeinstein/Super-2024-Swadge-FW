afb49a8dbb12c840eab1f6fb0afce64732d5f02b
Emily Anthony <70810799+emilyanthony4244@users.noreply.github.com>
2026-06-11 19:05:46 -0400

    Update hdw-aw32001e-bms.h

--- components/hdw-aw32001e-bms/include/hdw-aw32001e-bms.h
+++ components/hdw-aw32001e-bms/include/hdw-aw32001e-bms.h
@@ -102,16 +102,39 @@ typedef enum {
   } JunctionTemperatureLimit;
 
 /**
+ * @brief Input Current Limit in mA
+ */
+
+  typedef enum {
+    INPUTCURRENT_50mA,
+    INPUTCURRENT_80mA,
+    INPUTCURRENT_110mA,
+    INPUTCURRENT_140mA,
+    INPUTCURRENT_170mA,
+    INPUTCURRENT_200mA,
+    INPUTCURRENT_230mA,
+    INPUTCURRENT_260mA,
+    INPUTCURRENT_290mA,
+    INPUTCURRENT_320mA,
+    INPUTCURRENT_350mA, //do not go below 350mA for the Swadge, as the Swadge draws approximately 250mA max at 5V input and Fast Charge is set to 128mA default. Going below this limit will prevent Dynamic Power Mode from functioning properly and the USB input may not charge the Battery.
+    INPUTCURRENT_380mA,
+    INPUTCURRENT_410mA,
+    INPUTCURRENT_440mA,
+    INPUTCURRENT_470mA,
+    INPUTCURRENT_500mA, //default setting, 500mA
+  } InputCurrent;
+
+/**
  * @brief Charge Rate Thresholds in mA
  */
 
   typedef enum {
-    FastCharge_8 = 0,
-    FastCharge_16 = 1,
-    FastCharge_64 = 7,
-    FastCharge_128 = 15, //default setting, 128mA
-    FastCharge_256 = 31,
-    FastCharge_512 = 63,
+    FASTCHARGE_8 = 0,
+    FASTCHARGE_16 = 1,
+    FASTCHARGE_64 = 7,
+    FASTCHARGE_128 = 15, //default setting, 128mA
+    FASTCHARGE_256 = 31,
+    FASTCHARGE_512 = 63,
   } FastChargeCurrent;
 
 /**
@@ -119,22 +142,22 @@ typedef enum {
  */
 
   typedef enum {
-    Discharge_200mA, //do not use, the Swadge draws approximately 250mA max at 5V input or ~275mA at 4.6V and ~340mA at 3.7V.
-    Discharge_400mA,
-    Discharge_600mA, //recommended Swadge setting
-    Discharge_800mA,
-    Discharge_1000mA,
-    Discharge_1200mA,
-    Discharge_1400mA,
-    Discharge_1600mA,
-    Discharge_1800mA,
-    Discharge_2000mA, //default setting
-    Discharge_2200mA,
-    Discharge_2400mA,
-    Discharge_2600mA,
-    Discharge_2800mA,
-    Discharge_3000mA,
-    Discharge_3200mA,
+    DISCHARGE_200mA, //do not use, the Swadge draws approximately 250mA max at 5V input or ~275mA at 4.6V and ~340mA at 3.7V.
+    DISCHARGE_400mA,
+    DISCHARGE_600mA, //recommended Swadge setting
+    DISCHARGE_800mA,
+    DISCHARGE_1000mA,
+    DISCHARGE_1200mA,
+    DISCHARGE_1400mA,
+    DISCHARGE_1600mA,
+    DISCHARGE_1800mA,
+    DISCHARGE_2000mA, //default setting
+    DISCHARGE_2200mA,
+    DISCHARGE_2400mA,
+    DISCHARGE_2600mA,
+    DISCHARGE_2800mA,
+    DISCHARGE_3000mA,
+    DISCHARGE_3200mA,
   } DischargeCurrent;
 
 /**
@@ -142,39 +165,38 @@ typedef enum {
  */
 
  typedef enum {
-    ChargeVoltage_3600mV = 0,
-    ChargeVoltage_3750mV = 10,
-    ChargeVoltage_3900mV = 20,
-    ChargeVoltage_4050mV = 30,
-    ChargeVoltage_4200mV = 40, //default setting
-    ChargeVoltage_4350mV = 50,
-    ChargeVoltage_4500mV = 60,
+    CHARGEVOLTAGE_3600mV = 0,
+    CHARGEVOLTAGE_3750mV = 10,
+    CHARGEVOLTAGE_3900mV = 20,
+    CHARGEVOLTAGE_4050mV = 30,
+    CHARGEVOLTAGE_4200mV = 40, //default setting
+    CHARGEVOLTAGE_4350mV = 50,
+    CHARGEVOLTAGE_4500mV = 60,
   } ChargeVoltageThreshold;
 
   /**
  * @brief System Regulated Output Voltage in mV
  */ 
 typedef enum {
-  SysVoltage4200,
-  SysVoltage4250,
-  SysVoltage4300,
-  SysVoltage4350,
-  SysVoltage4400,
-  SysVoltage4450,
-  SysVoltage4500,
-  SysVoltage4550,
-  SysVoltage4600,   //default output voltage
-  SysVoltage4650,
-  SysVoltage4700,
-  SysVoltage4750,
-  SysVoltage4800,
-  SysVoltage4850,
-  SysVoltage4900,
-  SysVoltage4950,
+  SYSVOLTAGE_4200,
+  SYSVOLTAGE_4250,
+  SYSVOLTAGE_4300,
+  SYSVOLTAGE_4350,
+  SYSVOLTAGE_4400,
+  SYSVOLTAGE_4450,
+  SYSVOLTAGE_4500,
+  SYSVOLTAGE_4550,
+  SYSVOLTAGE_4600,   //default output voltage
+  SYSVOLTAGE_4650,
+  SYSVOLTAGE_4700,
+  SYSVOLTAGE_4750,
+  SYSVOLTAGE_4800,
+  SYSVOLTAGE_4850,
+  SYSVOLTAGE_4900,
+  SYSVOLTAGE_4950,
 } SystemRegulatedVoltage;
 
 esp_err_t initBMS(FastChargeCurrent current, ChargeVoltageThreshold voltage, UnderVoltageLockout uvlo, JunctionTemperatureLimit tj, SystemRegulatedVoltage sys_voltage, uint8_t timeout);
-
 
   void enableCharge();                                          // enable charging
   void setChargeEnable(bool enable);                            // charge control
