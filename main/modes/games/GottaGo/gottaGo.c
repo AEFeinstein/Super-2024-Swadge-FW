363057330501ae9ebf6d2f5b51f6e5286a7748c0
Jeremy Stintzcum <johnnywycliffe@gmail.com>
2026-08-15 19:21:03 -0400

    Added toilet drawing function, updated sprs to match

--- main/modes/games/GottaGo/gottaGo.c
+++ main/modes/games/GottaGo/gottaGo.c
@@ -22,7 +22,10 @@
 #define FLOOR_HEIGHT 40
 
 // Toilets
-#define MAX_TOILETS 7
+#define MAX_TOILETS        7
+#define DIVIDER_X_OFFSET   32
+#define DIVIDER_TOP_OFFSET -24
+#define DIVIDER_BOT_OFFSET 12
 
 // UI
 #define UI_STALL_X       12
@@ -54,14 +57,30 @@ static const cnfsFileIdx_t backgroundImages[] = {
     GG_BATHROOM_TILE_WSG,
 };
 static const cnfsFileIdx_t toiletImages[] = {
-    GG_FLUSH_HANDLE_WSG,  GG_FLUSH_AUTO_WSG,      GG_TOILET_TOP_WSG,
-    GG_TOILET_STRIP_WSG,  GG_TOILET_BOTTOM_WSG,   GG_TOILET_BOTTOM_BROKEN_WSG,
-    GG_DOWNPIPE_WSG,      GG_DOWNPIPE_BROKEN_WSG, GG_DOWNPIPE_BROKEN_2_WSG,
-    GG_DIVIDER_WSG,       GG_DIVIDER_TOP_WSG,     GG_DIVIDER_BOTTOM_WSG,
-    GG_CRACK_1_WSG,       GG_CRACK_2_WSG,         GG_GRAFFITI_1_WSG,
-    GG_GRAFFITI_2_WSG,    GG_GRAFFITI_3_WSG,      GG_OUT_OF_ORDER_WSG,
-    GG_PLUGGED_DRAIN_WSG, GG_WATER_LEAK_WSG,      GG_PEE_S_WSG,
-    GG_PEE_M_WSG,         GG_PEE_L_WSG,
+    GG_FLUSH_HANDLE_WSG,
+    GG_FLUSH_AUTO_WSG,
+    GG_TOILET_TOP_WSG,
+    GG_TOILET_STRIP_WSG,
+    GG_TOILET_BOTTOM_WSG,
+    GG_TOILET_BOTTOM_BROKEN_WSG,
+    GG_TOILET_BOTTOM_BROKEN_2_WSG,
+    GG_DOWNPIPE_WSG,
+    GG_DOWNPIPE_BROKEN_WSG,
+    GG_DOWNPIPE_BROKEN_2_WSG,
+    GG_GRAFFITI_1_WSG,
+    GG_GRAFFITI_2_WSG,
+    GG_GRAFFITI_3_WSG,
+    GG_CRACK_1_WSG,
+    GG_CRACK_2_WSG,
+    GG_WATER_LEAK_WSG,
+    GG_PLUGGED_DRAIN_WSG,
+    GG_OUT_OF_ORDER_WSG,
+    GG_DIVIDER_WSG,
+    GG_DIVIDER_TOP_WSG,
+    GG_DIVIDER_BOTTOM_WSG,
+    GG_PEE_S_WSG,
+    GG_PEE_M_WSG,
+    GG_PEE_L_WSG,
 };
 static const cnfsFileIdx_t uiImages[] = {
     GG_STALL_ICON_WSG,
@@ -102,14 +121,18 @@ typedef enum
 
 typedef struct
 {
-    bool active;              // if urinal is active
-    int heightOffFloor;       // Height above floor
-    int height;               // Height of actual unit (default: 35)
-    bool autoFlush;           // Autoflush or manual
-    bool mildBreak;           // Graffiti, Cracks, Water flowing
-    bool hardBreak;           // Drainpipe, Out-Of-Order, Broken Bowl, Not draining
-    ggPeePool_t pp;           // Puddle on the ground (Small, Med, Large)
-    ggDivider_t rightDivider; // Full, top, bottom, none
+    int height; // Height of actual unit (default: 35)
+    uint autoFlush    : 1;
+    uint graffiti     : 3;
+    uint cracks       : 2;
+    uint waterLeak    : 1;
+    uint brokenDrain  : 1;
+    uint outOfOrder   : 1;
+    uint brokenBowl   : 1;
+    uint pluggedDrain : 1;
+    uint puddle       : 2;
+    uint divider      : 2;
+    uint small        : 1;
 } ggToilet_t;
 
 typedef struct
@@ -151,6 +174,7 @@ void drawTitle(void);
 
 // Common draw
 void drawBackground(void);
+void drawToiletArray(void);
 void drawToilet(ggToilet_t* t, int x, int y);
 void drawUI(void);
 
@@ -276,6 +300,9 @@ static void drawSplash(int64_t elapsedUs)
     drawBackground();
 
     // Draw set pattern of urinals
+    // Set urinals
+    // Draw
+    drawToiletArray();
 
     // Draw some people
 
@@ -335,23 +362,148 @@ void drawBackground()
                   TFT_HEIGHT - (ggd->backgroundImages[0].h + FLOOR_HEIGHT));
 }
 
+void drawToiletArray()
+{
+    // FIXME: test code
+    ggd->toilets[4].waterLeak    = 1;
+    ggd->toilets[1].pluggedDrain = 1;
+    ggd->toilets[0].outOfOrder   = 1;
+    for (int idx = 0; idx < MAX_TOILETS; idx++)
+    {
+        ggd->toilets[idx].autoFlush   = idx % 2;
+        ggd->toilets[idx].brokenBowl  = (idx >= 5) ? 1 : 0;
+        ggd->toilets[idx].brokenDrain = (idx < 2) ? 1 : 0;
+        ggd->toilets[idx].graffiti    = idx;
+        ggd->toilets[idx].cracks      = idx % 4;
+        ggd->toilets[idx].puddle      = idx % 4;
+        ggd->toilets[idx].divider     = idx % 4;
+        ggd->toilets[idx].height      = 35;
+        drawToilet(&ggd->toilets[idx], idx * 37, 120);
+    }
+}
+
 void drawToilet(ggToilet_t* t, int x, int y)
 {
+    // Puddle - Needs to happen before toilet is generated since debris can be on top
+    switch (t->puddle)
+    {
+        case 0:
+        default:
+        {
+            break;
+        }
+        case 1:
+        {
+            drawWsgSimple(&ggd->toiletImages[21], x + 6, TFT_HEIGHT - FLOOR_HEIGHT + 4);
+            break;
+        }
+        case 2:
+        {
+            drawWsgSimple(&ggd->toiletImages[22], x + 3, TFT_HEIGHT - FLOOR_HEIGHT + 1);
+            break;
+        }
+        case 3:
+        {
+            drawWsgSimple(&ggd->toiletImages[23], x - 2, TFT_HEIGHT - FLOOR_HEIGHT + 1);
+            break;
+        }
+    }
     // Flush
-
+    int flag = (t->autoFlush == 1) ? 1 : 0;
+    drawWsgSimple(&ggd->toiletImages[flag], x + (ggd->toiletImages[2].w - ggd->toiletImages[flag].w) / 2,
+                  y - ggd->toiletImages[flag].h);
     // Top
-
+    drawWsgSimple(&ggd->toiletImages[2], x, y);
     // Middle
-
+    int currHeight;
+    for (currHeight = ggd->toiletImages[2].h; currHeight < t->height; currHeight++)
+    {
+        drawWsgSimple(&ggd->toiletImages[3], x, y + currHeight);
+    }
     // Bottom
-
+    if (t->brokenBowl == 1)
+    {
+        drawWsgSimple(&ggd->toiletImages[5], x, y + currHeight);
+        drawWsgSimple(&ggd->toiletImages[6], x + 16, TFT_HEIGHT - FLOOR_HEIGHT + 4);
+    }
+    else
+    {
+        drawWsgSimple(&ggd->toiletImages[4], x, y + currHeight);
+    }
     // Drain
-
-    // Aesthetics
-
+    if (t->brokenDrain == 1)
+    {
+        drawWsgSimple(&ggd->toiletImages[8], x + (ggd->toiletImages[2].w - ggd->toiletImages[8].w) / 2,
+                      y + currHeight + ggd->toiletImages[4].h);
+        drawWsgSimple(&ggd->toiletImages[9], x + 5, TFT_HEIGHT - FLOOR_HEIGHT + 4);
+    }
+    else
+    {
+        drawWsgSimple(&ggd->toiletImages[7], x + (ggd->toiletImages[2].w - ggd->toiletImages[7].w) / 2,
+                      y + currHeight + ggd->toiletImages[4].h);
+    }
+    // Graffiti
+    if ((t->graffiti & 0x1) == 0x1)
+    {
+        drawWsgSimple(&ggd->toiletImages[10], x + 16, y + 25);
+    }
+    if ((t->graffiti & 0x2) == 0x2)
+    {
+        drawWsgSimple(&ggd->toiletImages[11], x + 5, y + 15);
+    }
+    if ((t->graffiti & 0x4) == 0x4)
+    {
+        drawWsgSimple(&ggd->toiletImages[12], x + 7, y + 5);
+    }
+    // Cracks
+    if ((t->cracks & 0x1) == 0x1)
+    {
+        drawWsgSimple(&ggd->toiletImages[13], x + 3, y + 25);
+    }
+    if ((t->cracks & 0x2) == 0x2)
+    {
+        drawWsgSimple(&ggd->toiletImages[14], x + ggd->toiletImages[2].w - (ggd->toiletImages[14].w + 4), y + 8);
+    }
+    // Water Leak
+    // FIXME: Doesn't work for variable length urinals (Currently works visually for h=35 to h=40)
+    if (t->waterLeak == 1)
+    {
+        drawWsgSimple(&ggd->toiletImages[15], x + 12, y + 4);
+    }
+    // Plugged Drain
+    if (t->pluggedDrain == 1)
+    {
+        drawWsgSimple(&ggd->toiletImages[16], x + 5, y + currHeight - 1);
+    }
+    // OOO
+    if (t->outOfOrder == 1)
+    {
+        drawWsgSimple(&ggd->toiletImages[17], x + 5, y + 9);
+    }
     // Divider
-
-    // Puddle
+    switch (t->divider)
+    {
+        case 0:
+        default:
+        {
+            drawWsgSimple(&ggd->toiletImages[18], x + DIVIDER_X_OFFSET, y + DIVIDER_TOP_OFFSET);
+            break;
+        }
+        case 1:
+        {
+            drawWsgSimple(&ggd->toiletImages[19], x + DIVIDER_X_OFFSET, y + DIVIDER_TOP_OFFSET);
+            break;
+        }
+        case 2:
+        {
+            drawWsgSimple(&ggd->toiletImages[20], x + DIVIDER_X_OFFSET, y + DIVIDER_BOT_OFFSET);
+            break;
+        }
+        case 3:
+        {
+            break;
+        }
+    }
 }
 
 void drawUI()
@@ -379,7 +531,7 @@ void drawUI()
                    UI_BAR_PADDING_Y + UI_BAR_HEIGHT - 1, c440);
 
     // Timer
-    float time = (ggd->loseTimerMax - ggd->loseTimer) / 1000000.0; // FIXME: Float
-    snprintf(buffer, sizeof(buffer) - 1, "Time left: %.3f", time);
+    long timeLeft = ggd->loseTimerMax - ggd->loseTimer;
+    snprintf(buffer, sizeof(buffer) - 1, "Time left: %ld.%03ld", timeLeft / 1000000, (timeLeft % 1000000) / 1000);
     drawText(&ggd->normalFont, c550, buffer, UI_TIMER_X, UI_TIMER_Y);
-}  
+}
