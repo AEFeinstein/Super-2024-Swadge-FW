54c41a0325687c7432991fe979032d887025498b
gelakinetic <gelakinetic@gmail.com>
2026-08-20 23:41:29 -0400

    Refactor bushes to be doors Add cracked door, need to check for bombs

--- main/modes/games/magtroid/enemies/ray_enemy.h
+++ main/modes/games/magtroid/enemies/ray_enemy.h
@@ -5,9 +5,9 @@
 #include "ray_object.h"
 
 #include "ray_enemy_box.h"
-#include "ray_enemy_bush.h"
 
 void rayEnemiesMoveAnimate(ray_t* ray, uint32_t elapsedUs);
 void rayEnemyGetShot(ray_t* ray, rayEnemy_t* enemy, rayMapCellType_t bullet);
 void rayEnemyCheckCollision(ray_t* ray, rayEnemy_t* enemy, rectangle_t player, q24_8* deltaX, q24_8* deltaY);
-rayMapCellType_t enemyDropAfterDeath(ray_t* ray, rayEnemy_t* enemy);
+rayMapCellType_t enemyDropAfterDeath(rayEnemy_t* enemy);
+rayMapCellType_t rayEnemyStandardItemDrop(void);
