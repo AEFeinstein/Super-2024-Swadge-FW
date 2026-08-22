fab2c3701f6c807cd701bec359ee1ad157cf0f1b
DebrisHauler <jla21@att.net>
2026-08-22 03:41:41 -0400

    message structs

---
+++ main/modes/games/GossipStone/gs_gossip.c
@@ -0,0 +1,35 @@
+#pragma once
+#include "gs_gossip.h"
+
+char gossipIntro[32] = "Shake the rock to make it talk.";
+char gossip[5][26] = {
+    "Yo, this is the first line.",
+    "Number 2 here.",
+    "Here is line number 3.",
+    "Yeah, number four.",
+    "Okay, that's number five.",
+};
+
+char AMAIntro[78] = "Speak a yes or no question clearly into the microphone then shake the swadge.";
+char AMA[20][26] = {
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sourcfes say no",
+    "Outlook not so good",
+    "Very doubtful",
+};
