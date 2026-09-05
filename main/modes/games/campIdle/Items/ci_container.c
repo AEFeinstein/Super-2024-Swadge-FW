c765a508e23d169996b29486934d57cdac65d070
Jeremy Stintzcum <johnnywycliffe@gmail.com>
2026-09-05 18:13:51 -0400

    Added containers

---
+++ main/modes/games/campIdle/Items/ci_container.c
@@ -0,0 +1,137 @@
+//==============================================================================
+// Include
+//==============================================================================
+
+#include "ci_container.h"
+#include "ci_items.h"
+
+//==============================================================================
+// Defines
+//==============================================================================
+
+#define MAX_PER_SLOT 255
+
+// Drawing
+#define EDGE_BUFFER 2
+
+//==============================================================================
+// Functions
+//==============================================================================
+
+void ciInitContainer(ciContainer_t* cont, int maxSlots, int weightLim, int sizeLim)
+{
+    cont->slotsLim  = maxSlots;
+    cont->sizeLim   = sizeLim;
+    cont->weightLim = weightLim;
+    cont->items     = (ciContainerItem_t*)heap_caps_calloc(maxSlots, sizeof(ciContainerItem_t), MALLOC_CAP_8BIT);
+    for (int idx = 0; idx < maxSlots; idx++)
+    {
+        cont->items[idx].item = CI_NO_ITEM;
+        cont->items[idx].qty  = 0;
+    }
+}
+
+void ciFreeContainer(ciContainer_t* cont)
+{
+    free(cont->items);
+}
+
+int ciAddItemToContainer(ciContainer_t* cont, ciItemIdx_t item, int qty)
+{
+    // Check if there's already a slot with the same item
+    for (int idx = 0; idx < cont->slotsLim; idx++)
+    {
+        if (cont->items[idx].item == item)
+        {
+            if (MAX_PER_SLOT == cont->items[idx].qty)
+            {
+                continue;
+            }
+            else if (MAX_PER_SLOT < cont->items[idx].qty + qty)
+            {
+                // Fills up
+                qty -= MAX_PER_SLOT - cont->items[idx].qty;
+                cont->items[idx].qty = MAX_PER_SLOT;
+            }
+            else
+            {
+                // Not full
+                cont->items[idx].qty += qty;
+                qty = 0;
+            }
+        }
+    }
+    if (qty > 0)
+    {
+        for (int idx = 0; idx < cont->slotsLim; idx++)
+        {
+            if (cont->items[idx].item == CI_NO_ITEM)
+            {
+                cont->items[idx].item = item;
+                if (qty >= MAX_PER_SLOT)
+                {
+                    qty -= MAX_PER_SLOT;
+                    cont->items[idx].qty = MAX_PER_SLOT;
+                }
+                else
+                {
+                    cont->items[idx].qty = qty;
+                    qty                  = 0;
+                    break;
+                }
+            }
+        }
+    }
+    return qty;
+}
+
+int ciRemoveFromContainer(ciContainer_t* cont, ciItemIdx_t item, int qty)
+{
+    for (int idx = cont->slotsLim - 1; idx >= 0; idx--)
+    {
+        if (cont->items[idx].item == item && qty > 0)
+        {
+            if (qty > cont->items[idx].qty)
+            {
+                qty -= cont->items[idx].qty;
+                cont->items[idx].item = CI_NO_ITEM;
+                cont->items[idx].qty  = 0;
+            }
+            else
+            {
+                cont->items[idx].qty -= qty;
+                qty = 0;
+                break;
+            }
+        }
+    }
+    return qty;
+}
+
+int ciFindItemInContainer(ciContainer_t* cont, ciItemIdx_t item, int* slotLoc)
+{
+    int total = 0;
+    for (int idx = 0; idx < cont->slotsLim; idx++)
+    {
+        if (cont->items[idx].item == item)
+        {
+            total += cont->items[idx].qty;
+            *slotLoc = idx;
+        }
+    }
+    return total;
+}
+
+void ciDrawContainer(ciCampData_t* ccd, ciContainer_t* cont, int x, int y, int maxCols, int maxRows, font_t* fnt)
+{
+    int calcXSize = 2 * EDGE_BUFFER + maxCols * ICON_WIDTH;
+    int calcYSize = 2 * EDGE_BUFFER + maxRows * ICON_HEIGHT;
+
+    fillDisplayArea(x, y, x + calcXSize, y + calcYSize, c111);
+    for (int idx = 0; idx < cont->slotsLim; idx++)
+    {
+        ciDrawItemIcon(ccd, cont->items[idx].item, x + EDGE_BUFFER + (idx % maxCols) * ICON_WIDTH,
+                       y + EDGE_BUFFER + (idx / maxCols) * ICON_HEIGHT, cont->items[idx].qty, false, true);
+    }
+    drawRect(x, y, x + calcXSize, y + calcYSize, c000);
+}
