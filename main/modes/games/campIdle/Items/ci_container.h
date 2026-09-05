dcc891e1e02f6f0019ed898067b9976ebff02d21
Jeremy Stintzcum <johnnywycliffe@gmail.com>
2026-09-05 18:13:51 -0400

    Added containers

---
+++ main/modes/games/campIdle/Items/ci_container.h
@@ -0,0 +1,24 @@
+#pragma once
+
+//==============================================================================
+// Include
+//==============================================================================
+
+#include "ci_genericData.h"
+#include "swadge.h"
+
+//==============================================================================
+// Function declarations
+//==============================================================================
+
+void ciInitContainer(ciContainer_t* cont, int maxSlots, int weightLim, int sizeLim);
+
+void ciFreeContainer(ciContainer_t* cont);
+
+int ciAddItemToContainer(ciContainer_t* cont, ciItemIdx_t item, int qty);
+
+int ciRemoveFromContainer(ciContainer_t* cont, ciItemIdx_t item, int qty);
+
+int ciFindItemInContainer(ciContainer_t* cont, ciItemIdx_t item, int* slotLoc);
+
+void ciDrawContainer(ciCampData_t* ccd, ciContainer_t* cont, int x, int y, int maxCols, int maxRows, font_t* fnt);
