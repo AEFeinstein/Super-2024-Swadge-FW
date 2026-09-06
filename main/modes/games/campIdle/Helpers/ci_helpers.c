97e5d00f5a9e9b97e0c39ec162c31e70362e06b7
Jeremy Stintzcum <johnnywycliffe@gmail.com>
2026-09-06 12:13:22 -0400

    Added menu navigation helpers

---
+++ main/modes/games/campIdle/Helpers/ci_helpers.c
@@ -0,0 +1,44 @@
+#include "ci_helpers.h"
+
+int ciMenu2DNavigate(buttonEvt_t* evt, int selection, int colLim, int maxVal)
+{
+    int outVal = selection;
+    if (evt->button & PB_RIGHT)
+    {
+        outVal++;
+        if (outVal % colLim == 0)
+        {
+            outVal -= colLim;
+        }
+        if (outVal >= maxVal)
+        {
+            outVal = (maxVal / colLim) * colLim;
+        }
+    }
+    else if (evt->button & PB_LEFT)
+    {
+        outVal--;
+        if (outVal == -1 || outVal % colLim == colLim - 1)
+        {
+            outVal += colLim;
+        }
+        if (outVal >= maxVal)
+        {
+            outVal = maxVal - 1;
+        }
+    }
+    else if (evt->button & PB_DOWN)
+    {
+        outVal += colLim;
+        outVal %= maxVal;
+    }
+    else if (evt->button & PB_UP)
+    {
+        outVal -= colLim;
+        if (outVal < 0)
+        {
+            outVal += maxVal;
+        }
+    }
+    return outVal;
+}
